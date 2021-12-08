5c911c1f8ff3efc113583f8afefa951a3eb0dbb8
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-12-08 22:51:22 +0900

    feat: SELECTOR 상수 정리

---
+++ src/constant/selector.js
@@ -0,0 +1,61 @@
+export const ID = {
+  APP: 'app',
+  PRODUCT: {
+    PURCHASE_MENU: 'product-purchase-menu',
+    ADD_MENU: 'product-add-menu',
+    NAME_INPUT: 'product-name-input',
+    PRICE_INPUT: 'product-price-input',
+    QUANTITY_INPUT: 'product-quantity-input',
+    ADD_BUTTON: 'product-add-button'
+  },
+  VENDING_MACHINE: {
+    MANAGE_MENU: 'vending-machine-manage-menu',
+    CHARGE: {
+      INPUT: 'vending-machine-charge-input',
+      BUTTON: 'veding-machine-charge-button',
+      AMOUNT: 'vending-machine-charge-amount'
+    },
+    COIN_QUANTITY: {
+      FIVE_HUNDREDS: 'vending-machine-coin-500-quantity',
+      ONE_HUNDRED: 'vending-machine-coin-100-quantity',
+      FIFTY: 'vending-machine-coin-50-quantinty',
+      TEN: 'vending-machine-coin-10-quantity'
+    }
+  },
+  CHARGE: {
+    INPUT: 'charge-input',
+    BUTTON: 'charge-button',
+    AMOUNT: 'charge-amount'
+  },
+  CHANGE: {
+    BUTTON: 'coin-return-button',
+    COIN_QUANTITY: {
+      FIVE_HUNDREDS: 'coin-500-quantity',
+      ONE_HUNDRED: 'coin-100-quantity',
+      FIFTY: 'coin-50-quantity',
+      TEN: 'coin-10-quantity'
+    }
+  }
+};
+
+export const CLASS = {
+  PRODUCT: {
+    MANAGE_ITEM: 'product-manage-item',
+    MANGAE_NAME: 'product-manage-name',
+    MANAGE_PRICE: 'product-manage-price',
+    MANAGE_QUANTITY: 'product-manage-quantity'
+  },
+  PURCHACE: {
+    ITEM: 'product-purchase-item',
+    BUY_BUTTON: 'purchase-button',
+    NAME: 'product-purchase-name',
+    PRICE: 'product-purchase-price',
+    QUANTITY: 'product-purchase-quantity'
+  }
+};
+
+export const DATASET = {
+  PRODUCT_NAME: 'data-product-name',
+  PRODUCT_PRICE: 'data-product-price',
+  PRODUCT_QUANTITY: 'data-product-quantity'
+};
