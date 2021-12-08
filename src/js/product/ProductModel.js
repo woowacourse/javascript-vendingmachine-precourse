0736dca6f29515abc2063bc04c38453191057063
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-12-09 00:57:14 +0900

    feat: 같은 이름의 상품 추가될시 최근에 들어온것으로 수정하는 로직추가

--- src/js/product/ProductModel.js
+++ src/js/product/ProductModel.js
@@ -8,6 +8,23 @@ export default class ProductModel {
   };
 
   addProduct = ({ name, price, quantity }) => {
-    this.products.push({ name, price, quantity });
+    if (this.isExsitName(name)) {
+      const idx = this.findProductIdx(name);
+      this.products.splice(idx, 1, { name, price, quantity });
+    } else {
+      this.products.push({ name, price, quantity });
+    }
+  };
+
+  findProduct = (name) => {
+    return this.products.find((product) => product.name === name);
+  };
+
+  findProductIdx = (name) => {
+    return this.products.findIndex((product) => product.name === name);
+  };
+
+  isExsitName = (name) => {
+    return this.findProductIdx(name) !== -1;
   };
 }
