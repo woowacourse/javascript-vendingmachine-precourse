37baa3253cce1df12191b203af7e2ea38ca0bbcc
InSeongSo <goflvhxj2547@gmail.com>
2021-12-09 02:53:32 +0900

    ✒️Feat: 이벤트 모듈 주입
    1. 마스터 컴포넌트에 이벤트 모듈의 의존성을 주입한다.
    2. 생성자 외의 초기화 함수를 추가하여 컨테이너 컴포넌트의 자유도를 높인다.
    3. 이벤트 모듈에 등록한 모듈을 mount 시점에 핸들링한다.

--- src/container/root/Component.js
+++ src/container/root/Component.js
@@ -1,14 +1,19 @@
-import { $ } from '../../common/index.js';
+import { $ } from '../../common/helper.js';
 import Storage from '../../storage/Storage.js';
+import EventBus from '../../common/EventBus.js';
 
 export default class Component {
   constructor(selector, props) {
     this.$element = $(selector);
     this.$props = props;
     this.$storage = Storage;
+    this.$eventBus = EventBus;
 
+    this.initialized();
     this.render();
   }
+
+  initialized() {}
 
   template() {
     return ``;
