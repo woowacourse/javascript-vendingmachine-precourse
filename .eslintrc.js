1723e94dc166f1c4ba420de2b6c76f17179c94c6
sanaandmomo <qajjang46@naver.com>
2021-12-08 17:21:36 +0900

    chore: eslint, prettier 설정해라

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+        jest: true,
+    },
+    extends: ['airbnb-base', 'eslint:recommended', 'plugin:prettier/recommended'],
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+        'import/extensions': [
+            'off',
+            {
+                extensions: ['.js'],
+            },
+        ],
+    },
+    globals: {
+        MissionUtils: 'readonly',
+    },
+};
