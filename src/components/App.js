f2a7b6c9a30a34cc41abfadef206acdf33145c3e
kevin7498 <kevin7498@naver.com>
2021-12-09 15:16:26 +0900

    feat: 메뉴 버튼별로 다른 화면을 보여주도록 하는 함수 'MenuButtonEvent' 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,9 @@
 import showMain from './showMain.js';
-import ProductAddView from './ProductAddView.js';
-import MachineManageView from './MachineManageView.js';
-import ProductPurchaseView from './ProductPurchaseView.js';
+import MenuButtonController from './MenuButtonController.js';
 
 export default class App {
   constructor() {
     this.main = showMain();
-
-    document
-      .getElementById('product-add-menu')
-      .addEventListener('click', (e) => {
-        e.preventDefault();
-        ProductAddView.render();
-      });
-
-    document
-      .getElementById('vending-machine-manage-menu')
-      .addEventListener('click', (e) => {
-        e.preventDefault();
-        MachineManageView.render();
-      });
-
-    document
-      .getElementById('product-purchase-menu')
-      .addEventListener('click', (e) => {
-        e.preventDefault();
-        ProductPurchaseView.render();
-      });
+    MenuButtonController.menuButtonEvent();
   }
 }
---
+++ src/components/MenuButtonController.js
@@ -0,0 +1,21 @@
+import ProductAddView from './ProductAddView.js';
+import MachineManageView from './MachineManageView.js';
+import ProductPurchaseView from './ProductPurchaseView.js';
+
+export default class MenuButtonController {
+  static menuButtonEvent() {
+    document.addEventListener('click', (e) => {
+      const targetId = e.target.id;
+
+      if (targetId === 'product-add-menu') {
+        ProductAddView.render();
+      }
+      if (targetId === 'vending-machine-manage-menu') {
+        MachineManageView.render();
+      }
+      if (targetId === 'product-purchase-menu') {
+        ProductPurchaseView.render();
+      }
+    });
+  }
+}
