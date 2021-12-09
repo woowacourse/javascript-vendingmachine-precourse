e40a3cd4aee881499e042b43d12ca9ec6ae563af
gytks4 <gytks4@naver.com>
2021-12-09 21:21:04 +0900

    style: unused function, 주석 제거

--- src/model/VendingMachineModel.js
+++ src/model/VendingMachineModel.js
@@ -17,14 +17,7 @@ export class VendingMachineModel {
     return this.products;
   }
 
-  updateCharge(chargeMoney) {
-    this.addChargeMoney(chargeMoney);
-    this.addCoin(chargeMoney);
-  }
-
   addChargeMoney(chargeMoney) {
-    // 총 금액을 충천
-    // 충전된 만큼 동전 추가
     this.chargeAmount += chargeMoney;
     localStorage.setItem('charge', JSON.stringify(this.chargeAmount));
     return this.chargeAmount;
@@ -33,14 +26,11 @@ export class VendingMachineModel {
   addCoin(chargeMoney) {
     while (chargeMoney > 0) {
       const randomCoin = MissionUtils.Random.pickNumberInList(coinList);
-      console.log(`randomCoin`, randomCoin);
       if (chargeMoney - randomCoin >= 0) {
         chargeMoney -= randomCoin;
-        console.log(this.coins[randomCoin]);
         this.coins[randomCoin]++;
       }
     }
-    console.log(`coins`, this.coins);
     localStorage.setItem('coins', JSON.stringify(this.coins));
     return this.coins;
   }
