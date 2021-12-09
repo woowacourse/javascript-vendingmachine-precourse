c3aa5f0c8801b173c854708fe1683945f008251a
LAH1203 <lah1203@naver.com>
2021-12-09 15:46:47 +0900

    feat: 상품명과 가격이 같은 경우에는 기존 재고에서 입력한 수량만큼 더하는 기능 구현

--- src/controllers/productManager/checkInputValues.js
+++ src/controllers/productManager/checkInputValues.js
@@ -1,20 +1,6 @@
 import { ALERT_MSG } from "../../utils/constants.js";
 import { isPositiveInteger } from "../chargeManager/checkMoneyInput.js";
 
-const isDuplicateName = name => {
-  const strOfProducts = JSON.parse(localStorage.getItem("products"));
-  const productNames = [];
-
-  if (strOfProducts) {
-    const products = strOfProducts.split(",");
-    products.forEach(product => {
-      productNames.push(product.split("/")[0]);
-    });
-  }
-
-  return productNames.includes(name);
-};
-
 const isValidPrice = priceStr => {
   const price = parseInt(priceStr, 10);
 
@@ -26,18 +12,11 @@ const isEmpty = (name, price, quantity) => {
 };
 
 const isValidInputs = (name, price, quantity) => {
-  const {
-    emptyInput,
-    wrongProductName,
-    wrongProductPrice,
-    wrongProductQuantity,
-  } = ALERT_MSG;
+  const { emptyInput, wrongProductPrice, wrongProductQuantity } = ALERT_MSG;
   let errorMessage = "";
 
   if (isEmpty(name, price, quantity)) {
     errorMessage = emptyInput;
-  } else if (isDuplicateName(name)) {
-    errorMessage = wrongProductName;
   } else if (!isValidPrice(price)) {
     errorMessage = wrongProductPrice;
   } else if (!isPositiveInteger(quantity)) {
--- src/controllers/productManager/productDataController.js
+++ src/controllers/productManager/productDataController.js
@@ -1,15 +1,68 @@
+import { ALERT_MSG } from "../../utils/constants.js";
+
+// 같은 이름과 가격을 가진 상품이 있으면 해당 상품의 위치를, 없으면 -1을 반환
+const checkIsSameProduct = (strOfProducts, name, price) => {
+  const products = strOfProducts.split(",");
+  let index = -1;
+
+  for (let i = 0; i < products.length; i++) {
+    const info = products[i].split("/");
+    if (info[0] === name && info[1] === price) {
+      index = i;
+      break;
+    }
+  }
+
+  return index;
+};
+
+// 상품명과 가격이 같은 상품이면 재고와 새로 추가한 수량을 더하여 저장
+const addIfDuplicateProduct = (index, quantity) => {
+  const products = JSON.parse(localStorage.getItem("products")).split(",");
+  const info = products[index].split("/");
+
+  info[2] = parseInt(info[2], 10) + parseInt(quantity, 10);
+  products[index] = info.join("/");
+
+  localStorage.setItem("products", JSON.stringify(products.join(",")));
+};
+
+const duplicateName = (strOfProducts, name) => {
+  const products = strOfProducts.split(",");
+  const productNames = [];
+
+  products.forEach(product => {
+    productNames.push(product.split("/")[0]);
+  });
+
+  return productNames.includes(name);
+};
+
+// 기존 상품 목록에 새 상품 추가하기
+const addProductInProducts = (name, price, quantity, products) => {
+  const index = checkIsSameProduct(products, name, price);
+
+  if (index !== -1) {
+    addIfDuplicateProduct(index, quantity);
+  } else if (duplicateName(products, name)) {
+    alert(ALERT_MSG.wrongProductName);
+  } else {
+    localStorage.setItem(
+      "products",
+      JSON.stringify(`${products},${name}/${price}/${quantity}`),
+    );
+  }
+};
+
 const addProduct = (name, price, quantity) => {
   const products = JSON.parse(localStorage.getItem("products"));
   const productInfo = `${name}/${price}/${quantity}`;
-  let strOfProducts = "";
 
   if (products === null) {
-    strOfProducts = JSON.stringify(productInfo);
+    localStorage.setItem("products", JSON.stringify(productInfo));
   } else {
-    strOfProducts = JSON.stringify(`${products},${productInfo}`);
+    addProductInProducts(name, price, quantity, products);
   }
-
-  localStorage.setItem("products", strOfProducts);
 };
 
 export { addProduct };
