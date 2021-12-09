f3668dfc8f53b97514797b75b50784a4f6240cdf
hozzijeong <ghwls0905@naver.com>
2021-12-09 15:33:31 +0900

    Feat: 사용되는 ID 값들 상수화

---
+++ src/storage/constant.js
@@ -0,0 +1,42 @@
+export const INITIAL_TAB_ID = Object.freeze({
+    PRODUCT_MANAGE_TAB: "product-add-menu",
+    PRODUCT_PURCHASE_TAB: "product-purchase-menu",
+    CHARGE_TAB: "vending-machine-manage-menu",
+});
+
+export const MANAGE_TAB_ID = Object.freeze({
+    NAME_INPUT: "product-name-input",
+    PRICE_INPUT: "product-price-input",
+    QUANTITY_INPUT: "product-quantity-input",
+    ADD_BUTTON: "product-add-button",
+});
+
+export const MANAGE_TAB_CLASS = Object.freeze({
+    EACH_ITEM: "product-manage-item",
+    MANAGE_NAME: "product-manage-name",
+    MANAGE_PRICE: "product-manage-price",
+    MANAGE_QUANTITY: "product-manage-quantity",
+});
+
+export const CHARGE_TAB_ID = Object.freeze({
+    CHARGE_INPUT: "vending-machine-charge-input",
+    CHARGE_BUTTON: "vending-machine-charge-button",
+    CHARGE_AMOUNT: "vending-machine-charge-amount",
+    COIN_500: "vending-machine-coin-500-quantity",
+    COIN_100: "vending-machine-coin-100-quantity",
+    COIN_50: "vending-machine-coin-50-quantity",
+    COIN_10: "vending-machine-coin-10-quantity",
+});
+
+export const PURCHASE_TAB_ID = Object.freeze({
+    CHARGE_INPUT: "charge-input",
+    CHARGE_BUTTON: "charge-button",
+    CHARGE_AMOUNT: "charge-amount",
+    RETURN_BUTTON: "coin-return-button",
+    COIN_500: "coin-500-quantity",
+    COIN_100: "coin-100-quantity",
+    COIN_50: "coin-50-quantity",
+    COIN_10: "coin-10-quantity",
+});
+
+export const PURCHASE_TAB_CLASS = Object.freeze({});
