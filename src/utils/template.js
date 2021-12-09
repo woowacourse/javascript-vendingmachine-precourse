9757ed4bee7cf35840a2243de91c0f0ae50c9cfe
gytks4 <gytks4@naver.com>
2021-12-09 20:16:47 +0900

    refactor: charge section template을 상수로 지정

--- src/utils/template.js
+++ src/utils/template.js
@@ -32,3 +32,35 @@ export function printProductTemplate(product) {
     </tr>
 `;
 }
+
+export const CHARGE_SECTION_TEMPLATE = `
+  <h3>자판기 동전 충전하기</h3>
+  <form>
+    <input type="number" id="vending-machine-charge-input" placeholder="자판기가 보유할 금액" />
+    <button id="vending-machine-charge-button">충전하기</button>
+  </form>
+  <div>보유 금액: <span id="vending-machine-charge-amount"></span> </div>
+  <h3>자판기가 보유한 동전</h3>
+  <table>
+    <tr>
+      <td>동전</td>
+      <td>개수</td>
+    </tr>
+    <tr>
+      <td>500원</td>
+      <td id="vending-machine-coin-500-quantity">0개</td>
+    </tr>
+    <tr>
+      <td>100원</td>
+      <td id="vending-machine-coin-100-quantity">0개</td>
+    </tr>
+    <tr>
+      <td>50원</td>
+      <td id="vending-machine-coin-50-quantity">0개</td>
+    </tr>
+    <tr>
+      <td>10원</td>
+      <td id="vending-machine-coin-10-quantity">0개</td>
+    </tr>
+  </table>
+`;
--- src/view/ChargeView.js
+++ src/view/ChargeView.js
@@ -1,4 +1,5 @@
 import { $ } from '../utils/DOM.js';
+import { CHARGE_SECTION_TEMPLATE } from '../utils/template.js';
 
 export class ChargeView {
   constructor() {
@@ -14,37 +15,7 @@ export class ChargeView {
   }
 
   addElements() {
-    this.$productBalanceSection.innerHTML = `
-      <h3>자판기 동전 충전하기</h3>
-      <form>
-        <input type="number" id="vending-machine-charge-input" placeholder="자판기가 보유할 금액" />
-        <button id="vending-machine-charge-button">충전하기</button>
-      </form>
-      <div>보유 금액: <span id="vending-machine-charge-amount"></span> </div>
-      <h3>자판기가 보유한 동전</h3>
-      <table>
-        <tr>
-          <td>동전</td>
-          <td>개수</td>
-        </tr>
-        <tr>
-          <td>500원</td>
-          <td id="vending-machine-coin-500-quantity">0개</td>
-        </tr>
-        <tr>
-          <td>100원</td>
-          <td id="vending-machine-coin-100-quantity">0개</td>
-        </tr>
-        <tr>
-          <td>50원</td>
-          <td id="vending-machine-coin-50-quantity">0개</td>
-        </tr>
-        <tr>
-          <td>10원</td>
-          <td id="vending-machine-coin-10-quantity">0개</td>
-        </tr>
-      </table>
-    `;
+    this.$productBalanceSection.innerHTML = CHARGE_SECTION_TEMPLATE;
     this.$chargeInput = $('#vending-machine-charge-input');
     this.$chargeButton = $('#vending-machine-charge-button');
     this.$chargeAmount = $('#vending-machine-charge-amount');
