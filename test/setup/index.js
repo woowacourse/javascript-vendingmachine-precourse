64cdbb97c4fc82af3b2eb59fe284b8fc737e213b
sanaandmomo <qajjang46@naver.com>
2021-12-09 18:41:43 +0900

    chore: jest 전역 함수 설정해라

--- test/setup/index.js
+++ test/setup/index.js
@@ -3,5 +3,8 @@ global.MissionUtils = {
     Random: {
         pickNumberInRange: (startInclusive, endInclusive) =>
             Math.floor(Math.random() * (endInclusive + 1 - startInclusive)) + startInclusive,
+        pickNumberInList(array) {
+            return array[this.pickNumberInRange(0, array.length - 1)];
+        },
     },
 };
