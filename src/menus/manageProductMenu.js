7fc15047c6e0fb7c7656aca7c5af686619d05aab
marble <kangsw1025@naver.com>
2021-12-10 00:12:12 +0900

    style: 함수 위치 변경

--- src/menus/manageProductMenu.js
+++ src/menus/manageProductMenu.js
@@ -2,19 +2,6 @@ import { vendingMachine } from "../components/vendingMachine.js";
 import { checkAddProductsInputs } from "../utils/validation.js";
 import { renderProduct } from "../views/manageProductView.js";
 
-export const onClickAddButton = event => {
-  event.preventDefault();
-  const form = event.target.parentElement;
-  const name = form.querySelector("#product-name-input").value;
-  const price = form.querySelector("#product-price-input").value;
-  const quantity = form.querySelector("#product-quantity-input").value;
-
-  if (checkAddProductsInputs(name, price, quantity)) {
-    addProductToVendingMachine(name, price, quantity);
-    resetInputs(form);
-  }
-};
-
 const addProductToVendingMachine = (name, price, quantity) => {
   const newProduct = vendingMachine.addProduct(name, price, quantity);
   renderProduct(newProduct);
@@ -28,4 +15,17 @@ const resetInputs = form => {
   nameInput.value = "";
   priceInput.value = "";
   quantityInput.value = "";
+};
+
+export const onClickAddButton = event => {
+  event.preventDefault();
+  const form = event.target.parentElement;
+  const name = form.querySelector("#product-name-input").value;
+  const price = form.querySelector("#product-price-input").value;
+  const quantity = form.querySelector("#product-quantity-input").value;
+
+  if (checkAddProductsInputs(name, price, quantity)) {
+    addProductToVendingMachine(name, price, quantity);
+    resetInputs(form);
+  }
 };
