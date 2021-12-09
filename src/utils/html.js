84fb1c0556b030588369a13285ca94221c9f3b49
kevin7498 <kevin7498@naver.com>
2021-12-09 15:00:19 +0900

    feat: 표에 해당하는 HTML 추가

--- src/utils/html.js
+++ src/utils/html.js
@@ -13,7 +13,14 @@ export const HTML_OF_PRODUCT_ADD_PART = `
   <input type="number" placeholder="가격" id="product-price-input"></input>
   <input type="number" placeholder="수량" id="product-quantity-input"></input>
   <button id="product-add-button">추가하기</button>
-  <h4>상품 현황</h4>`;
+  <h4>상품 현황</h4>
+  <table border="1px" cellspacing="0" cellpadding="5">
+    <thead>
+      <th>상품명</th>
+      <th>가격</th>
+      <th>수량</th>
+    </thead>
+  </table>`;
 
 // 잔돈 충전 부분
 export const HTML_OF_MACHINE_MANAGE_PART = `
@@ -22,7 +29,13 @@ export const HTML_OF_MACHINE_MANAGE_PART = `
   <button id="vending-machine-charge-button">충전하기</button><br><br>
   <span>보유 금액:</span>
   <span id="vending-machine-charge-amount"></span><br><br>
-  <h4>자판기가 보유한 동전</h4>`;
+  <h4>자판기가 보유한 동전</h4>
+  <table border="1px" cellspacing="0" cellpadding="5">
+    <thead>
+      <th>동전</th>
+      <th>개수</th>
+    </thead>
+  </table>`;
 
 // 상품 구매 부분
 export const HTML_OF_PRODUCT_PURCHASE_PART = `
@@ -32,5 +45,19 @@ export const HTML_OF_PRODUCT_PURCHASE_PART = `
   <span>투입한 금액:</span>
   <span id="charge-amount"></span><br><br>
   <h4>구매할 수 있는 상품 현황</h4>
+  <table border="1px" cellspacing="0" cellpadding="5">
+    <thead>
+      <th>상품명</th>
+      <th>가격</th>
+      <th>수량</th>
+      <th>구매</th>
+    </thead>
+  </table>
   <h4>잔돈</h4>
-  <button id="coin-return-button">반환하기</button><br><br>`;
+  <button id="coin-return-button">반환하기</button><br><br>
+  <table border="1px" cellspacing="0" cellpadding="5">
+    <thead>
+      <th>동전</th>
+      <th>개수</th>
+    </thead>
+  </table>`;
