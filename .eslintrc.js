62c84c1d10a6b5edd5f117ee1fbfd12ce595ac3f
Daye Lee <leeda1801@gmail.com>
2021-12-10 01:56:32 +0900

    chore: use airbnb style eslint for code convention

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb'],
+  rules: {
+    'import/extensions': 'off',
+    'linebreak-style': 0,
+  },
+};
