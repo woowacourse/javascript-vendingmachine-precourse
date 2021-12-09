fdd5e876689942695d1fe7e4ce939532fb71b354
sanaandmomo <qajjang46@naver.com>
2021-12-09 18:42:04 +0900

    test: 동전 랜덤 분배 함수 테스트해라

---
+++ test/distributeCoin.test.js
@@ -0,0 +1,12 @@
+import distributeCoin from '../src/app/asset/util/distributeCoin.js';
+import COIN from '../src/app/asset/constants/COIN.js';
+
+test('distributeCoin()', () => {
+    expect(distributeCoin(10)).toEqual([0, 0, 0, 1]);
+    expect(distributeCoin(100).reduce((m, v, i) => m + v * COIN[i], 0)).toEqual(100);
+    expect(distributeCoin(120).reduce((m, v, i) => m + v * COIN[i], 0)).toEqual(120);
+    expect(distributeCoin(150).reduce((m, v, i) => m + v * COIN[i], 0)).toEqual(150);
+    expect(distributeCoin(700).reduce((m, v, i) => m + v * COIN[i], 0)).toEqual(700);
+    expect(distributeCoin(734).reduce((m, v, i) => m + v * COIN[i], 0)).toEqual(730);
+    expect(distributeCoin(1002).reduce((m, v, i) => m + v * COIN[i], 0)).toEqual(1000);
+});
