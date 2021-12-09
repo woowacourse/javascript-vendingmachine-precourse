8b4d5f587ec1c500f0887c018da30c2426a6cfda
minbr0ther <minbr0ther@naver.com>
2021-12-09 22:45:46 +0900

    refactor: 상품 입력창의 값을 가져오는 함수 추출

--- src/controllers/ProductAddMenu.js
+++ src/controllers/ProductAddMenu.js
@@ -1,4 +1,4 @@
-import { $ } from '../views/DOMUtils.js';
+import { $, default as DOM } from '../views/DOMUtils.js';
 import { default as V } from '../utils/validators.js';
 
 export default class ProductAddMenu {
@@ -16,9 +16,9 @@ export default class ProductAddMenu {
     $('#product-add-button').addEventListener('click', e => {
       e.preventDefault();
 
-      if (!V.isValidProductName($('#product-name-input').value)) return;
-      if (!V.isValidProductPrice($('#product-price-input').value)) return;
-      if (!V.isValidProductQuantity($('#product-quantity-input').value)) return;
+      if (!V.isValidProductName(DOM.getProduct().name)) return;
+      if (!V.isValidProductPrice(DOM.getProduct().price)) return;
+      if (!V.isValidProductQuantity(DOM.getProduct().quantity)) return;
     });
   }
 }
--- src/views/DOMUtils.js
+++ src/views/DOMUtils.js
@@ -4,6 +4,14 @@ const DOMUtils = {
   initElement: selector => {
     $(selector).innerHTML = '';
   },
+
+  getProduct: () => {
+    return {
+      name: $('#product-name-input').value,
+      price: $('#product-price-input').value,
+      quantity: $('#product-quantity-input').value,
+    };
+  },
 };
 
 export default DOMUtils;
