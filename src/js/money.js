156913e22302ef449929f2024a65fdfd7951bbfd
JihyeokUm <75592315+Jihyeok-Um@users.noreply.github.com>
2021-12-10 01:27:31 +0900

    feat: 입력받은 금액이 잘못된 값일 경우 alert로 표시 구현

--- src/js/money.js
+++ src/js/money.js
@@ -5,4 +5,9 @@ import { checkNotNum, checkMenuPriceDivideTen } from './addMenu.js';
 export const addMoney = e => {
   e.preventDefault();
   const money = $('#vending-machine-charge-input').value;
+  if (checkNotNum(money) || checkMenuPriceDivideTen(money)) {
+    window.alert('잘못된 값을 입력하셨습니다.');
+  } else {
+    $('#vending-machine-charge-amount').innerText += ` ${money}`;
+  }
 };
