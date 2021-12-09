8d5fbc54d59120e3d943629292ba92a58eb3c82a
prefer2 <sunho6200@gmail.com>
2021-12-09 15:32:24 +0900

    feat: 자판기 초기화

--- src/Model/Model.js
+++ src/Model/Model.js
@@ -1,1 +1,6 @@
-export default class Model {}
+export default class Model {
+  constructor() {
+    this.product = JSON.parse(localStorage.getItem("product")) || [];
+    this.coin = JSON.parse(localStorage.getItem("coin")) || [];
+  }
+}
