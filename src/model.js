a93bb0b197eb81d6938602cc07c5e225a97b45ff
wonsss <bigdatajang@gmail.com>
2021-12-09 23:49:57 +0900

    feat: Model 클래스 내에 setter와 getter 추가

--- src/model.js
+++ src/model.js
@@ -1,8 +1,40 @@
 export default class VendingModel {
   constructor() {
-    this.productObj = {};
-    this.coinObj = {};
-    this.insertedMoney = 0;
-    this.changesObj = {};
+    this._productObj = {};
+    this._coinObj = {};
+    this._insertedMoney = 0;
+    this._changesObj = {};
+  }
+
+  set productObj(newProductObj) {
+    this._productObj = newProductObj;
+  }
+
+  get productObj() {
+    return this._productObj;
+  }
+
+  set coinObj(newCoinObj) {
+    this._coinObj = newCoinObj;
+  }
+
+  get coinObj() {
+    return this._coinObj;
+  }
+
+  set insertedMoney(newInsertedMoney) {
+    this._insertedMoney = newInsertedMoney;
+  }
+
+  get insertedMoney() {
+    return this._insertedMoney;
+  }
+
+  set changesObj(newChangesObj) {
+    this._changesObj = newChangesObj;
+  }
+
+  get changesObj() {
+    return this._changesObj;
   }
 }
