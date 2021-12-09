790a0881d3bd13182949b459a53953a1a1e0cf38
simyeen <simyeen@naver.com>
2021-12-09 19:17:18 +0900

    Add : SELETOR들 const.js에 추가

---
+++ src/common/const.js
@@ -0,0 +1,61 @@
+export const SELETOR = {
+  TAB_NAME: {
+    PRDUCT_PURCHASE_MEMU: "product-purchase-menu",
+    VENDING_MACHINE_MANAGE_MEMU: "vending-machine-manage-menu",
+    PRODUCT_ADD_MEMU: "product-add-menu",
+  },
+
+  PRODUCT_ADD: {
+    ID: {
+      PRDUCT_NAME_INPUT: "product-name-input",
+      PRDUCT_PRICE_INPUT: "product-price-input",
+      PRDUCT_QUANTITY_INPUT: "product-quantity-input",
+      PRDUCT_ADD_BUTTON: " product-add-button",
+    },
+
+    CALSS: {
+      PRDUCT_MANAGE_Item: "product-manage-item",
+      PRDUCT_MANAGE_Name: "product-manage-name",
+      PRDUCT_MANAGE_Price: "product-manage-price",
+      PRDUCT_MANAGE_Quantity: "product-manage-quantity",
+    },
+  },
+
+  VENDING_MACHINE_MANAGE: {
+    ID: {
+      VENDING_MACHINE_CHARGE_INPUT: "vending-machine-charge-input",
+      VENDING_MACHINE_CHARGE_BUTTON: "vending-machine-charge-button",
+      VENDING_MACHINE_CHARGE_AMOUNT: "vending-machine-charge-amount ",
+
+      VENDING_MACHINE_COIN_500_QUANTITY: "vending-machine-coin-500-quantity",
+      VENDING_MACHINE_COIN_100_QUANTITY: "vending-machine-coin-100-quantity",
+      VENDING_MACHINE_COIN_50_QUANTITY: "vending-machine-coin-50-quantity",
+      VENDING_MACHINE_COIN_10_QUANTITY: "vending-machine-coin-10-quantity",
+    },
+  },
+
+  PRDUCT_PURCHASE: {
+    ID: {
+      CHARGE_INPUT: "charge-input",
+      CHARGE_BUTTON: "charge-button",
+      CHARGE_AMOUNT: "charge-amount",
+      COIN_RETURN_BUTTON: "coin-return-button",
+    },
+
+    CLASS: {
+      COIN_500_QUANTITY: "coin-500-quantity",
+      COIN_500_QUANTITY: "coin-100-quantity",
+      COIN_500_QUANTITY: "coin-50-quantity",
+      COIN_500_QUANTITY: "coin-10-quantity",
+
+      PRDUCT_PURCHASE_ITEM: "product-purchase-item",
+      PURCHASE_BUTTON: "purchase-button",
+      PRDUCT_PURCHASE_NAME: "product-purchase-name",
+      PRDUCT_PURCHASE_PRICE: "product-purchase-price",
+      PRDUCT_PURCHASE_QUANTITY: "product-purchase-quantity",
+      DATA_PRODUCT_NAME: "data-product-name ",
+      DATA_PRODUCT_PRICE: "data-product-price ",
+      DATA_PRODUCT_QUANTITY: "data-product-quantity",
+    },
+  },
+};
