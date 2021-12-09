ac86f698bf42ed8c86e586c191e4a31441462851
sanaandmomo <qajjang46@naver.com>
2021-12-09 19:48:57 +0900

    fix: 충전 금액 검증 함수 고쳐라

--- src/app/asset/constants/ERROR_MSG.js
+++ src/app/asset/constants/ERROR_MSG.js
@@ -5,6 +5,7 @@ const ERROR_MSG = {
     requireProductName: `${INPUT_ITEM.productName}을 입력해주세요.`,
     requireProductPrice: `${INPUT_ITEM.productPrice}을 입력해주세요.`,
     requireProductQuantity: `${INPUT_ITEM.productQuantity}을 입력해주세요.`,
+    requireChargeCoin: `${INPUT_ITEM.chargeCoin}을 입력해주세요.`,
     naturalNumber: '양수로 입력해주세요.',
     number: '숫자로만 입력해주세요.',
     minPrice: `상품 가격은 ${INPUT_NUMBER_RULE.minPrice}이상만 가능합니다.`,
--- src/app/asset/validation/checkChargeAmount.js
+++ src/app/asset/validation/checkChargeAmount.js
@@ -1,5 +1,16 @@
-import checkProductQuantity from './checkProductQuantity.js';
+import {
+    createValidFunction,
+    createCheckEveryFunction,
+    isRequired,
+    checkNumber,
+    checkNaturalNumber,
+} from './index.js';
+import ERROR_MSG from '../constants/ERROR_MSG.js';
 
-const checkChargeAmount = checkProductQuantity;
+const checkRequired = createValidFunction(isRequired, ERROR_MSG.requireChargeCoin);
 
-export default checkChargeAmount;
+const checkFunctions = [checkRequired, checkNumber, checkNaturalNumber];
+
+const checkProductQuantity = createCheckEveryFunction(checkFunctions);
+
+export default checkProductQuantity;
