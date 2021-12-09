3822dc898ba740bdf4e2043fc6ecabff2a565c54
ellynhan <wown252@naver.com>
2021-12-10 00:52:04 +0900

    refactor: utils로 상수와 선택자 추가

---
+++ src/utils/constant.js
@@ -0,0 +1,79 @@
+export const TITLE = '🥤자판기🥤';
+
+export const TAB = {
+  TEXT_PRODUCT_MANAGE: '상품 관리',
+  TEXT_CHARGE: '잔돈 충전',
+  TEXT_PURCHASE: '상품 구매',
+  ID_PRODUCT_MANAGE: 'product-add-menu',
+  ID_CHARGE: 'vending-machine-manage-menu',
+  ID_PURCHASE: 'product-purchase-menu',
+  MARGIN: '4px',
+};
+
+export const SUBTITLE = {
+  ADD_PRODUCT: '상품 추가하기',
+  CURRENT_PRODUCT: '상품 현황',
+  CHARGE: '자판기 동전 충전하기',
+  CURRENT_COIN: '자판기가 보유한 동전',
+  INSERT_COIN: '금액 투입',
+  CAN_BUY_PRODUCT: '구매할 수 있는 상품 현황',
+  LEFT_COIN: '잔돈',
+};
+
+export const ID = {
+  INPUT_PRODUCT_NAME: 'product-name-input',
+  INPUT_PRODUCT_PRICE: 'product-price-input',
+  INPUT_PRODUCT_QUANTITY: 'product-quantity-input',
+  INPUT_CHARGE_COIN: 'vending-machine-charge-input',
+  INPUT_PURCHASE_COIN: 'charge-input',
+  BUTTON_ADD_PRODUCT: 'product-add-button',
+  BUTTON_CHARGE: 'vending-machine-charge-button',
+  BUTTON_PURCHASE_CHARGE: 'charge-button',
+  BUTTON_RETURN: 'coin-return-button',
+  TEXT_CHARGED_COIN: 'vending-machine-charge-amount',
+  TEXT_PURCHASE_COIN: 'charge-amount',
+  TABLE_CURRENT_500: 'vending-machine-coin-500-quantity',
+  TABLE_CURRENT_100: 'vending-machine-coin-100-quantity',
+  TABLE_CURRENT_50: 'vending-machine-coin-50-quantity',
+  TABLE_CURRENT_10: 'vending-machine-coin-10-quantity',
+  TABLE_RETURN_500: 'coin-500-quantity',
+  TABLE_RETURN_100: 'coin-100-quantity',
+  TABLE_RETURN_50: 'coin-50-quantity',
+  TABLE_RETURN_10: 'coin-10-quantity',
+};
+
+export const CLASS = {
+  PRODUCT_ITEM: 'product-manage-item',
+  PRODUCT_NAME: 'product-manage-name',
+  PRODUCT_PRICE: 'product-manage-price',
+  PRODUCT_QUANTITY: 'product-manage-quantity',
+  PURCHASE_ITEM: 'product-purchase-item',
+  PURCHASE_BUTTON: 'purchase-button',
+  PURCHASE_NAME: 'product-purchase-name',
+  PURCHASE_PRICE: 'product-purchase-price',
+  PURCHASE_QUANTITY: 'product-purchase-quantity',
+  DATA_PRODUCT_NAME: 'data-product-name',
+  DATA_PRODUCT_PRICE: 'data-product-price',
+  DATA_PRODUCT_QUANTITY: 'data-product-quantity',
+};
+
+export const TEXT = {
+  ADD: '추가하기',
+  CHARGE: '충전하기',
+  HAVE: '보유 금액',
+  INSERT: '투입 하기',
+  RETURN: '반환하기',
+};
+
+export const LABEL = {
+  PRODUCT_NAME: '상품명',
+  PRICE: '가격',
+  QUANTITY_PRODUCT: '수량',
+  QUANTITY_COIN: '개수',
+  COIN: '동전',
+  PURCHASE: '구매',
+  500: '500원',
+  100: '100원',
+  50: '50원',
+  10: '10원',
+};
