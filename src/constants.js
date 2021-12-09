1787f6f563d3b16f9a15b8d22ded3662cf73532b
정현수 <jung660317@naver.com>
2021-12-09 21:51:34 +0900

    chore: 탭 이름 상수 추가
    - 탭 이름을 나타내는 상수 추가

--- src/constants.js
+++ src/constants.js
@@ -45,17 +45,21 @@ export const SELECTOR = {
   dataProductQuantityDataset: 'data-product-quantity',
 };
 
+export const PRODUCT_ADD_TAB = '상품 관리';
+export const VENDING_MACHINE_MANAGE_TAB = '잔돈 충전';
+export const PRODUCT_PURCHASE_TAB = '상품 구매';
+
 export const TAB_BUTTONS = [
   {
-    text: '상품 관리',
+    text: PRODUCT_ADD_TAB,
     id: SELECTOR.productAddMenuId,
   },
   {
-    text: '잔돈 충전',
-    id: SELECTOR.productPurchaseMenuId,
+    text: VENDING_MACHINE_MANAGE_TAB,
+    id: SELECTOR.vendingMachineManageMenuId,
   },
   {
-    text: '상품 구매',
+    text: PRODUCT_PURCHASE_TAB,
     id: SELECTOR.productPurchaseMenuId,
   },
 ];
