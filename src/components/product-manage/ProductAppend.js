60bd5be10af5c81346c1e44e237729f3e9d70edc
2doo <2doo@kakao.com>
2021-12-11 00:34:31 +0900

    feat(components/product-manage): ProductAppend 구현

---
+++ src/components/product-manage/ProductAppend.js
@@ -0,0 +1,66 @@
+import { ELEMENT_ID } from '../../constants/index.js';
+import { parseString, parseNumber } from '../../utils/inputParser.js';
+import Button from '../base/Button.js';
+import Component from '../base/Component.js';
+import Input from '../base/Input.js';
+import Label from '../base/Label.js';
+
+const inputPropsList = [
+  {
+    id: ELEMENT_ID.PRODUCT_MANAGE_NAME_INPUT,
+    placeholder: '상품명',
+  },
+  {
+    id: ELEMENT_ID.PRODUCT_MANAGE_PRICE_INPUT,
+    placeholder: '가격',
+    type: 'number',
+  },
+  {
+    id: ELEMENT_ID.PRODUCT_MANAGE_QUANTITY_INPUT,
+    placeholder: '수량',
+    type: 'number',
+  },
+];
+
+class ProductAppend extends Component {
+  $title;
+
+  $inputs;
+
+  $submit;
+
+  onSubmit;
+
+  constructor() {
+    super(document.createElement('div'));
+
+    this.$title = new Label('h3', '상품 추가하기');
+    this.$inputs = inputPropsList.map((prop) => new Input(prop));
+    this.$submit = new Button('추가하기', {
+      id: ELEMENT_ID.PRODUCT_MANAGE_ADD_BUTTON,
+    });
+    this.setEvent();
+  }
+
+  resetInputs() {
+    this.$inputs.forEach(($input) => {
+      $input.resetValue();
+    });
+  }
+
+  setEvent() {
+    this.$submit.addOnClick(() => {
+      this.onSubmit?.({
+        name: parseString(this.$inputs[0].value),
+        price: parseNumber(this.$inputs[1].value),
+        count: parseNumber(this.$inputs[2].value),
+      });
+    });
+  }
+
+  render() {
+    this.renderChildrenView([this.$title, ...this.$inputs, this.$submit]);
+  }
+}
+
+export default ProductAppend;
--- src/components/product-manage/index.js
+++ src/components/product-manage/index.js
@@ -1,12 +1,26 @@
+import VendingMachineStore from '../../store/vendingMachineStore.js';
 import Component from '../base/Component.js';
+import ProductAppend from './ProductAppend.js';
 
 class ProductManage extends Component {
+  $append;
+
   constructor() {
     super(document.createElement('div'));
+
+    this.$append = new ProductAppend();
+    this.setEvent();
+  }
+
+  setEvent() {
+    this.$append.onSubmit = (product) => {
+      VendingMachineStore.instance.addProduct(product);
+      this.$append.resetInputs();
+    };
   }
 
   render() {
-    this.renderText('TODO: 상품 관리');
+    this.renderChildrenView([this.$append]);
   }
 }
 
