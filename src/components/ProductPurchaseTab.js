c76eec31241bda376273dfacb87cca269dcc7af1
정현수 <jung660317@naver.com>
2021-12-10 21:42:08 +0900

    feat: 금액 투입 form 구현
    - 금액 투입 form을 생성하는 createChargeFormTemplate 함수를 구현
    - ProductPurchaseTab class의 render 함수에 추가

--- src/components/ProductPurchaseTab.js
+++ src/components/ProductPurchaseTab.js
@@ -1,12 +1,11 @@
 import { $ } from '../utils/dom.js';
-import { createTitle } from '../templates/productPurchaseMenu.js';
+import { createTitleTemplate, createChargeFormTemplate } from '../templates/productPurchaseMenu.js';
 import { SELECTOR } from '../constants.js';
 
 class ProductPurchaseTab {
   render() {
-    const title = createTitle();
-
-    $(`#${SELECTOR.tabContentContainerId}`).innerHTML = title;
+    $(`#${SELECTOR.tabContentContainerId}`).innerHTML =
+      createTitleTemplate() + createChargeFormTemplate(0);
   }
 }
 
--- src/templates/productPurchaseMenu.js
+++ src/templates/productPurchaseMenu.js
@@ -1,5 +1,18 @@
-import { PRODUCT_PURCHASE_TAB } from '../constants.js';
+import { PRODUCT_PURCHASE_TAB, SELECTOR } from '../constants.js';
 
-export const createTitle = () => `
+export const createTitleTemplate = () => `
   <h2>${PRODUCT_PURCHASE_TAB}</h2>
+`;
+
+export const createChargeFormTemplate = amount => `
+  <h3>금액 투입</h3>
+  <div>
+    <input id="${SELECTOR.chargeInputId}" />
+    <button id="${SELECTOR.chargeButtonId}">투입하기</button>
+  </div>
+  <br />
+  <div>
+    <span>투입한 금액:</span>
+    <span id="${SELECTOR.chargeAmountId}">${amount}<span>원</span></span>
+  </div>
 `;
