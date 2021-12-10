a5562166f76b40d38f797b507041ef87830a3d83
sanaandmomo <qajjang46@naver.com>
2021-12-10 19:40:21 +0900

    refactor: 컴포넌트 한 곳에 모아둬라

---
+++ src/app/asset/components/index.js
@@ -0,0 +1,31 @@
+import MainTitle from './MainTitle/index.js';
+import InputNumber from './Input/InputNumber.js';
+import ButtonById from './Button/ButtonById.js';
+import CellById from './Cell/CellById.js';
+import SubmitForm from './SubmitForm/index.js';
+import SubTitle from './SubTitle/index.js';
+import CoinTable from './CoinTable/index.js';
+import Span from './Span/index.js';
+import InputText from './Input/InputText.js';
+import Table from './Table/index.js';
+import InventoryHeadRow from './Row/InventoryHeadRow.js';
+import InventoryBodyRow from './Row/InventoryBodyRow.js';
+import PurchaseHeadRow from './Row/PurchaseHeadRow.js';
+import PurchaseBodyRow from './Row/PurchaseBodyRow.js';
+
+export {
+    MainTitle,
+    InputNumber,
+    ButtonById,
+    CellById,
+    SubmitForm,
+    SubTitle,
+    CoinTable,
+    Span,
+    InputText,
+    Table,
+    InventoryHeadRow,
+    InventoryBodyRow,
+    PurchaseHeadRow,
+    PurchaseBodyRow,
+};
