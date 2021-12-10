b632bc65d7c27ae52e3e9498298e46848bfa5d2c
SoYi Jeon <soyijeon47@gmail.com>
2021-12-11 02:15:43 +0900

    feat: 상품 구매 - 투입 금액 충전 기능 및 충전 시 투입 금액 출력 업데이트

--- src/ProductPurchaseTab.js
+++ src/ProductPurchaseTab.js
@@ -1,5 +1,5 @@
 import ProductPurchaseTabView from './views/ProductPurchaseTabView.js';
-import { getCharge, getProducts } from './utils/localStorage.js';
+import { getCharge, getProducts, setCharge } from './utils/localStorage.js';
 
 export default class ProductPurchaseTab {
   constructor() {
@@ -10,5 +10,29 @@ export default class ProductPurchaseTab {
     this.charge = getCharge();
     this.products = getProducts();
     this.view.render(this.charge, this.products);
+    this.initInputElements();
+    this.setButtonClickEvent();
+  }
+
+  initInputElements() {
+    this.chargeInput = document.querySelector('#charge-input');
+  }
+
+  setButtonClickEvent() {
+    const chargeButton = document.querySelector('#charge-button');
+    chargeButton.addEventListener('click', this.onClickChargeButton.bind(this));
+  }
+
+  onClickChargeButton(e) {
+    e.preventDefault();
+    const amountToBeCharged = Number(this.chargeInput.value);
+    this.charge += amountToBeCharged;
+    setCharge(this.charge);
+    this.view.renderChargeAmount(this.charge);
+    this.clearInputValue();
+  }
+
+  clearInputValue() {
+    this.chargeInput.value = '';
   }
 }
