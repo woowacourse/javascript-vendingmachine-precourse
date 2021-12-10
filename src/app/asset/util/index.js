d84ea24b07894a54dff03fd7e4b4d04def1c319c
sanaandmomo <qajjang46@naver.com>
2021-12-10 20:14:00 +0900

    refactor: 유틸 함수 한 곳에 모아놔라

---
+++ src/app/asset/util/index.js
@@ -0,0 +1,6 @@
+import calcCoinAmount from './calcCoinAmount.js';
+import distributeCoin from './distributeCoin.js';
+import distributeCoinGreedily from './distributeCoinGreedily.js';
+import getEnableChargeAmount from './getEnableChargeAmount.js';
+
+export { calcCoinAmount, distributeCoin, distributeCoinGreedily, getEnableChargeAmount };
