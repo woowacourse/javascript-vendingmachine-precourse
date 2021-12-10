298f115903c35c4aab7f46477af690489d567fed
2013250433 <shiplight@korea.ac.kr>
2021-12-10 16:15:02 +0900

    feat: 버튼 클릭 시, 해당 탭을 이동

--- src/index.js
+++ src/index.js
@@ -1,14 +1,52 @@
 import elementCreator from './dom/util.js'
 
+const addButtonId = 'product-add-menu';
+const manageButtonId = 'vending-machine-manage-menu';
+const purchaseButtonId = 'product-purchase-menu';
+
 const app = document.getElementById('app');
 
 app.append(elementCreator('h1','title','🥤 자판기 🥤'));
 
 const buttons = elementCreator('span','buttons','');
 
-const addButton = elementCreator('button','product-add-menu','상품 관리');
-const manageButton = elementCreator('button','vending-machine-manage-menu','잔돈 충전');
-const purchaseButton = elementCreator('button','product-purchase-menu','상품 구매');
+const addButton = elementCreator('button', addButtonId, '상품 관리');
+const manageButton = elementCreator('button', manageButtonId,'잔돈 충전');
+const purchaseButton = elementCreator('button', purchaseButtonId,'상품 구매');
 
 buttons.append(addButton, manageButton, purchaseButton);
 app.append(buttons);
+
+// 탭 구현
+const addTabId = 'add-menu';
+const manageTabId = 'manage-menu';
+const purchaseTabId = 'purchase-menu';
+
+const addTab = elementCreator('div', addTabId, 'addTab');
+const manageTab = elementCreator('div', manageTabId, 'manageTab');
+const purchaseTab = elementCreator('div', purchaseTabId, 'purchaseTab');
+
+// 처음화면, addTab
+manageTab.hidden = true;
+purchaseTab.hidden = true;
+
+app.append(addTab,manageTab,purchaseTab);
+
+// 탭 버튼 이벤트 리스너
+let tabs = {};
+tabs[addButtonId] = addTab;
+tabs[manageButtonId] = manageTab;
+tabs[purchaseButtonId] = purchaseTab; 
+
+buttons.addEventListener('click', e =>{
+    e.preventDefault();
+    const targetButton = e.target;
+    console.log(targetButton);
+    for(let buttonId in tabs){
+        if(buttonId === targetButton.id){
+            tabs[buttonId].hidden = false;
+        }else {
+            tabs[buttonId].hidden = true;
+        }
+    };
+});
