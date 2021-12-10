72f8447ab45fddc9cf22702ec45e99355a245d2f
minbr0ther <minbr0ther@naver.com>
2021-12-10 09:48:40 +0900

    feat: 잔돈 충전 탭, 상품 구매 탭 임시 생성

--- src/views/View.js
+++ src/views/View.js
@@ -1,14 +1,27 @@
 import { default as UI } from './templates.js';
-import { $, default as DOM } from './DOMUtils.js';
+import { $ } from './DOMUtils.js';
 
 export default class View {
   constructor() {
     $('#app').insertAdjacentHTML('beforeend', UI.commonHTML);
+    this.makeComponents();
+  }
+
+  makeComponents() {
+    this.showProductAddComponent();
+    this.showVendingMachineManageComponent();
+    this.showProductPurchaseComponent();
   }
 
   showProductAddComponent() {
-    DOM.initElement('#component');
+    $('#app').insertAdjacentHTML('beforeend', UI.productAddHTML);
+  }
 
-    $('#component').insertAdjacentHTML('beforeend', UI.productAddHTML);
+  showVendingMachineManageComponent() {
+    $('#app').insertAdjacentHTML('beforeend', UI.vendingMachineManageHTML);
+  }
+
+  showProductPurchaseComponent() {
+    $('#app').insertAdjacentHTML('beforeend', UI.productPurchaseHTML);
   }
 }
--- src/views/templates.js
+++ src/views/templates.js
@@ -5,29 +5,40 @@ const templates = {
         <button id="product-add-menu">상품 관리</button>
         <button id="vending-machine-manage-menu">잔돈 충전</button>
         <button id="product-purchase-menu">상품 구매</button>
-    </nav>
-    <div id="component"></div>`,
+    </nav>`,
 
-  productAddHTML: ` 
-    <h3>상품 추가하기</h3>
-    <form>
-        <input type="text" id="product-name-input" placeholder="상품명" />
-        <input type="number" id="product-price-input" placeholder="가격" />
-        <input type="number" id="product-quantity-input" placeholder="수량" />
-        <button id="product-add-button">추가하기</button>
-    </form>
-    <h3>상품 현황</h3>
-    <table border="1" style="width:400px; border-collapse: collapse;">
-        <thead>
-            <tr>
-                <th style="padding:10px;">상품명</th>
-                <th style="padding:10px;">가격</th>
-                <th style="padding:10px;">수량</th>
-            </tr>
-        </thead>
-        <tbody id="product-inventory">
-        </tbody>
-    </table>`,
+  productAddHTML: `
+    <div id="product-add-component">
+        <h3>상품 추가하기</h3>
+        <form>
+            <input type="text" id="product-name-input" placeholder="상품명" />
+            <input type="number" id="product-price-input" placeholder="가격" />
+            <input type="number" id="product-quantity-input" placeholder="수량" />
+            <button id="product-add-button">추가하기</button>
+        </form>
+        <h3>상품 현황</h3>
+        <table border="1" style="width:400px; border-collapse: collapse;">
+            <thead>
+                <tr>
+                    <th style="padding:10px;">상품명</th>
+                    <th style="padding:10px;">가격</th>
+                    <th style="padding:10px;">수량</th>
+                </tr>
+            </thead>
+            <tbody id="product-inventory">
+            </tbody>
+        </table>
+    </div>`,
+
+  vendingMachineManageHTML: `
+    <div id="vending-machine-manage-component">
+        <h3>자판기 동전 충전하기</h3>
+    </div>`,
+
+  productPurchaseHTML: `
+      <div id="product-purchase-component">
+          <h3>금액 투입</h3>
+      </div>`,
 };
 
 export default templates;
