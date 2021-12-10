4f2ea556841a01fd48f3807f2ac19d9da1964d43
minbr0ther <minbr0ther@naver.com>
2021-12-10 19:05:27 +0900

    feat: 자판기가 보유할 금액만큼의 동전이 무작위로 생성된다

--- src/utils/convertChargeIntoCoin.js
+++ src/utils/convertChargeIntoCoin.js
@@ -1,9 +1,27 @@
+/* eslint-disable no-undef */
 import { default as DB } from '../model/database.js';
 
 const convertChargeIntoCoin = charge => {
-  const wallet = DB.load('vendingMachineCharge')[0];
-  console.log(wallet);
-  //while()
+  const coinWallet = DB.load('vendingMachineCharge')[0];
+
+  console.log(updateCoinWallet(charge, coinWallet));
+};
+
+const updateCoinWallet = (charge, coinWallet) => {
+  const addRandomCoin = array => {
+    const number = MissionUtils.Random.pickNumberInList(array);
+    coinWallet['coin' + number] += 1;
+    charge -= number;
+  };
+
+  while (charge > 0) {
+    if (charge >= 500) addRandomCoin([500, 100, 50, 10]);
+    if (charge >= 100) addRandomCoin([100, 50, 10]);
+    if (charge >= 50) addRandomCoin([50, 10]);
+    if (charge >= 10) addRandomCoin([10]);
+  }
+
+  return coinWallet;
 };
 
 export default convertChargeIntoCoin;
--- src/utils/generateCoins.js
+++
@@ -1,9 +0,0 @@
-import { default as DB } from '../model/database.js';
-
-const generateCoins = charge => {
-  const wallet = DB.load('vendingMachineCharge');
-  console.log(wallet);
-  //while()
-};
-
-export default generateCoins;
