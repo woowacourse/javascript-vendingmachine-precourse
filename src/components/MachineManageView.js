b6abc4f2e861f4602cac9a8380fe0e7566e899c4
kevin7498 <kevin7498@naver.com>
2021-12-10 14:52:14 +0900

    feat: 동전을 랜덤으로 생성하는 함수 'chooseRandomCoin' 구현

--- src/components/MachineManageView.js
+++ src/components/MachineManageView.js
@@ -1,4 +1,4 @@
-import { CHANGE, ERROR_MESSAGE, VALUES } from '../utils/constants.js';
+import { CHANGE, ERROR_MESSAGE, FIFTY, FIVE_HUNDRED, ONE_HUNDRED, TEN, VALUES } from '../utils/constants.js';
 import { HTML_OF_MACHINE_MANAGE_PART, HTML_OF_MACHINE_MANAGE_TABLE } from '../utils/html.js';
 import MachineManageCheck from './MachineManageCheck.js';
 
@@ -18,6 +18,7 @@ export default class MachineManageView {
         const machineManageCheck = new MachineManageCheck(charge);
         if(machineManageCheck.checkAll()) {
             this.addChange(charge);
+            this.chooseRandomCoin(charge);
         } else {
             alert(ERROR_MESSAGE);
         }
@@ -39,10 +40,42 @@ export default class MachineManageView {
   }
 
   static showFirstChange(charge) {
-    document.getElementById('vending-machine-charge-amount').innerHTML = charge;
+    document.getElementById('vending-machine-charge-amount').innerHTML = `${charge}원`;
   }
 
   static showChange(change) {
-    document.getElementById('vending-machine-charge-amount').innerHTML = change[VALUES];
+    document.getElementById('vending-machine-charge-amount').innerHTML = `${change[VALUES]}원`;
+  }
+
+  static chooseRandomCoin(charge) {
+      let remain = charge;
+
+      while(remain >= 0){
+        const coin = MissionUtils.Random.pickNumberInList(this.check(remain));
+
+        console.log("남은돈:", remain);
+        console.log("뽑은돈:", coin);
+        remain -= coin;
+        if(remain === 0) {
+            break;
+        }
+      }
+      console.log(remain);
+  }
+
+  static check(remain) {
+    if(remain < FIFTY) {
+        return [TEN];
+    } else if(remain < ONE_HUNDRED) {
+        return [TEN, FIFTY];
+    } else if(remain < FIVE_HUNDRED) {
+        return [TEN, FIFTY, ONE_HUNDRED];
+    } else {
+        return [TEN, FIFTY, ONE_HUNDRED, FIVE_HUNDRED];
+    }
+  }
+
+  static storeRandomCoin() {
+
   }
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,13 @@ export const MIN_PRODUCT_PRICE = 100;
 export const MIN_PRODUCT_QUANTITY = 1;
 export const DIVISOR_NUMBER = 10;
 
+export const FIVE_HUNDRED = 500;
+export const ONE_HUNDRED = 100;
+export const FIFTY = 50;
+export const TEN = 10;
+
+
+
 export const PRODUCT = 'Product';
 export const VALUES = 'values';
 export const CHANGE = 'Change';
