8a310137b555018955429729f3352a4db53a16de
marble <kangsw1025@naver.com>
2021-12-10 20:19:31 +0900

    refactor: 함수 분리

--- src/menus/manageProductMenu.js
+++ src/menus/manageProductMenu.js
@@ -2,11 +2,6 @@ import { vendingMachine } from "../components/vendingMachine.js";
 import { checkAddProductsInputs } from "../utils/validation.js";
 import { renderProduct } from "../views/manageProductView.js";
 
-const addProductToVendingMachine = (name, price, quantity) => {
-  const newProduct = vendingMachine.addProduct(name, price, quantity);
-  renderProduct(newProduct);
-};
-
 const resetInputs = form => {
   const nameInput = form.querySelector("#product-name-input");
   const priceInput = form.querySelector("#product-price-input");
@@ -25,7 +20,8 @@ export const onClickAddButton = event => {
   const quantity = form.querySelector("#product-quantity-input").value;
 
   if (checkAddProductsInputs(name, price, quantity)) {
-    addProductToVendingMachine(name, price, quantity);
+    const newProduct = vendingMachine.addProduct(name, price, quantity);
+    renderProduct(newProduct);
     resetInputs(form);
   }
 };
