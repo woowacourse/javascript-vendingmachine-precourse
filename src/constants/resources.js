4cd2883f4cb70becf996964f724e0e17e2820821
NE5054 <w_lan123@naver.com>
2021-12-10 20:05:37 +0900

    feat: 필요 상수 분리

---
+++ src/constants/resources.js
@@ -0,0 +1,24 @@
+export const HEADER_TITLE = '🥤자판기🥤';
+export const PRODUCT_ADD_HEAD = '상품 추가하기';
+export const PRODUCT_STATUS = '상품 현황';
+
+export const VENDING_MACHINE_MANAGE_HEAD = '자판기 동전 충전하기';
+export const VENDING_MACHINE_MANAGE_NOW = '보유 금액: ';
+export const VENDING_MACHINE_MANAGE_STATUS = '자판기가 보유한 동전';
+
+export const PURCHASE_PRODUCT_HEAD = '금액 투입';
+export const PURCHASE_PRODUCT_NOW = '투입한 금액: ';
+export const PURCHASE_PRODUCT_STATUS = '구매할 수 있는 상품 현황';
+export const PURCHASE_PRODUCT_CHANGE_STATUS = '잔돈';
+
+export const ERROR_MESSAGE = {
+    'PRODUCT-NAME-INPUT-ERROR':
+        '상품이름은 1~255 길이의 이름을 입력해야 합니다.',
+    'PRODUCT-PRICE-INPUT-ERROR':
+        '상품 가격은 100원 이상 10원 단위 가격을 입력해야 합니다.',
+    'PRODUCT-QUANTITY-INPUT-ERROR': '상품 수량은 음수를 입력할 수 없습니다.',
+    'VENDING-MACHINE-CHARGE-INPUT-ERROR':
+        '충전 금액은 10원 단위로 입력해야 합니다.',
+    'CHARGE-INPUT-ERROR': '투입 금액은 10원 단위로 입력해야 합니다.',
+    'INSUFFICIENT-AMOUNT-ERROR': '상품을 구매할 금액이 부족합니다.',
+};
