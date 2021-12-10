0652c2a8976dc5236d2fa8fe379ad792291f42d9
DomMorello <zuzudnf@gmail.com>
2021-12-10 14:21:53 +0900

    feat: 상품가격 100이상 10배수인지 검사

--- src/validator/Validator.js
+++ src/validator/Validator.js
@@ -2,10 +2,20 @@ function isNotEmptyName(name) {
   return name.trim() !== '';
 }
 
+function isAbove100(price) {
+  return +price >= 100;
+}
+
+function isMultipleOf10(price) {
+  return +price % 10 === 0;
+}
+
+function isValidPrice(price) {
+  return isAbove100(price) && isMultipleOf10(price);
+}
+
 export default class Validator {
   static isValidAddInput({ name, price, quantity }) {
-    if (isNotEmptyName(name)) {
-      return true;
-    }
+    return isNotEmptyName(name) && isValidPrice(price);
   }
 }
