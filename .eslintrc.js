d92e8cf7569e5410bf0e5590b15aac0c9faa3876
mangming2 <59263564+mangming2@users.noreply.github.com>
2021-12-11 01:00:02 +0900

    [Feature]: eslint, prettier
    prettier과 eslint 적용

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+      ecmaVersion: 2015,
+      sourceType: 'module',
+    },
+    rules: {
+      'no-new': 'off',
+      'no-console': 'off',
+      'no-alert': 'off',
+      'no-undef': 'off',
+      'no-plusplus': 'off',
+      'no-param-reassign': 'off',
+      'no-return-assign': 'off',
+      'import/extensions': 'off',
+      'max-depth': ['error', 2],
+    },
+  };
