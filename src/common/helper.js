7ed63a8d0cc6a366eb02b6fddf9dd2e09ff0483d
InSeongSo <goflvhxj2547@gmail.com>
2021-12-11 00:32:31 +0900

    🗃Refactor: 함수 수정
    1. Main 컴포넌트의 클래스 메서드를 외부 함수 선언문으로 분리한다.
    2. 불필요하게 첨부되어 있던 applyProduct 메서드를 헬퍼 함수로 분리한다.
    3. 헬퍼 함수에 주석을 추가한다.
    4. Storage에 헬퍼 함수를 적용한다.

--- src/common/helper.js
+++ src/common/helper.js
@@ -14,17 +14,30 @@ import {
 const { pickNumberInList } = MissionUtils.Random;
 
 /**
- * 값이 null인지 검사합니다.
+ * DOM 객체를 하나 선택합니다.
  *
- * @param {string|number} value
- * @returns {boolean}
+ * @param {string} selector
+ * @param {HTMLElement | Document} target
+ * @returns {Node}
  */
-export const isNull = value => value === null || value === undefined;
-
 export const $ = (selector, target = document) => target.querySelector(selector);
 
+/**
+ * DOM 객체를 여러 개 선택합니다.
+ *
+ * @param {string} selector
+ * @param {HTMLElement | Document} target
+ * @returns {NodeList}
+ */
 export const $all = (selector, target = document) => target.querySelectorAll(selector);
 
+/**
+ * 타겟의 형제 DOM 객체를 선택합니다.
+ *
+ * @param {string} selector
+ * @param {HTMLElement | Document} target
+ * @returns {Node}
+ */
 export const $closest = (element, selector) => {
   if (element.nodeType === 9) return false;
 
@@ -42,12 +55,21 @@ export const $closest = (element, selector) => {
  * alert에 출력할 에러 메세지를 설정합니다.
  *
  * @param {string} type
- * @returns {undefined} : Falsy
+ * @param {string} description
+ * @returns {string}
  */
 export const setErrorMessage = (type, description = '') => {
   alert(`${description}${ERROR_MESSAGES[type]}`);
   return EMPTY;
 };
+
+/**
+ * 값이 null인지 검사합니다.
+ *
+ * @param {string|number} value
+ * @returns {boolean}
+ */
+export const isNull = value => value === null || value === undefined;
 
 /**
  * 첫 번째 인자와 두 번째 인자의 값이 같은지 검사합니다.
@@ -72,6 +94,15 @@ export const isEmpty = value => {
 };
 
 /**
+ * 배열에 값이 포함 되어 있는지 검사합니다.
+ *
+ * @param {number|string} value
+ * @param {any[]} items
+ * @returns {boolean}
+ */
+export const isIncludes = (value, items) => items.includes(value);
+
+/**
  * 양의 정수인지 검사합니다.
  * 1. 값이 0일 수 없습니다.
  * 2. 값이 음수일 수 없습니다.
@@ -93,12 +124,32 @@ export const isPositiveInteger = (target, description) => {
   return parsed;
 };
 
-export const isIncludes = (value, items) => items.includes(value);
-
+/**
+ * 내림 값을 반환합니다.
+ *
+ * @param {number} value
+ * @returns
+ */
 export const roundDown = value => Math.floor(value / +DIVIDE_CHARGING) * +DIVIDE_CHARGING;
 
+/**
+ * EventBus의 모듈 등록 시 사용할 key를 생성합니다.
+ *
+ * @param {string} elements
+ * @param {string} selector
+ * @param {string} type
+ * @returns
+ */
 export const generateKey = (elements, selector, type) => `${elements}-${selector}-${type}`;
 
+/**
+ * 잔돈을 계산합니다.
+ *
+ * @param {number} remainCount
+ * @param {number} changes
+ * @param {number} coin
+ * @returns
+ */
 export const getChangesCount = (remainCount, changes, coin) => {
   if (coin > changes) return ZERO;
   if (remainCount < 1) return ZERO;
@@ -106,6 +157,13 @@ export const getChangesCount = (remainCount, changes, coin) => {
   return remainCount - count < ZERO ? remainCount : count;
 };
 
+/**
+ * 상품 구매에 따른 잔돈과 수량을 반환합니다.
+ *
+ * @param {object} param
+ * @param {object} tabData
+ * @returns
+ */
 export const calculateProduct = ({ name, quantity, price }, tabData) => [
   tabData[PRODUCT_ADD].map(item => {
     if (isEquals(item.name, name)) return { ...item, quantity: quantity - 1 };
@@ -114,12 +172,23 @@ export const calculateProduct = ({ name, quantity, price }, tabData) => [
   +tabData[CHARGE_AMOUNT] - +price,
 ];
 
-export const getChildInput = $element =>
-  Array.from($element.childNodes).reduce((result, node) => {
+/**
+ * 자식 HTMLInputElement 태그를 모두 반환합니다.
+ *
+ * @param {string} selector
+ * @returns
+ */
+export const getChildInput = selector =>
+  Array.from($(selector).childNodes).reduce((result, node) => {
     if (node instanceof HTMLInputElement) return [...result, node];
     return result;
   }, []);
 
+/**
+ * 인자로 넘겨받은 HTMLInputElement[]을 비웁니다.
+ *
+ * @param {HTMLInputElement[]} targets
+ */
 export const clearInput = targets => {
   const copied = Array.from(targets);
   copied.forEach((_, index) => {
@@ -127,7 +196,15 @@ export const clearInput = targets => {
   });
 };
 
-export const getParsedItem = (charge, unit, item) => [
+/**
+ * 잔돈 충전 시 해당 코인의 개수를 증가시킵니다.
+ *
+ * @param {number} charge
+ * @param {number} unit
+ * @param {object[]} item
+ * @returns
+ */
+export const getParsedCharges = (charge, unit, item) => [
   charge,
   item.map(({ description, count }) => {
     if (isEquals(description, unit)) return { description, count: count + 1 };
@@ -135,14 +212,21 @@ export const getParsedItem = (charge, unit, item) => [
   }),
 ];
 
+/**
+ * 탐욕법을 적용하여 잔돈을 교환합니다.
+ *
+ * @param {number} changes
+ * @param {object[]} charges
+ * @returns
+ */
 export const coinExchange = (changes, charges) => {
   const copied = [...charges];
   let leftChanges = changes;
-  let index = 0;
-  while (leftChanges > 0 && index < charges.length) {
+  let index = ZERO;
+  while (leftChanges > ZERO && index < charges.length) {
     const { description, count } = copied[index];
     const changesCount = getChangesCount(count, leftChanges, +description);
-    if (changesCount <= 0) index += 1;
+    if (changesCount <= ZERO) index += 1;
     else {
       leftChanges -= +description * changesCount;
       copied[index] = { ...copied[index], count: count - changesCount };
@@ -151,29 +235,70 @@ export const coinExchange = (changes, charges) => {
   return [copied, leftChanges];
 };
 
+/**
+ * 상품 관리 탭: 상품을 추가합니다.
+ *
+ * @param {HTMLInputElement[]} elements
+ * @returns
+ */
 export const addItems = elements => {
   const [{ value: name }, { value: price }, { value: quantity }] = elements;
   return { name, price, quantity };
 };
 
+/**
+ * 잔돈 충전 탭: 자판기에 잔돈을 충전합니다.
+ *
+ * @param {object} storageItem
+ * @param {HTMLInputElement} element
+ * @returns
+ */
 export const charging = (storageItem, element) => {
   let items = [...storageItem];
   const [{ value }] = element;
   let parsed = roundDown(value);
-  while (parsed > 0) {
+  while (parsed > ZERO) {
     const pickUnit = pickNumberInList(CHARGE_UNIT);
     const restCharge = parsed - pickUnit;
-    if (restCharge >= 0) [parsed, items] = getParsedItem(restCharge, pickUnit, items);
+    if (restCharge >= ZERO) [parsed, items] = getParsedCharges(restCharge, pickUnit, items);
   }
   return items;
 };
 
+/**
+ * 상품 구매 탭: 금액을 투입합니다.
+ *
+ * @param {object} storageItem
+ * @param {HTMLInputElement} elements
+ * @returns
+ */
 export const purchase = (storageItem, elements) => {
   const [{ value: charge }] = elements;
-  const currentAmount = storageItem[CHARGE_AMOUNT] || 0;
+  const currentAmount = storageItem[CHARGE_AMOUNT] || ZERO;
   return +charge + +currentAmount;
 };
 
+/**
+ * 상품 구매 탭: 상품을 구매하고 그 결과를 적용합니다.
+ *
+ * @param {HTMLTableRowElement} element
+ * @param {object} tabData
+ * @returns
+ */
+export const applyProduct = (element, tabData) => {
+  const { textContent: name } = $('[data-product-name]', $closest(element, 'tr'));
+  const { textContent: price } = $('[data-product-price]', $closest(element, 'tr'));
+  const { textContent: quantity } = $('[data-product-quantity]', $closest(element, 'tr'));
+
+  const productInto = { name, quantity, price };
+  const [product, changes] = calculateProduct(productInto, tabData);
+
+  return { name, quantity, product, changes };
+};
+
+/**
+ * Storage에 수정된 아이템을 반영합니다.
+ */
 export const setStorage = {
   [PRODUCT_ADD]: (storage, elements) => storage.add(PRODUCT_ADD, addItems(elements)),
   [MACHINE_MANAGE]: (storage, elements, item) =>
--- src/container/Main.js
+++ src/container/Main.js
@@ -1,24 +1,59 @@
 import {
-  $,
-  $closest,
-  calculateProduct,
   clearInput,
   isEquals,
   getChildInput,
   coinExchange,
   setStorage,
+  applyProduct,
 } from '../common/helper.js';
 import { isValidateInput, purchaseValidate } from '../common/validations.js';
 import { CHARGE_AMOUNT, MACHINE_MANAGE, PRODUCT_ADD, PURCHASE_MENU } from '../constants/index.js';
 import Content from '../presentational/Content/index.js';
 import Component from './root/Component.js';
 
+function formButtonEvent(event) {
+  event.preventDefault();
+
+  const storageItem = this.itemGetter(this.$props.component);
+  const targetInputs = getChildInput('form');
+  const checkedInputs = isValidateInput(targetInputs, storageItem);
+
+  if (!isEquals(checkedInputs.length, targetInputs.length)) return clearInput(targetInputs);
+
+  setStorage[this.$props.component](this.$storage, checkedInputs, storageItem);
+
+  clearInput(targetInputs);
+}
+
+function tableButtonEvent({ target }) {
+  const { name, quantity, product, changes } = applyProduct(target, this.$props.tabData);
+
+  if (!purchaseValidate({ quantity, changes }, name)) return;
+
+  this.$storage.creation(PURCHASE_MENU, {
+    ...this.$props.tabData,
+    [PRODUCT_ADD]: product,
+    [CHARGE_AMOUNT]: changes,
+  });
+}
+
+function divButtonEvent() {
+  const { tabData } = this.$props;
+  const [changes, leftChanges] = coinExchange(tabData[CHARGE_AMOUNT], tabData[MACHINE_MANAGE]);
+
+  this.$storage.creation(PURCHASE_MENU, {
+    ...tabData,
+    [MACHINE_MANAGE]: [...changes],
+    [CHARGE_AMOUNT]: leftChanges,
+  });
+}
+
 export default class Main extends Component {
   initialized() {
     this.$props = { ...this.$props, tabData: this.itemGetter(this.$props.component) };
-    this.$eventBus.addEvent('form', 'button', 'click', this.formButtonEvent.bind(this));
-    this.$eventBus.addEvent('table', 'button', 'click', this.tableButtonEvent.bind(this));
-    this.$eventBus.addEvent('.changes', 'button', 'click', this.divButtonEvent.bind(this));
+    this.$eventBus.addEvent('form', 'button', 'click', formButtonEvent.bind(this));
+    this.$eventBus.addEvent('table', 'button', 'click', tableButtonEvent.bind(this));
+    this.$eventBus.addEvent('.changes', 'button', 'click', divButtonEvent.bind(this));
   }
 
   template() {
@@ -33,53 +68,5 @@ export default class Main extends Component {
     this.$eventBus.delegation('form', 'button', 'click');
     this.$eventBus.delegation('table', 'button', 'click');
     this.$eventBus.delegation('.changes', 'button', 'click');
-  }
-
-  formButtonEvent(event) {
-    event.preventDefault();
-
-    const storageItem = this.itemGetter(this.$props.component);
-    const targetInputs = getChildInput($('form'));
-    const checkedInputs = isValidateInput(targetInputs, storageItem);
-
-    if (!isEquals(checkedInputs.length, targetInputs.length)) return clearInput(targetInputs);
-
-    setStorage[this.$props.component](this.$storage, checkedInputs, storageItem);
-
-    clearInput(targetInputs);
-  }
-
-  tableButtonEvent({ target }) {
-    const { name, quantity, product, changes } = this.applyProduct(target);
-
-    if (!purchaseValidate({ quantity, changes }, name)) return;
-
-    this.$storage.creation(PURCHASE_MENU, {
-      ...this.$props.tabData,
-      [PRODUCT_ADD]: product,
-      [CHARGE_AMOUNT]: changes,
-    });
-  }
-
-  divButtonEvent() {
-    const { tabData } = this.$props;
-    const [changes, leftChanges] = coinExchange(tabData[CHARGE_AMOUNT], tabData[MACHINE_MANAGE]);
-
-    this.$storage.creation(PURCHASE_MENU, {
-      ...tabData,
-      [MACHINE_MANAGE]: [...changes],
-      [CHARGE_AMOUNT]: leftChanges,
-    });
-  }
-
-  applyProduct(element) {
-    const { textContent: name } = $('[data-product-name]', $closest(element, 'tr'));
-    const { textContent: price } = $('[data-product-price]', $closest(element, 'tr'));
-    const { textContent: quantity } = $('[data-product-quantity]', $closest(element, 'tr'));
-
-    const productInto = { name, quantity, price };
-    const [product, changes] = calculateProduct(productInto, this.$props.tabData);
-
-    return { name, quantity, product, changes };
   }
 }
