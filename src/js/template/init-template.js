30e7df6c98d415b1a8c8095b4a39d48f993e7b75
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-12-11 04:01:59 +0900

    fix: 띄워쓰기 안한거 띄워쓰기 해주기

--- src/js/template/init-template.js
+++ src/js/template/init-template.js
@@ -3,8 +3,8 @@ import { ID } from "../util/constant.js";
 export const INIT_TEMPLATE = `
   <div id="head">
     <h1>🥤자판기🥤</h1>
-    <button id=${ID.PRODUCT_ADD_MENU}>상품관리</button>
-    <button id=${ID.VENDING_MACHINE_MANAGE_MENU}>잔돈충전</button>
+    <button id=${ID.PRODUCT_ADD_MENU}>상품 관리</button>
+    <button id=${ID.VENDING_MACHINE_MANAGE_MENU}>잔돈 충전</button>
     <button id=${ID.PRODUCT_PURCHASE_MENU}>상품 구매</button>
   </div>
 `;
