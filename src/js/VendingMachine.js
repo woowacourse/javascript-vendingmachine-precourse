6ff38b1e3a4ba606fad241466aaca86d24269fb3
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-12-11 01:44:08 +0900

    refactor: 처음 제목과 버튼 렌더링하는 템플릿 template폴더로 이동

--- src/js/VendingMachine.js
+++ src/js/VendingMachine.js
@@ -1,6 +1,7 @@
 import CoinStorageController from "./coinStorage/coinStorageController.js";
 import ProductController from "./product/ProductController.js";
 import PurchaseController from "./purchase/purchaseController.js";
+import { INIT_TEMPLATE } from "./template/init-template.js";
 
 export default class VendingMachine {
   constructor() {
@@ -21,15 +22,7 @@ export default class VendingMachine {
   };
 
   renderHeader = () => {
-    const template = `
-      <div id="head">
-        <h1>🥤자판기🥤</h1>
-        <button id="product-add-menu">상품관리</button>
-        <button id="vending-machine-manage-menu">잔돈충전</button>
-        <button id="product-purchase-menu">상품 구매</button>
-      </div>
-    `;
-    this.$container.insertAdjacentHTML("beforeend", template);
+    this.$container.insertAdjacentHTML("beforeend", INIT_TEMPLATE);
   };
 
   setEvent = () => {
---
+++ src/js/template/init-template.js
@@ -0,0 +1,8 @@
+export const INIT_TEMPLATE = `
+  <div id="head">
+    <h1>🥤자판기🥤</h1>
+    <button id="product-add-menu">상품관리</button>
+    <button id="vending-machine-manage-menu">잔돈충전</button>
+    <button id="product-purchase-menu">상품 구매</button>
+  </div>
+`;
