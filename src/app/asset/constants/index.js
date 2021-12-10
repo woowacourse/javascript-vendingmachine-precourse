4c67ea831fc83fc24798463ba9690686c0587138
sanaandmomo <qajjang46@naver.com>
2021-12-10 19:58:32 +0900

    refactor: 상수 한 곳에 모아놔라

---
+++ src/app/asset/constants/index.js
@@ -0,0 +1,41 @@
+import BEVERAGE_ICON_PATH from './BEVERAGE_ICON_PATH.js';
+import BUTTON from './BUTTON.js';
+import CHANGE_COIN_ID from './CHANGE_COIN_ID.js';
+import COIN_ID from './COIN_ID.js';
+import COIN from './COIN.js';
+import ERROR_MSG from './ERROR_MSG.js';
+import GUIDE from './GUIDE.js';
+import INPUT_ID from './INPUT_ID.js';
+import INPUT_ITEM from './INPUT_ITEM.js';
+import INPUT_NUMBER_RULE from './INPUT_NUMBER_RULE.js';
+import STORAGE_KEY from './STORAGE_KEY.js';
+import SUB_TITLE_TEXT from './SUB_TITLE_TEXT.js';
+import TABLE_CELL_CLASS_NAME from './TABLE_CELL_CLASS_NAME.js';
+import TABLE_CELL_DATASET from './TABLE_CELL_DATASET.js';
+import TABLE_ROW_CLASS_NAME from './TABLE_ROW_CLASS_NAME.js';
+import TABLE_TITLE from './TABLE_TITLE.js';
+import TAP from './TAP.js';
+import UNIT from './UNIT.js';
+import VENDING_MACHINE_TITLE from './VENDING_MACHINE_TITLE.js';
+
+export {
+    BEVERAGE_ICON_PATH,
+    BUTTON,
+    CHANGE_COIN_ID,
+    COIN_ID,
+    COIN,
+    ERROR_MSG,
+    GUIDE,
+    INPUT_ID,
+    INPUT_ITEM,
+    INPUT_NUMBER_RULE,
+    STORAGE_KEY,
+    SUB_TITLE_TEXT,
+    TABLE_CELL_CLASS_NAME,
+    TABLE_CELL_DATASET,
+    TABLE_ROW_CLASS_NAME,
+    TABLE_TITLE,
+    TAP,
+    UNIT,
+    VENDING_MACHINE_TITLE,
+};
