5bba6e8652b2454564f810225c140151f9a0b169
Chamy619 <yd95069506@naver.com>
2021-12-12 01:00:24 +0900

    refactor: 상품 구매 탭의 반환된 동전 개수를 출력하는 기능 동전 값과 DOM id 를 매핑해 반복문으로 처리하도록 리팩터링

--- src/dom/printReturnCoinTable.js
+++ src/dom/printReturnCoinTable.js
@@ -1,15 +1,22 @@
 import { DOM_ID_SELECTOR } from '../constants.js';
+import makeCoinDomIdMapper from '../utils/makeCoinDomIdMapper.js';
+
+const getCoinDomIdMapper = () => {
+  return makeCoinDomIdMapper([
+    DOM_ID_SELECTOR.coin500Quantity,
+    DOM_ID_SELECTOR.coin100Quantity,
+    DOM_ID_SELECTOR.coin50Quantity,
+    DOM_ID_SELECTOR.coin10Quantity,
+  ]);
+};
 
 const printReturnCoinTable = (coins) => {
-  const $coin500Quantity = document.getElementById(DOM_ID_SELECTOR.coin500Quantity);
-  const $coin100Quantity = document.getElementById(DOM_ID_SELECTOR.coin100Quantity);
-  const $coin50Quantity = document.getElementById(DOM_ID_SELECTOR.coin50Quantity);
-  const $coin10Quantity = document.getElementById(DOM_ID_SELECTOR.coin10Quantity);
+  const coinDomIdMapper = getCoinDomIdMapper();
 
-  $coin500Quantity.innerText = `${coins[500]}개`;
-  $coin100Quantity.innerText = `${coins[100]}개`;
-  $coin50Quantity.innerText = `${coins[50]}개`;
-  $coin10Quantity.innerText = `${coins[10]}개`;
+  for (let coin in coinDomIdMapper) {
+    const $coinQuantity = document.getElementById(coinDomIdMapper[coin]);
+    $coinQuantity.innerText = `${coins[coin]}개`;
+  }
 };
 
 export default printReturnCoinTable;
