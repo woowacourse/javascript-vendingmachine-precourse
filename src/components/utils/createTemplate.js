a4343db06250378eb39ebf8ea4e13fe8f527f7f7
leegwae <leegwae@gmail.com>
2021-12-12 06:03:32 +0900

    refactor: remove unused element

--- src/components/utils/createTemplate.js
+++ src/components/utils/createTemplate.js
@@ -1,15 +1,12 @@
-import createElement from './createElement.js';
 import createDivision from './createDivision.js';
 import createHeading from './createHeading.js';
 
 const createTemplate = (id, title, child) => {
   const template = createDivision({ id });
   const heading = createHeading(3, title);
-  const container = createElement('p');
-  container.appendChild(child);
 
   template.appendChild(heading);
-  template.appendChild(container);
+  template.appendChild(child);
 
   return template;
 };
