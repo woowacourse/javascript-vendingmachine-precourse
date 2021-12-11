fc3807c956db6524dbb7db5dd911b8ff363f2e74
leezer94 <2kunhee94@gmail.com>
2021-12-12 00:17:58 +0900

    feat: 상수 추가 selector 상수 추가

---
+++ src/constants/constants.js
@@ -0,0 +1,13 @@
+import {
+  header,
+  productMangeMenu,
+  vendingMachineManageMenu,
+  productPurchaseMenu,
+} from '../dom/dom.js';
+
+export const SELECTOR = {
+  HEADER: header,
+  PRODUCT_MANAGE: productMangeMenu,
+  VENDING_MANAGE: vendingMachineManageMenu,
+  PRODUCT_PURCHASE: productPurchaseMenu,
+};
