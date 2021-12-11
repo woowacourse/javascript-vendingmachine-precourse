cdc135db763c9f0b5cdae69a8655389dea5fa3dc
hozzijeong <ghwls0905@naver.com>
2021-12-11 12:27:41 +0900

    Feat: localStorage get,set 추가

--- src/storage/localStorage.js
+++ src/storage/localStorage.js
@@ -1,7 +1,15 @@
 import { LOACL_STORAGE as LOCAL, EMPTY } from "./constant.js";
 
 export const initialStorageSet = () => {
-    localStorage.setItem(LOCAL.PRODUCT, EMPTY);
-    localStorage.setItem(LOCAL.TOTAL_AMOUNT, EMPTY);
-    localStorage.setItem(LOCAL.COIN_AMOUNT, EMPTY);
+    [LOCAL.PRODUCT, LOCAL.TOTAL_AMOUNT, LOCAL.COIN_AMOUNT].forEach((element) =>
+        setLocalStorage(element, EMPTY),
+    );
+};
+
+export const setLocalStorage = (key, value) => {
+    localStorage.setItem(key, value);
+};
+
+export const getLocalStorage = (key) => {
+    return localStorage.getItem(key);
 };
