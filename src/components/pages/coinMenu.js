83965e7b5c77d4d470dedd137f88476e59066565
moonyerim <moonyerim2@gmail.com>
2021-12-12 01:32:18 +0900

    feat: 잔돈 충전 메뉴 html 템플릿 모듈화

---
+++ src/components/pages/coinMenu.js
@@ -0,0 +1,60 @@
+import { COIN_MENU } from '../../data/elementData.js';
+
+export default class CoinMenu {
+  constructor() {
+    this.$main = document.createElement('main');
+    this.template = this.coinMenuTemplate();
+    this.closeMenu();
+  }
+
+  closeMenu() {
+    this.$main.style.display = 'none';
+  }
+
+  showMenu() {
+    this.$main.style.display = 'block';
+  }
+
+  coinMenuTemplate() {
+    this.$main.insertAdjacentHTML('beforeend', `${this.inputTemplate()}${this.coinTableTemplate()}`);
+  }
+  
+  inputTemplate() {
+    return `<h3>자판기 동전 충전하기</h3>
+      <form>
+        <input type="number" id="${COIN_MENU.INPUT_SELECTOR.COIN_CHARGE_INPUT}" placeholder="자판기가 보유할 금액">
+        <button id="${COIN_MENU.INPUT_SELECTOR.COIN_CHARGE_BUTTON}">충전하기</button>
+        <p id="${COIN_MENU.INPUT_SELECTOR.COIN_CHARGE_AMOUNT}">보유 금액:</p>
+      </form>`;
+  }
+
+  coinTableTemplate() {
+    return `<h3>자판기가 보유한 동전</h3>
+      <table style="border-collapse: collapse;" width=200 border="1">
+      <thead style="text-align: center;">
+        <tr>
+          <td>동전</td>
+          <td>개수</td>
+        </tr>
+      </thead>
+      <tbody style="text-align: center;">
+        <tr>
+          <td>500원</td>
+          <td id="${COIN_MENU.TABLE_SELECTOR.COIN_500}"></td>
+        </tr>
+        <tr>
+          <td>100원</td>
+          <td id="${COIN_MENU.TABLE_SELECTOR.COIN_100}"></td>
+        </tr>
+        <tr>
+          <td>50원</td>
+          <td id="${COIN_MENU.TABLE_SELECTOR.COIN_50}"></td>
+        </tr>
+        <tr>
+          <td>10원</td>
+          <td id="${COIN_MENU.TABLE_SELECTOR.COIN_10}"></td>
+        </tr>
+      </tbody>
+      </table>`;
+  }
+}
