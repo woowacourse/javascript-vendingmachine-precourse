0603c034e779be0e965185aa88dc8ab3c873572d
2doo <2doo@kakao.com>
2021-12-11 19:36:00 +0900

    feat(components/App): 새로 시작 버튼 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import { ELEMENT_ID, MENUS } from '../constants/index.js';
+import LocalStorage from '../utils/localStorage.js';
+import Button from './base/Button.js';
 import Component from './base/Component.js';
 import Label from './base/Label.js';
 import Machine from './Machine.js';
@@ -9,16 +11,24 @@ class App extends Component {
 
   $title;
 
+  $restartButton;
+
   $machine;
 
   constructor() {
     super(document.getElementById(ELEMENT_ID.APP));
     this.$tabs = new Tabs(MENUS.PRODUCT_MANAGE);
     this.$title = new Label('h2', '🥤자판기🥤');
+    this.$restartButton = new Button('새로 시작');
     this.$machine = new Machine(MENUS.PRODUCT_MANAGE);
     this.setEvent();
 
-    this.children = [this.$tabs, this.$title, this.$machine];
+    this.children = [
+      this.$tabs,
+      this.$title,
+      this.$restartButton,
+      this.$machine,
+    ];
   }
 
   setEvent() {
@@ -30,6 +40,11 @@ class App extends Component {
       const { currentTab } = newStateParams;
       this.$machine.setState({ currentMenu: currentTab });
     };
+
+    this.$restartButton.setOnClick(() => {
+      LocalStorage.clear();
+      window.location.reload();
+    });
   }
 
   update() {
