6f9db5e6a4efb6249c6ecade5a14aedc6534c8bf
joker77z <joker77z@naver.com>
2021-12-11 21:31:42 +0900

    feat($productAddMenu): [상품 관리] 버튼 클릭 시 렌더 구현
    + 이전 커밋 제목 수정
    "첫 접속 시 상품 구매 페이지 렌더"
    -> "첫 접속 시 상품 관리 페이지 렌더"

--- src/index.mjs
+++ src/index.mjs
@@ -8,3 +8,22 @@ function init() {
 window.addEventListener('DOMContentLoaded', () => {
   init();
 });
+
+// 상품 관리 버튼
+window.addEventListener('click', ({ target, preventDefault }) => {
+  // preventDefault();
+  const $productAddMenu = document.querySelector('#product-add-menu');
+
+  if (target !== $productAddMenu) return;
+  if (!document.querySelector('.productAddWrap')) renderProductAddMenu();
+});
+
+window.addEventListener('click', ({ target, preventDefault }) => {
+  // preventDefault();
+  const $vendingMachineManageMenu = document.querySelector(
+    '#vending-machine-manage-menu'
+  );
+
+  if (target !== $vendingMachineManageMenu) return;
+  document.querySelector('main').innerHTML = '';
+});
