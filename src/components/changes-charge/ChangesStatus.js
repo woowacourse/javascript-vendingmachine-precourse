732fba89637e18e38007dfa1aedfb258d26e1d9b
2doo <2doo@kakao.com>
2021-12-11 12:55:31 +0900

    feat(components/changes-charge): 잔돈 현황 테이블 구현 + CurrentChanges 위치 변경

---
+++ src/components/changes-charge/ChangesStatus.js
@@ -0,0 +1,31 @@
+import VendingMachineStore from '../../store/vendingMachineStore.js';
+import { $tag } from '../../utils/index.js';
+import Component from '../base/Component.js';
+import Label from '../base/Label.js';
+import ChangesTable from './ChangesTable.js';
+import CurrentChanges from './CurrentChanges.js';
+
+class ChangesStatus extends Component {
+  $title;
+
+  constructor() {
+    super($tag('div'));
+
+    this.$currentChanges = new CurrentChanges();
+    this.$title = new Label('h3', '자판기가 보유한 동전');
+    this.$table = new ChangesTable();
+    this.children = [this.$currentChanges, this.$title, this.$table];
+    this.update();
+  }
+
+  update() {
+    const coins = VendingMachineStore.instance.coinStorage.items;
+    const totalAmount =
+      VendingMachineStore.instance.coinStorage.getTotalAmount();
+
+    this.$table.setState({ dataset: coins });
+    this.$currentChanges.setState({ amount: totalAmount });
+  }
+}
+
+export default ChangesStatus;
--- src/components/changes-charge/index.js
+++ src/components/changes-charge/index.js
@@ -2,7 +2,7 @@ import VendingMachineStore from '../../store/vendingMachineStore.js';
 import { $tag } from '../../utils/index.js';
 import Component from '../base/Component.js';
 import Charge from './Charge.js';
-import CurrentChanges from './CurrentChanges.js';
+import ChangesStatus from './ChangesStatus.js';
 
 class ChangesCharge extends Component {
   $charge;
@@ -10,19 +10,17 @@ class ChangesCharge extends Component {
   constructor() {
     super($tag('div'));
     this.$charge = new Charge();
-    this.$currentChanges = new CurrentChanges();
+    this.$status = new ChangesStatus();
 
     this.setEvent();
-    this.children = [this.$charge, this.$currentChanges];
+    this.children = [this.$charge, this.$status];
   }
 
   setEvent() {
     this.$charge.onSubmit = ({ amount }) => {
       VendingMachineStore.instance.chargeChanges(amount);
       this.$charge.resetInputs();
-      this.$currentChanges.setState({
-        amount: VendingMachineStore.instance.coinStorage.getTotalAmount(),
-      });
+      this.$status.update();
     };
   }
 }
