4e97835041dad1d1f4e4427a79cee3c9b673c10a
Daye Lee <leeda1801@gmail.com>
2021-12-11 22:49:11 +0900

    feat: add tab menu

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,9 @@
-const HEADER = {
+export const HEADER = {
   MAIN: '🥤자판기🥤',
 };
 
-export default HEADER;
+export const MENU = {
+  PRODUCT_ADD: '상품 관리',
+  VENDING_MACHINE_MANAGE: '잔돈 충전',
+  PRODUCT_PURCHASE: '상품 구매',
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import HEADER from './constants.js';
+import { HEADER, MENU } from './constants.js';
 import $app from './elements.js';
 
 function createMainHeader() {
@@ -8,3 +8,36 @@ function createMainHeader() {
 }
 
 createMainHeader();
+
+function createDiv() {
+  return document.createElement('div');
+}
+
+function createButton(attr) {
+  const button = document.createElement('button');
+  const [id, innerText] = attr;
+  button.setAttribute('id', id);
+  button.innerText = innerText;
+
+  return button;
+}
+
+function createMenuButtons() {
+  const buttonAttrs = [
+    ['product-add-menu', MENU.PRODUCT_ADD],
+    ['vending-machine-manage-menu', MENU.VENDING_MACHINE_MANAGE],
+    ['product-purchase-menu', MENU.PRODUCT_PURCHASE],
+  ];
+  const menuButtons = buttonAttrs.map((attr) => createButton(attr));
+
+  return menuButtons;
+}
+
+function createTabMenu() {
+  const menuDiv = createDiv();
+  const menuButtons = createMenuButtons();
+  menuButtons.forEach((button) => menuDiv.append(button));
+  $app.appendChild(menuDiv);
+}
+
+createTabMenu();
