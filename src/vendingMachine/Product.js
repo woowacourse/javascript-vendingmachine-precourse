f578dabfdbcb43526c04e7e472bb4999f929ca24
DomMorello <zuzudnf@gmail.com>
2021-12-11 16:22:57 +0900

    fix: Product에서 price, quantity를 number type으로 수정
    입력시 3e3 입력하면 render에서 3e3문자열로 나오는 버그 수정

--- src/vendingMachine/Product.js
+++ src/vendingMachine/Product.js
@@ -8,8 +8,8 @@ import {
 export default class Product {
   constructor({ name, price, quantity }) {
     this.name = name.trim();
-    this.price = price;
-    this.quantity = quantity;
+    this.price = +price;
+    this.quantity = +quantity;
   }
 
   addedTemplate() {
