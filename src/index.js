4a908412613ad9c3ef8a61b07d8e9137bff6e9ff
Chamy619 <yd95069506@naver.com>
2021-12-11 21:53:54 +0900

    refactor: 메뉴 탭에 이벤트를 등록하는 부분 외부 모듈로 분리

--- src/index.js
+++ src/index.js
@@ -1,40 +1,7 @@
 import renderAppTemplate from './renderAppTemplate.js';
-import renderProductAddTemplate from './renderProductAddTemplate.js';
-import renderVendingMachineManageMenuTemplate from './renderVendingMachineManageMenuTemplate.js';
-import renderProductPurchaseTemplate from './renderProductPurchaseTemplate.js';
-import attachProductAddEvent from './attachProductAddEvent.js';
-import { DOM_ID_SELECTOR } from './constants.js';
-import attachVendingMachineChargeEvent from './attachVendingMachineChargeEvent.js';
-import printProductManageTable from './printProductManageTable.js';
-import printVendingMachineAmountAndCoinTable from './printVendingMachineAmountAndCoinTable.js';
-import attachChargeEvent from './attachChargeEvent.js';
 import VendingMachine from './VendingMachine.js';
-import printChargeAmount from './printChargeAmount.js';
-import printProductPurchaseItemTable from './printProductPurchaseItemTable.js';
-import attachReturnCoinEvent from './attachReturnCoinEvent.js';
+import attachMenuTabEvent from './attachMenuTabEvent.js';
 
 renderAppTemplate();
 const vendingMachine = new VendingMachine();
-
-const $productAddMenu = document.getElementById(DOM_ID_SELECTOR.productAddMenu);
-$productAddMenu.addEventListener('click', () => {
-  renderProductAddTemplate();
-  printProductManageTable(vendingMachine.product);
-  attachProductAddEvent(vendingMachine.product);
-});
-
-const $vendingMachineManageMenu = document.getElementById(DOM_ID_SELECTOR.vendingMachineManageMenu);
-$vendingMachineManageMenu.addEventListener('click', () => {
-  renderVendingMachineManageMenuTemplate();
-  printVendingMachineAmountAndCoinTable(vendingMachine.coin);
-  attachVendingMachineChargeEvent(vendingMachine.coin);
-});
-
-const $productPurchaseMenu = document.getElementById(DOM_ID_SELECTOR.productPurchaseMenu);
-$productPurchaseMenu.addEventListener('click', () => {
-  renderProductPurchaseTemplate();
-  printChargeAmount(vendingMachine.getMoney());
-  printProductPurchaseItemTable(vendingMachine);
-  attachChargeEvent(vendingMachine);
-  attachReturnCoinEvent(vendingMachine);
-});
+attachMenuTabEvent(vendingMachine);
