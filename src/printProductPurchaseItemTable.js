a2c0bae36b08043ddc3c0cdfac9342ee5142dac1
Chamy619 <yd95069506@naver.com>
2021-12-11 13:26:22 +0900

    fix: 구매할 수 있는 상품 현황 테이블 상품명, 가격, 수량에 dataset 사용

--- src/printProductPurchaseItemTable.js
+++ src/printProductPurchaseItemTable.js
@@ -1,23 +1,24 @@
+import attachPurchaseEvent from './attachPurchaseEvent.js';
 import { DOM_ID_SELECTOR, DOM_CLASS_SELECTOR, PRODUCT_PURCHASE_MESSAGE } from './constants.js';
 import makeButton from './makeButton.js';
 
-const makeTd = (className, text) => {
+const makeTd = (className, key, value) => {
   const $td = document.createElement('td');
   $td.className = className;
-  $td.appendChild(document.createTextNode(text));
+  $td.appendChild(document.createTextNode(value));
+  $td.dataset[key] = value;
 
   return $td;
 };
 
-const makeButtonTd = (className, text, name, price, quantity) => {
+const makeButtonTd = (className, text, productName, vendingMachine) => {
   const $td = document.createElement('td');
 
   const $button = makeButton(text);
+  $button.setAttribute('value', productName);
   $button.className = className;
 
-  $button.dataset.productName = name;
-  $button.dataset.productPrice = price;
-  $button.dataset.productQuantity = quantity;
+  attachPurchaseEvent($button, vendingMachine);
 
   $td.appendChild($button);
 
@@ -31,13 +32,21 @@ const makeTr = (className) => {
   return $tr;
 };
 
-const makeProductPurchaseItem = (name, price, quantity) => {
+const removeProductPurchaseItems = () => {
+  const $productPurchaseItems = document.getElementsByClassName(DOM_CLASS_SELECTOR.purchaseItem);
+
+  while ($productPurchaseItems.length > 0) {
+    $productPurchaseItems[0].remove();
+  }
+};
+
+const makeProductPurchaseItem = (name, price, quantity, vendingMachine) => {
   const $purchaseItem = makeTr(DOM_CLASS_SELECTOR.purchaseItem);
 
-  const $productPurchaseName = makeTd(DOM_CLASS_SELECTOR.productPurchaseName, name);
-  const $productPurchasePrice = makeTd(DOM_CLASS_SELECTOR.productPurchasePrice, price);
-  const $productPurchaseQuantity = makeTd(DOM_CLASS_SELECTOR.productPurchaseQuantity, quantity);
-  const $purchaseButton = makeButtonTd(DOM_CLASS_SELECTOR.purchaseButton, PRODUCT_PURCHASE_MESSAGE.purchase, name, price, quantity);
+  const $productPurchaseName = makeTd(DOM_CLASS_SELECTOR.productPurchaseName, 'productName', name);
+  const $productPurchasePrice = makeTd(DOM_CLASS_SELECTOR.productPurchasePrice, 'productPrice', price);
+  const $productPurchaseQuantity = makeTd(DOM_CLASS_SELECTOR.productPurchaseQuantity, 'productQuantity', quantity);
+  const $purchaseButton = makeButtonTd(DOM_CLASS_SELECTOR.purchaseButton, PRODUCT_PURCHASE_MESSAGE.purchase, name, vendingMachine);
 
   $purchaseItem.appendChild($productPurchaseName);
   $purchaseItem.appendChild($productPurchasePrice);
@@ -47,12 +56,13 @@ const makeProductPurchaseItem = (name, price, quantity) => {
   return $purchaseItem;
 };
 
-const printProductPurchaseItemTable = (product) => {
+const printProductPurchaseItemTable = (vendingMachine) => {
   const $productPurchaseTable = document.getElementById(DOM_ID_SELECTOR.productPurchaseTable);
+  removeProductPurchaseItems();
 
-  const productItems = product.getProductItems();
+  const productItems = vendingMachine.product.getProductItems();
   for (let name in productItems) {
-    const $productPurchaseItem = makeProductPurchaseItem(name, productItems[name].price, productItems[name].quantity);
+    const $productPurchaseItem = makeProductPurchaseItem(name, productItems[name].price, productItems[name].quantity, vendingMachine);
     $productPurchaseTable.appendChild($productPurchaseItem);
   }
 };
