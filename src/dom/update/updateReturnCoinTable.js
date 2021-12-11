c03955a55dcc502a8da147e47bbc4c580e617526
byhhh2 <rlaalsdl111@gmail.com>
2021-12-12 04:07:43 +0900

    refactor: separation of constants

--- src/dom/update/updateReturnCoinTable.js
+++ src/dom/update/updateReturnCoinTable.js
@@ -1,11 +1,11 @@
 import ReturnCoin from '../../class/returnCoin.js';
 import { getReturnCoinTableData } from '../domElement.js';
+import { COINS } from '../../constants/data.js';
 
 export const updateReturnCoinTable = () => {
-  const coins = [500, 100, 50, 10];
   const returnCoinTableData = getReturnCoinTableData();
 
-  coins.forEach((coin) => {
+  COINS.forEach((coin) => {
     returnCoinTableData[coin].innerHTML = `${ReturnCoin.returnedCoins[coin]}개`;
   });
 };
