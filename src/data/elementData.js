18530cea2cab8dd475520b0b9347678ec25b633f
moonyerim <moonyerim2@gmail.com>
2021-12-12 01:29:00 +0900

    feat: DOM선택자 상수로 선언

---
+++ src/data/elementData.js
@@ -0,0 +1,64 @@
+export const TABMENU = {
+  SELECTOR: {
+    PRODUCT_MENU: 'product-add-menu',
+    COIN_MENU: 'vending-machine-manage-menu',
+    PURCHASE_MENU: 'product-purchase-menu',
+  },
+};
+
+export const PRODUCT_MENU = {
+  INPUT_SELECTOR: {
+    PRODUCT_NAME_INPUT: 'product-name-input',
+    PRODUCT_PRICE_INPUT: 'product-price-input',
+    PRODUCT_QUANTITY_INPUT: 'product-quantity-input',
+    PRODUCT_ADD_BUTTON: 'product-add-button',
+  },
+  TABLE_SELECTOR: {
+    ITEM: 'product-manage-item',
+    PRODUCT_NAME: 'product-manage-name',
+    PRODUCT_PRICE: 'product-manage-price',
+    PRODUCT_QUANTITY: 'product-manage-quantity',
+  }
+};
+
+export const COIN_MENU = {
+  INPUT_SELECTOR: {
+    COIN_CHARGE_INPUT: 'vending-machine-charge-input',
+    COIN_CHARGE_BUTTON: 'vending-machine-charge-button',
+    COIN_CHARGE_AMOUNT: 'vending-machine-charge-amount',
+  },
+  TABLE_SELECTOR: {
+    COIN_500: 'vending-machine-coin-500-quantity',
+    COIN_100: 'vending-machine-coin-100-quantity',
+    COIN_50: 'vending-machine-coin-50-quantity',
+    COIN_10: 'vending-machine-coin-10-quantity',
+  },
+};
+
+export const PURCHASE_MENU = {
+  INPUT_SELECTOR: {
+    PURCHASE_CHARGE_INPUT: 'charge-input',
+    PURCHASE_CHARGE_BUTTON: 'charge-button',
+    PURCHASE_CHARGE_AMOUNT: 'charge-amount',
+  },
+  PURCHASE_ITEM_BUTTON: 'purchase-button',
+  PRODUCT_TABLE_SELECTOR: {
+    PRODUCT_PURCHASE_ITEM: 'product-purchase-item',
+    PRODUCT_NAME: 'product-purchase-name',
+    PRODUCT_PRICE: 'product-purchase-price',
+    PRODUCT_QUANTITY: 'product-purchase-quantity',
+    PURCHASE_BUTTON: 'purchase-button',
+  },
+  COIN_RETURN_BUTTON: 'coin-return-button',
+  RETURN_TABLE_SELECTOR: {
+    COIN_500: 'coin-500-quantity',
+    COIN_100: 'coin-100-quantity',
+    COIN_50: 'coin-50-quantity',
+    COIN_10: 'coin-10-quantity',
+  },
+  DATASET: {
+    PRODUCT_NAME: 'data-product-name',
+    PRODUCT_PRICE: 'data-purchase-price',
+    PRODUCT_QUANTITY: 'data-purchase-quantity',
+  },
+};
