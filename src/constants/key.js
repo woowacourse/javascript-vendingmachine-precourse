da43aa9820088b19dd58489849d0b348b2d6118a
정현수 <jung660317@naver.com>
2021-12-11 17:08:19 +0900

    feat: 상품 localStorage 추가 및 불러오기 구현
    - 추가된 상품들은 localStorage에 저장하도록 구현
    - 새로고침 후에도 localStorage에 저장된 상품들을 들고오도록 변경

--- src/constants/key.js
+++ src/constants/key.js
@@ -1,3 +1,7 @@
 export default {
+  // 메뉴 관련 Key
   currentMenu: 'currentMenu',
+
+  // 상품 관련 Key
+  productItems: 'productItems',
 };
--- src/models/ProductAddMenuModel.js
+++ src/models/ProductAddMenuModel.js
@@ -1,10 +1,17 @@
+import Store from '../utils/store.js';
+
+import Key from '../constants/key.js';
+
 class ProductAddMenuModel {
   constructor() {
-    this.$productItems = [];
+    this.$productItems = Store.getLocalStorage(Key.productItems)
+      ? Store.getLocalStorage(Key.productItems)
+      : [];
   }
 
   setProductItems(items) {
     this.$productItems = items;
+    Store.setLocalStorage(Key.productItems, items);
   }
 
   getProductItems() {
