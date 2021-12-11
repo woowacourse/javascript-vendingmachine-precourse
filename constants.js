c951eb1c5e2a5dd9216dad038f97392730136ceb
Jungjjeong <sky990106@dgu.ac.kr>
2021-12-12 02:18:58 +0900

    feat: 상수 추가

---
+++ constants.js
@@ -0,0 +1,58 @@
+export const TAB_MENU = {
+    TITLE: '🥤자판기🥤',
+    PRODUCT_ADD: '상품 관리',
+    MACHINE_MANAGE: '잔돈 충전',
+    PRODUCT_PURCHASE: '상품 구매',
+  };
+  
+  export const PRODUCT_ADD = {
+    ADD_TITLE: '상품 추가하기',
+    PRODUCT_TITLE: '상품 현황',
+    NAME: '상품명',
+    PRICE: '가격',
+    QUANTITY: '수량',
+    ADD_BTN: '추가하기',
+  };
+  
+  export const MACHINE_MANAGE = {
+    ADD_TITLE: '자판기 동전 충전하기',
+    COIN_TITLE: '자판기가 보유한 동전',
+    COIN: '동전',
+    NUMBER_OF_COIN: '개수',
+    ADD_BTN: '충전하기',
+    MINIMUM_CHARGE = 100,
+  };
+  
+  export const PRODUCT_PURCHASE = {
+    ADD_TITLE: '금액 투입',
+    PRODUCT_TITLE: '구매할 수 있는 상품 현황',
+    COIN_TITLE: '잔돈',
+    NAME: '상품명',
+    PRICE: '가격',
+    QUANTITY: '수량',
+    PURCHASE: '구매',
+    COIN: '동전',
+    NUMBER_OF_COIN: '개수',
+    ADD_BTN: '투입하기',
+    PURCHASE_BTN: '구매하기',
+    RETURN_BTN: '반환하기',
+  };
+  
+  export const ALERT = {
+    NULL_PRODUCT_NAME: '상품명을 입력해주세요.',
+    DUPLI_PRODUCT_NAME: '해당 상품명은 이미 존재합니다.',
+    WRONG_PRODUCT_PRICE:
+      '100원 이상의, 10으로 나누어 떨어지는 상품 가격을 입력해주세요.',
+    WRONG_PRODUCT_QUANTITY: '1 이상의 수량을 입력해주세요.',
+    WRONG_COIN_CHARGE:
+      '10원 이상의, 10으로 나누어 떨어지는 충전 금액을 입력해주세요.',
+    WRONG_USER_CHARGE:
+      '10원 이상의, 10으로 나누어 떨어지는 투입 금액을 입력해주세요.',
+    NO_QUANTITY: '해당 상품을 구매할 수 있는 수량이 없습니다.',
+    NO_PRICE: '해당 상품을 구매할 수 있는 금액이 없습니다.',
+    NO_COIN: '반환할 잔돈이 없어 반환이 불가합니다.',
+  };
+  
+  export const NULL = '';
+  export const ZERO = 0;
+  export const DIVIDE_CHARGE = 10;
