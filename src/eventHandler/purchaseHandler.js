a813f0c1957e5be70f0f0b426710873760d01f01
DomMorello <zuzudnf@gmail.com>
2021-12-12 22:04:31 +0900

    feat: 투입한 금액 UI 업데이트

--- src/eventHandler/purchaseHandler.js
+++ src/eventHandler/purchaseHandler.js
@@ -2,13 +2,16 @@ import $ from '../util/$.js';
 import {
   PURCHASE_BUTTON_ID,
   PURCHASE_INPUT_ID,
+  PURCHASE_AMOUNT_ID,
 } from '../constant/constant.js';
 
 function onMoneyInput(event, vendingMachine) {
   event.preventDefault();
   const input = $(`#${PURCHASE_INPUT_ID}`).value;
+  const $moneySpan = $(`#${PURCHASE_AMOUNT_ID}`);
 
   vendingMachine.setCurrentMoney(input);
+  $moneySpan.textContent = vendingMachine.money;
 }
 
 export default function purchaseHandler(vendingMachine) {
