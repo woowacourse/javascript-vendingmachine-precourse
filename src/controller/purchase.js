8cfceb3d0ebbaf3f93580db7a70ea8cb11e5683f
hanyoonseok <hys6078@naver.com>
2021-12-12 15:14:47 +0900

    feat: 상품 구매 탭 금액투입 구현 localstorage에 투입 금액 갱신 dom property 상태 변경

--- src/controller/purchase.js
+++ src/controller/purchase.js
@@ -11,6 +11,26 @@ import { KEY, SELECTOR, COIN_ARRAY } from '../model/constants.js';
 import { productPurchaseTableRow } from '../model/dom.js';
 import VendingMachine from '../model/vendingMachine.js';
 
+const initChargeDomProperty = () => {
+  const charge = getItemOrNull(KEY.charge);
+  $(SELECTOR.chargeInput).value = '';
+  if (charge) {
+    $(SELECTOR.chargeAmount).innerHTML = charge;
+  }
+};
+
+const chargeMoney = () => {
+  let charge = getItemOrNull(KEY.charge);
+  const chargeInput = $(SELECTOR.chargeInput);
+  if (charge || charge === 0) {
+    charge += parseInt(chargeInput.value);
+  } else if (charge === null) {
+    charge = parseInt(chargeInput.value);
+  }
+  setItem(KEY.charge, charge);
+  initChargeDomProperty();
+};
+
 const addTableRow = (table, product) =>
   table.insertAdjacentHTML('beforeend', productPurchaseTableRow(product));
 
@@ -22,4 +42,6 @@ const initProductStatusTable = () => {
 
 export const initAllPurchase = () => {
   initProductStatusTable();
+  initChargeDomProperty();
+  $(SELECTOR.chargeButton).addEventListener('click', () => chargeMoney());
 };
