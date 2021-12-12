d4dca7f5f8dc22072352208ab874bdb25b88f1aa
Leejin-Yang <ejin1996@gmail.com>
2021-12-13 05:50:28 +0900

    refactor: 자판기 관리 메뉴 생성 및 클릭 이벤트 view로 이동

--- src/components/vendingMachineManageMenu.js
+++ src/components/vendingMachineManageMenu.js
@@ -1,40 +1,15 @@
 import { ID } from '../constants/selector.js';
 import { COIN_TABLE } from '../constants/table.js';
 import {
-  Container,
-  SubTitle,
   Form,
   Input,
   Button,
-  Span,
   Table,
   TableHead,
   TableRow,
   TableData,
   TableDataWithId,
 } from './elements.js';
-
-const onClickAddCoinButton = (e) => {
-  e.preventDefault();
-};
-
-const createAddCoinForm = () => {
-  const addCoinForm = Form();
-  const addCoinInput = Input(
-    ID.VENDING_MACHINE_CHARGE_INPUT,
-    'number',
-    '자판기가 보유할 금액'
-  );
-  const addCoinButton = Button(
-    '충전하기',
-    ID.VENDING_MACHINE_CHARGE_BUTTON,
-    onClickAddCoinButton
-  );
-
-  addCoinForm.append(addCoinInput, addCoinButton);
-
-  return addCoinForm;
-};
 
 const createCoinRow = (coin, id) => {
   const tableRow = TableRow();
@@ -46,7 +21,25 @@ const createCoinRow = (coin, id) => {
   return tableRow;
 };
 
-const createCoinTable = () => {
+export const createAddCoinForm = (event) => {
+  const addCoinForm = Form();
+  const addCoinInput = Input(
+    ID.VENDING_MACHINE_CHARGE_INPUT,
+    'number',
+    '자판기가 보유할 금액'
+  );
+  const addCoinButton = Button(
+    '충전하기',
+    ID.VENDING_MACHINE_CHARGE_BUTTON,
+    event
+  );
+
+  addCoinForm.append(addCoinInput, addCoinButton);
+
+  return addCoinForm;
+};
+
+export const createCoinTable = () => {
   const coinTable = Table();
   const coin500Row = createCoinRow('500원', ID.VENDING_MACHINE_COIN_500);
   const coin100Row = createCoinRow('100원', ID.VENDING_MACHINE_COIN_100);
@@ -58,25 +51,3 @@ const createCoinTable = () => {
 
   return coinTable;
 };
-
-const VendingMachineManageMenu = () => {
-  const container = Container('vending-machine-manage-view');
-  const addCoinSubTitle = SubTitle('자판기 동전 충전하기');
-  const addCoinForm = createAddCoinForm();
-  const coinSpan = Span('보유 금액: ');
-  const haveCoinSubTitle = SubTitle('자판기가 보유한 동전');
-  const coinTable = createCoinTable();
-
-  container.append(
-    addCoinSubTitle,
-    addCoinForm,
-    coinSpan,
-    haveCoinSubTitle,
-    coinTable
-  );
-  container.setAttribute('class', 'invisible');
-
-  return container;
-};
-
-export default VendingMachineManageMenu;
--- src/views/vendingMachineManageMenuView.js
+++ src/views/vendingMachineManageMenuView.js
@@ -1,9 +1,43 @@
-import VendingMachineManageMenu from '../components/vendingMachineManageMenu.js';
+import { ID } from '../constants/selector.js';
+import {
+  Container,
+  SubTitle,
+  Span,
+  SpanWithId,
+} from '../components/elements.js';
+import {
+  createAddCoinForm,
+  createCoinTable,
+} from '../components/vendingMachineManageMenu.js';
 
 export default function VendingMachineManageView(container) {
-  this.initView = VendingMachineManageMenu();
+  this.vendingMachineManageMenu = () => {
+    const container = Container('vending-machine-manage-view');
+    const addCoinSubTitle = SubTitle('자판기 동전 충전하기');
+    const addCoinForm = createAddCoinForm(this.onClickAddCoinButton);
+    const holdAmountSpan = Span('보유 금액: ');
+    const chargeAmountSpan = SpanWithId('', ID.VENDING_MACHINE_CHARGE_AMOUNT);
+    const haveCoinSubTitle = SubTitle('자판기가 보유한 동전');
+    const coinTable = createCoinTable();
+
+    container.append(
+      addCoinSubTitle,
+      addCoinForm,
+      holdAmountSpan,
+      chargeAmountSpan,
+      haveCoinSubTitle,
+      coinTable
+    );
+    container.setAttribute('class', 'invisible');
+
+    return container;
+  };
+
+  this.onClickAddCoinButton = (e) => {
+    e.preventDefault();
+  };
 
   this.render = () => {
-    container.append(this.initView);
+    container.append(this.vendingMachineManageMenu());
   };
 }
