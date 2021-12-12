bf4bede35fe0d0553c7f2908fd8f61e6d8237fe4
intae92 <kangit2013@gmail.com>
2021-12-12 23:06:53 +0900

    refactor: 미션 요구 사항에 따른 DOM선택자 지정하기

--- src/components/product_purchase/ProductPurchase.js
+++ src/components/product_purchase/ProductPurchase.js
@@ -11,10 +11,10 @@ export default class ProductPurchase extends Component {
       <div>
         <h3>금액 투입</h3>
         <form>
-          <input type="number" placeholder="투입할 금액"/>
-          <button>투입하기</button>
+          <input id="charge-input" type="number" placeholder="투입할 금액"/>
+          <button id="charge-button">투입하기</button>
         </form>
-        <p>투입한 금액: <span>000</span></p>
+        <p>투입한 금액: <span id="charge-amount">000</span></p>
       </div>
       <div>
         <h3>구매할 수 있는 상품 현황</h3>
@@ -28,24 +28,24 @@ export default class ProductPurchase extends Component {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>예시</td>
-              <td>1000</td>
-              <td>10</td>
-              <td><button>구매하기</button></td>
+            <tr class="product-purchase-item">
+              <td class="product-purchase-name" data-product-name="에시">예시</td>
+              <td class="product-purchase-price" data-product-price="1000">1000</td>
+              <td class="product-purchase-quantity" data-product-quantity="10">10</td>
+              <td><button class="purchase-button">구매하기</button></td>
             </tr>
-            <tr>
-              <td>테스트</td>
-              <td>1000</td>
-              <td>10</td>
-              <td><button>구매하기</button></td>
+            <tr class="product-purchase-item">
+              <td class="product-purchase-name" data-product-name="테스트">테스트</td>
+              <td class="product-purchase-price" data-product-price="1000">1000</td>
+              <td class="product-purchase-quantity" data-product-quantity="10">10</td>
+              <td><button class="purchase-button">구매하기</button></td>
             </tr>
           </tbody>
         </table>
       </div>
       <div>
         <h3>잔돈</h3>
-        <button>반환하기</button>
+        <button id="coin-return-button">반환하기</button>
         <table>
           <thead>
             <tr><th>동전</th><th>개수</th></tr>
@@ -53,25 +53,25 @@ export default class ProductPurchase extends Component {
           <tbody>
             <tr>
               <td>500원</td>
-              <td id="vending-machine-coin-500-quantity">
+              <td id="coin-500-quantity">
                 00
               </td>
             </tr>
             <tr>
               <td>100원</td>
-              <td id="vending-machine-coin-100-quantity">
+              <td id="coin-100-quantity">
                 00
               </td>
             </tr>
             <tr>
               <td>50원</td>
-              <td id="vending-machine-coin-50-quantity">
+              <td id="coin-50-quantity">
                 00
               </td>
             </tr>
             <tr>
               <td>10원</td>
-              <td id="vending-machine-coin-10-quantity">
+              <td id="coin-10-quantity">
                 00
               </td>
             </tr>
