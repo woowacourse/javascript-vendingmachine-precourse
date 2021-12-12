c6ee077c22cba0a902c8aef9c1a841e3cab15299
jswith <9geul@naver.com>
2021-12-12 16:45:42 +0900

    feat: [예외] 충전 금액이 10원으로 나누어 떨어지지 않는 경우

--- src/utils/changeValidator.js
+++ src/utils/changeValidator.js
@@ -6,8 +6,16 @@ class ChangeValidator {
     }
   }
 
+  static isMoneyInputDoesNotDivideBy10(moneyInput) {
+    if (moneyInput % 10) {
+      alert('10원으로 나누어 떨어지는 금액만 충전 가능합니다.');
+      return true;
+    }
+  }
+
   static isInvalidMoneyInput(moneyInput) {
     if (this.isMoneyInputLessThan10(moneyInput)) return true;
+    if (this.isMoneyInputDoesNotDivideBy10(moneyInput)) return true;
     return false;
   }
 }
