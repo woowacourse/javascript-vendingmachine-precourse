5afb4d5f120395ed894d3a3c839496c7741cb255
compy-ryu <94832610+compy-ryu@users.noreply.github.com>
2021-12-12 22:28:55 +0900

    refactor(`component`): 컴포넌트 구조 개선
    - 컨트롤러 영역 분리

---
+++ src/controller/VendingMachine.js
@@ -0,0 +1,78 @@
+import { ERROR } from '../constants/constants.js';
+import {
+  checkAmountVaild,
+  checkProductVaild,
+} from '../models/UserInputCheck.js';
+import { errorAlert } from '../utils/error-alert.js';
+import { removeTags } from '../utils/element-tools.js';
+
+import Coins from '../models/Coins.js';
+import Product from '../models/Product.js';
+
+import {
+  $productState,
+  $coinState,
+  $chargeState,
+} from '../components/StateList.js';
+
+export const handleAddCoins = (amount) => {
+  const inputAmount = Number(amount);
+
+  const resultCode = checkAmountVaild(inputAmount);
+  if (errorAlert(resultCode) === true) return false;
+
+  $coinState.value = new Coins($coinState.value).add(inputAmount).result;
+};
+
+export const handleAddProduct = (inputName, inputPrice, inputQuantity) => {
+  const product = {
+    name: removeTags(inputName),
+    price: Number(inputPrice),
+    quantity: Number(inputQuantity),
+  };
+
+  const resultCode = checkProductVaild(product);
+  if (errorAlert(resultCode) === true) return false;
+
+  $productState.value = new Product($productState.value).add(product).result;
+  return true;
+};
+
+export const handleTotalAmountCoins = () => new Coins($coinState.value).total;
+
+export const handlePutAmount = (amount) => {
+  const inputAmount = Number(amount);
+
+  const resultCode = checkAmountVaild(inputAmount);
+  if (errorAlert(resultCode) === true) return false;
+
+  $chargeState.value += inputAmount;
+};
+
+export const handlePurchase = (primaryKey) => {
+  const productManager = new Product($productState.value);
+  const { price } = productManager.itemInfo(primaryKey);
+
+  if ($chargeState.value < price) {
+    errorAlert(ERROR.PURCHASE_NEED_BALANCE);
+    return false;
+  }
+
+  $chargeState.value -= price;
+  $productState.value = productManager.purchase(primaryKey).result;
+};
+
+export const handleReturnAmount = () => {
+  if ($chargeState.value === 0) {
+    errorAlert(ERROR.RETURN_AMOUNT_NO_BALANCE);
+    return false;
+  }
+
+  const coinManager = new Coins($coinState.value);
+  const { output, failed } = coinManager.return($chargeState.value);
+
+  $chargeState.value = failed;
+  $coinState.value = coinManager.result;
+
+  return output;
+};
