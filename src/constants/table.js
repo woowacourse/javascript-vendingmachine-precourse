60d5e05307e66b8bfaab67d000e27376a2f0e6bd
Leejin-Yang <ejin1996@gmail.com>
2021-12-12 21:48:22 +0900

    feat: 테이블 헤더 상수화

---
+++ src/constants/table.js
@@ -0,0 +1,11 @@
+export const PRODUCT_TABLE = {
+  HEADS: ['상품명', '가격', '수량'],
+};
+
+export const COIN_TABLE = {
+  HEADS: ['동전', '개수'],
+};
+
+export const PRODUCT_PURCHASE_TABLE = {
+  HEADS: ['상품명', '가격', '수량', '구매'],
+};
