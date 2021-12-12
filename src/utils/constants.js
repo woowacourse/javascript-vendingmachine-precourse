4dd7d0937cf6413bf567ed6781571a97441b242c
ziho <ziho@kakao.com>
2021-12-12 16:43:00 +0900

    feat: 상수 추가 정의

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,15 +10,45 @@ export const ERROR_MESSAGE = Object.freeze({
   EMPTY: '값을 입력해주세요.'
 });
 
-export const SELECTOR = Object.freeze({
-  PRODUCT_ADD_MENU: '#product-add-menu',
-  VENDING_MACHINE_MANAGE_MENU: '#vending-machine-manage-menu',
-  PRODUCT_PURCHASE_MENU: '#product-purchase-menu',
-  PRODUCT_ADD_BUTTON: '#product-add-button',
-  VENDING_MACHINE_CHARGE_BUTTON: '#vending-machine-charge-button',
-  CHARGE_BUTTON: '#charge-button',
-  COIN_RETURN_BUTTON: '#coin-return-button',
-  PURCHASE_BUTTON: '.purchase-button'
+export const ID = Object.freeze({
+  MENU_COTAINER: 'menu-container',
+  MAIN_CONTAINER: 'main-container',
+  PRODUCT_ADD_MENU: 'product-add-menu',
+  VENDING_MACHINE_MANAGE_MENU: 'vending-machine-manage-menu',
+  PRODUCT_PURCHASE_MENU: 'product-purchase-menu',
+  PRODUCT_NAME_INPUT: 'product-name-input',
+  PRODUCT_PRICE_INPUT: 'product-price-input',
+  PRODUCT_QUANTITY_INPUT: 'product-quantity-input',
+  PRODUCT_ADD_BUTTON: 'product-add-button',
+  VENDING_MACHINE_CHARGE_INPUT: 'vending-machine-charge-input',
+  VENDING_MACHINE_CHARGE_BUTTON: 'vending-machine-charge-button',
+  VENDING_MACHINE_CHARGE_AMOUNT: 'vending-machine-charge-amount',
+  VENDING_MACHINE_COIN_500_QUANTITY: 'vending-machine-coin-500-quantity',
+  VENDING_MACHINE_COIN_100_QUANTITY: 'vending-machine-coin-100-quantity',
+  VENDING_MACHINE_COIN_50_QUANTITY: 'vending-machine-coin-50-quantity',
+  VENDING_MACHINE_COIN_10_QUANTITY: 'vending-machine-coin-10-quantity',
+  CHARGE_INPUT: 'charge-input',
+  CHARGE_AMOUNT: 'charge-amount',
+  CHARGE_BUTTON: 'charge-button',
+  COIN_RETURN_BUTTON: 'coin-return-button',
+  COIN_500_QUANTITY: 'coin-500-quantity',
+  COIN_100_QUANTITY: 'coin-100-quantity',
+  COIN_50_QUANTITY: 'coin-50-quantity',
+  COIN_10_QUANTITY: 'coin-10-quantity'
+});
+
+export const CLASS = Object.freeze({
+  PRODUCT_MANAGE_ITEM: 'product-manage-item',
+  PRODUCT_MANAGE_NAME: 'product-manage-name',
+  PRODUCT_MANAGE_PRICE: 'product-manage-price',
+  PRODUCT_MANAGE_QUANTITY: 'product-manage-quantity',
+  PURCHASE_BUTTON: 'purchase-button',
+  PRODUCT_PURCHASE_BUTTON: 'product-purchase-name',
+  PRODUCT_PURCHASE_PRICE: 'product-purchase-price',
+  PRODUCT_ADD_QUANTITY: 'product-purchase-quantity',
+  DATA_PRODUCT_NAME: 'data-product-name',
+  DATA_PRODUCT_PRICE: 'data-product-price',
+  DATA_PRODUCT_QUANTITY: 'data-product-quantity'
 });
 
 export const MINIMUM_PRICE = 100;
