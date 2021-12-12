2883b4a2b14190e5812c5ed09bd246db71ed97ba
ziho <ziho@kakao.com>
2021-12-13 00:50:08 +0900

    feat: 상품 구매탭 마운트 시에 localStorage에서 값 가져오기

--- src/components/Containers/PurchaseContainer.js
+++ src/components/Containers/PurchaseContainer.js
@@ -1,7 +1,14 @@
+import {STORAGE_KEY} from '../../utils/constants.js';
+import {getLocalStorage} from '../../utils/localStorage.js';
 import Component from '../core/Component.js';
 
 export default class PurchaseContainer extends Component {
-  template() {
-    return 'purchaseContainer';
+  init() {
+    this.$state = {
+      purchaseAmount: getLocalStorage(STORAGE_KEY.PURCHASE_CHARGE_AMOUNT, 0),
+      vendingMachineAmount: getLocalStorage(STORAGE_KEY.VENDING_MACHINE_CHARGE_AMOUNT, 0),
+      vendingMachinecoins: getLocalStorage(STORAGE_KEY.VENDING_MACHINE_CHARGE_COIN, {}),
+      products: getLocalStorage(STORAGE_KEY.PRODUCT_MANAGE, [])
+    };
   }
 }
