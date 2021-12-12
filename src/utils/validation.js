59717e5b17e7f1b87ca880b245dd08044baec6c4
ziho <ziho@kakao.com>
2021-12-12 20:43:14 +0900

    feat: 상품 관리탭 입력값 검증 함수 구현

---
+++ src/utils/validation.js
@@ -0,0 +1,71 @@
+import {DIVIDEING_NUM, ERROR_MESSAGE, MINIMUM_PRICE, STORAGE_KEY} from './constants.js';
+import {getLocalStorage} from './localStorage.js';
+
+export const isInputEmpty = (inputValue) => {
+  return inputValue.length < 1;
+};
+
+export const isInputNotPositive = (inputValue) => {
+  return inputValue <= 0;
+};
+
+export const isInputNotInteger = (inputValue) => {
+  return isNaN(inputValue) || !Number.isInteger(inputValue);
+};
+
+export const isNotMultipleOfTen = (inputValue) => {
+  return inputValue % DIVIDEING_NUM !== 0;
+};
+
+export const isLessThanMinimumPrice = (inputValue) => {
+  return inputValue < MINIMUM_PRICE;
+};
+
+export const isDuplicatedName = (inputValue) => {
+  const products = getLocalStorage(STORAGE_KEY.PRODUCT_MANAGE);
+  return products.map(({name}) => name).includes(inputValue);
+};
+
+export const isValidProductName = (inputValue) => {
+  if (isInputEmpty(inputValue)) {
+    alert(ERROR_MESSAGE.EMPTY);
+    return false;
+  }
+
+  if (isDuplicatedName(inputValue)) {
+    alert(ERROR_MESSAGE.DUPLICATED_NAME);
+    return false;
+  }
+
+  return true;
+};
+
+export const isValidProductPrice = (inputValue) => {
+  if (isInputEmpty(inputValue)) {
+    alert(ERROR_MESSAGE.EMPTY);
+    return false;
+  }
+  if (isInputNotInteger(inputValue) || isInputNotPositive(inputValue) || isLessThanMinimumPrice(inputValue)) {
+    alert(ERROR_MESSAGE.MINIMUM_PRICE);
+    return false;
+  }
+  if (isNotMultipleOfTen(inputValue)) {
+    alert(ERROR_MESSAGE.DIVIDEING_NUM);
+    return false;
+  }
+
+  return true;
+};
+
+export const isValidProductQuantity = (inputValue) => {
+  if (isInputEmpty(inputValue)) {
+    alert(ERROR_MESSAGE.EMPTY);
+    return false;
+  }
+  if (isInputNotInteger(inputValue) || isInputNotPositive(inputValue)) {
+    alert(ERROR_MESSAGE.MINIMUN_QUANTITY);
+    return false;
+  }
+
+  return true;
+};
