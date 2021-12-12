1a469c64258a9dc65480c98c87cc4819d42ea6bc
Chamy619 <yd95069506@naver.com>
2021-12-12 10:19:20 +0900

    feat: 자판기에 동전이 없을 경우 경고 메시지 보여주는 기능

--- src/event/attachReturnCoinEvent.js
+++ src/event/attachReturnCoinEvent.js
@@ -2,6 +2,15 @@ import { DOM_ID_SELECTOR, ERROR_MESSAGE } from '../constants.js';
 import printChargeAmount from '../dom/printChargeAmount.js';
 import printReturnCoinTable from '../dom/printReturnCoinTable.js';
 
+const isEmptyCoin = (vendingMachine) => {
+  if (vendingMachine.coin.getAmount() === 0) {
+    alert(ERROR_MESSAGE.lackOfCoin);
+    return true;
+  }
+
+  return false;
+};
+
 const isEmpty = (vendingMachine) => {
   if (vendingMachine.getMoney() === 0) {
     alert(ERROR_MESSAGE.noMoney);
@@ -14,7 +23,7 @@ const isEmpty = (vendingMachine) => {
 const attachReturnCoinEvent = (vendingMachine) => {
   const $coinReturnButton = document.getElementById(DOM_ID_SELECTOR.coinReturnButton);
   $coinReturnButton.addEventListener('click', () => {
-    if (!isEmpty(vendingMachine)) {
+    if (!isEmptyCoin(vendingMachine) && !isEmpty(vendingMachine)) {
       printReturnCoinTable(vendingMachine.returnCoin());
       printChargeAmount(vendingMachine.getMoney());
       vendingMachine.save();
