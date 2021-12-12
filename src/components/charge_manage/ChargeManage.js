ef9391c5dff81303d68468e5f9f8352528373cd5
intae92 <kangit2013@gmail.com>
2021-12-12 21:30:33 +0900

    feat: [추가] 누적 동전 개수 화면에 보여주는 컴포넌트 - 실시간으로 누적 동전 개수 노출시킨다 - 동전 단위를 붙인다

--- src/components/charge_manage/ChargeManage.js
+++ src/components/charge_manage/ChargeManage.js
@@ -1,5 +1,6 @@
 import Component from "../root/Component.js";
 import InputForm from "./InputForm.js";
+import CoinStatus from "./CoinStatus.js";
 import { getRandomChargeCoins, getSumCoins } from "../../utils/coin.js";
 
 import API from "../../libs/api.js";
@@ -18,46 +19,24 @@ export default class ChargeManage extends Component {
   template() {
     return `
       <div id="charge-input-form"></div>
-      <div id="coin-status">
-        <h3>자판기가 보유한 동전</h3>
-        <table>
-          <thead>
-            <tr><th>동전</th><th>개수</th></tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>500원</td>
-              <td id="vending-machine-coin-500-quantity">--</td>
-            </tr>
-            <tr>
-              <td>100원</td>
-              <td id="vending-machine-coin-100-quantity">--</td>
-            </tr>
-            <tr>
-              <td>50원</td>
-              <td id="vending-machine-coin-50-quantity">--</td>
-            </tr>
-            <tr>
-              <td>10원</td>
-              <td id="vending-machine-coin-10-quantity">--</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <div id="coin-status"></div>
     `;
   }
 
   mounted() {
     const {
       addChargeAmount,
-      $state: { chargeAmount },
+      $state: { chargeAmount, coins },
     } = this;
     const $chargeInputForm = document.querySelector("#charge-input-form");
+    const $coinStatus = document.querySelector("#coin-status");
 
     new InputForm($chargeInputForm, {
       addChargeAmount: addChargeAmount.bind(this),
       chargeAmount,
     });
+
+    new CoinStatus($coinStatus, { coins });
   }
 
   addChargeAmount(charge) {
@@ -66,15 +45,8 @@ export default class ChargeManage extends Component {
       newCoins: getRandomChargeCoins(charge),
     });
     const payload = { chargeAmount: this.$state.chargeAmount + charge, coins };
+
     this.callAPI.setVendingMachine(payload);
     this.setState(payload);
   }
 }
-
-// const test = { coin500: 10, coin100: 0, coin50: 20, coin10: 80 };
-
-//     for (let coin in this.$state.coins) {
-//       console.log("***", coin);
-//       test[coin] += this.$state.coins[coin];
-//     }
-//     const payload = { coins: { ...test } };
---
+++ src/components/charge_manage/CoinStatus.js
@@ -0,0 +1,52 @@
+import Component from "../root/Component.js";
+
+export default class CoinStatus extends Component {
+  setup() {
+    console.log("CoinStatus", this);
+  }
+
+  template() {
+    const {
+      coins: { coin500, coin100, coin50, coin10 },
+    } = this.$props;
+
+    return `
+    <h3>자판기가 보유한 동전</h3>
+        <table>
+          <thead>
+            <tr><th>동전</th><th>개수</th></tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>500원</td>
+              <td id="vending-machine-coin-500-quantity">
+                ${this.getCoinQuantityText(coin500)}
+              </td>
+            </tr>
+            <tr>
+              <td>100원</td>
+              <td id="vending-machine-coin-100-quantity">
+                ${this.getCoinQuantityText(coin100)}
+              </td>
+            </tr>
+            <tr>
+              <td>50원</td>
+              <td id="vending-machine-coin-50-quantity">
+                ${this.getCoinQuantityText(coin50)}
+              </td>
+            </tr>
+            <tr>
+              <td>10원</td>
+              <td id="vending-machine-coin-10-quantity">
+                ${this.getCoinQuantityText(coin10)}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+    `;
+  }
+
+  getCoinQuantityText(coin) {
+    return coin ? `${coin}개` : ``;
+  }
+}
