8d6cb9140bfe962f2e57eb0571065a7665e89070
hanyoonseok <hys6078@naver.com>
2021-12-12 14:31:34 +0900

    feat: 잔돈 충전 탭 충전금액에 따른 무작위 동전 생성 구현 무작위로 동전 생성 localstorage 갱신

--- src/model/vendingMachine.js
+++ src/model/vendingMachine.js
@@ -1,6 +1,11 @@
 export default class VendingMachine {
   constructor(changes) {
     this.changes = changes;
-    this.coins = [];
+    this.coins = [
+      { coin: 500, quantity: 0 },
+      { coin: 100, quantity: 0 },
+      { coin: 50, quantity: 0 },
+      { coin: 10, quantity: 0 },
+    ];
   }
 }
