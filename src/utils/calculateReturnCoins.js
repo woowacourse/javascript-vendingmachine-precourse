9160733a18e0890757b32e0906ed3df6ce31c090
minbr0ther <minbr0ther@naver.com>
2021-12-12 11:55:01 +0900

    feat: 투입한 금액이 잔돈보다 많을 경우 반환 금액을 차감하고 저장한다

--- src/utils/calculateReturnCoins.js
+++ src/utils/calculateReturnCoins.js
@@ -1,8 +1,20 @@
 import { default as DB } from '../model/database.js';
+import { default as UT } from './utils.js';
+import { default as DOM } from '../views/DOMUtils.js';
 
 const calculateReturnCoins = () => {
   const charge = DB.load('chargeToPurchaseProduct');
   const coins = DB.load('vendingMachineCoins');
+  const sumCoins = UT.calculateToCharge('vendingMachineCoins');
+
+  if (charge > sumCoins) {
+    DB.overwrite('chargeToPurchaseProduct', charge - sumCoins);
+
+    DOM.showChargeToPurchaseProduct();
+    DOM.showReturnCoins();
+
+    DB.overwrite('vendingMachineCoins', { coin500: 0, coin100: 0, coin50: 0, coin10: 0 });
+  }
 };
 
 export default calculateReturnCoins;
