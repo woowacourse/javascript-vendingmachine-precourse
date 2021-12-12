a1832b8aff267ec60ab8cc9802fbbd4acbc1fab7
hanyoonseok <hys6078@naver.com>
2021-12-12 15:04:21 +0900

    feat: 상품 구매 탭 구매 가능 상품 테이블 갱신 구현

--- src/controller/purchase.js
+++ src/controller/purchase.js
@@ -1,14 +1,25 @@
-import { KEY, SELECTOR, COIN_ARRAY } from '../model/constants.js';
 import {
   $,
+  getItemOrArray,
   getItemOrNull,
   setItem,
   isMultipleOf10,
   isInputNumberValid,
   onKeyUpNumericEvent,
 } from './utils.js';
+import { KEY, SELECTOR, COIN_ARRAY } from '../model/constants.js';
+import { productPurchaseTableRow } from '../model/dom.js';
 import VendingMachine from '../model/vendingMachine.js';
 
+const addTableRow = (table, product) =>
+  table.insertAdjacentHTML('beforeend', productPurchaseTableRow(product));
+
+const initProductStatusTable = () => {
+  const table = document.querySelector('tbody');
+  const allProducts = getItemOrArray(KEY.product);
+  allProducts.forEach(product => addTableRow(table, product));
+};
+
 export const initAllPurchase = () => {
-  alert('a');
+  initProductStatusTable();
 };
