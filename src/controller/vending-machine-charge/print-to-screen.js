d90ff24512093392bbabe27ed9e198dc23eab786
leezer94 <2kunhee94@gmail.com>
2021-12-13 00:22:29 +0900

    feat: 무작위로 동전 갯수 프린트 하는 함수 구현 전역변수 설정시 새로고침 해도 화면에 프린트 된 값이 나오나 이벤트 안 함수 발동시에만 프린트 되는 기능 고치기

--- src/controller/vending-machine-charge/print-to-screen.js
+++ src/controller/vending-machine-charge/print-to-screen.js
@@ -1,4 +1,5 @@
 import { $ } from '../../dom/dom.js';
+import { getRandomCoinsAmongList } from '../../utils.js';
 
 export const printMoneyToScreen = () => {
   const chargedAmountList = JSON.parse(localStorage.getItem('chargedAmount'));
@@ -12,4 +13,23 @@ export const printMoneyToScreen = () => {
 
   sum = amountArray.reduce((a, b) => a + b);
   $currentMachineMoney.innerHTML = `보유금액: ${sum}원`;
+
+  return sum;
 };
+
+export const printAmountOfCoinToScreen = () => {
+  const sum = printMoneyToScreen();
+  const randomCoinAmount = getRandomCoinsAmongList(sum);
+  const $vendingMachine500Coin = $('#vending-machine-coin-500-quantity');
+  const $vendingMachine100Coin = $('#vending-machine-coin-100-quantity');
+  const $vendingMachine50Coin = $('#vending-machine-coin-50-quantity');
+  const $vendingMachine10Coin = $('#vending-machine-coin-10-quantity');
+
+  $vendingMachine500Coin.innerHTML = `${randomCoinAmount['500']}개`;
+  $vendingMachine100Coin.innerHTML = `${randomCoinAmount['100']}개`;
+  $vendingMachine50Coin.innerHTML = `${randomCoinAmount['50']}개`;
+  $vendingMachine10Coin.innerHTML = `${randomCoinAmount['10']}개`;
+};
+
+// 전역으로 설정시 새로고침해도 보이고, 버튼이벤트 안 설정시 이벤트가 작동해야지 보인다.
+//즉 , 전역변수 설정 하거나 없이도 새로고침했을때 나타날 수 있는 방법 고민
