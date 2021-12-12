cbd86615ae3a15a69b6e91c195dfba947a8ece41
gytks4 <gytks4@naver.com>
2021-12-12 19:01:16 +0900

    feat: 가격 100원 이상만 입력되도록 하는 함수 구현

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,4 +2,5 @@ export const coinList = [500, 100, 50, 10];
 
 export const USER_INPUT_ALERT = {
   blankNameError: '자동차 이름이 비어있습니다.',
+  over100Error: '100원 이상으로 입력해주세요.',
 };
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -13,3 +13,17 @@ export const ProductNameCheckMethods = [
 export function isProductNameValid(value) {
   return ProductNameCheckMethods.every((ProductNameCheckMethod) => ProductNameCheckMethod(value));
 }
+
+export const PriceCheckMethods = [
+  (value) => {
+    const isOver100 = value < 100;
+    if (isOver100) {
+      alert(USER_INPUT_ALERT.over100Error);
+    }
+    return !isOver100;
+  },
+];
+
+export function isPriceValid(value) {
+  return PriceCheckMethods.every((PriceCheckMethod) => PriceCheckMethod(value));
+}
