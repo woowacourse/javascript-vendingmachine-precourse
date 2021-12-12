c14bb35539af55e727635f4ed159b53d01311d9b
leezer94 <2kunhee94@gmail.com>
2021-12-12 23:58:21 +0900

    feat: 랜덤함수 이용 무작위 코인 생성 랜덤함수 이용 무작위로 코인 생성하는 함수 구현

--- src/utils.js
+++ src/utils.js
@@ -6,14 +6,16 @@ export const getRandomCoins = () => {
   return randomCoin;
 };
 
-export const insertRandomAmountCoins = (amount) => {
-  const moneyCharged = coinChargeInputValue();
-  let count = 0;
-  const randomCoin = getRandomCoins();
-  amount = 3000;
+export const getRandomCoinsAmongList = (money) => {
+  const coinList = { 500: 0, 100: 0, 50: 0, 10: 0 };
 
-  count += Math.floor(amount / randomCoin);
-  amount -= Math.floor(amount / randomCoin);
+  while (money) {
+    const coin = getRandomCoins();
+    if (money - coin < 0) continue;
 
-  return count;
+    money -= coin; // 빼준 coin number ++
+    coinList[coin]++;
+  }
+  console.log(coinList);
+  return coinList;
 };
