d8c057c9801e4c0bb4238b137fc6d0b4bf4ad720
Jungjjeong <sky990106@dgu.ac.kr>
2021-12-12 19:44:18 +0900

    feat: 상품 관리 기능을 위한 ProductAdd html Class 생성

---
+++ src/elements/ProductAdd.js
@@ -0,0 +1,9 @@
+export default class ProductAdd {
+  constructor() {
+    this.nameInput = document.querySelector('#product-name-input');
+    this.priceInput = document.querySelector('#product-price-input');
+    this.quantityInput = document.querySelector('#product-quantity-input');
+    this.submit = document.querySelector('#product-add-button');
+    this.tableBody = document.querySelector('#product-manage-item');
+  }
+}
