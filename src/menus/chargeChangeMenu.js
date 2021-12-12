ea0a72b3585d61a046635665c8ec0a6560efa784
marble <kangsw1025@naver.com>
2021-12-13 00:11:24 +0900

    fix: 잘못된 사용된 변수 수정

--- src/menus/chargeChangeMenu.js
+++ src/menus/chargeChangeMenu.js
@@ -1,15 +1,11 @@
 import { vendingMachine } from "../components/vendingMachine.js";
-import {
-  ID,
-  IS_RENDERED_CHARGE_TAP,
-  PURCHASE_TAP,
-} from "../utils/constants.js";
+import { ID, IS_RENDERED_CHARGE_TAP, CHARGE_TAP } from "../utils/constants.js";
 import { saveToLocalStorage } from "../utils/utils.js";
 import { checkChargeChangeInput } from "../utils/validation.js";
 import { renderChangeAmount, renderCoins } from "../views/chargeChangeView.js";
 
 const resetInput = form => {
-  const input = form.querySelector(`#${PURCHASE_TAP.chargeInput[1]}`);
+  const input = form.querySelector(`#${CHARGE_TAP.chargeInput[ID]}`);
   input.value = "";
 };
 
@@ -21,7 +17,7 @@ export const onClickChargeButton = event => {
   event.preventDefault();
   const form = event.target.parentElement;
   const chargeInput = form.querySelector(
-    `#${PURCHASE_TAP.chargeInput[ID]}`
+    `#${CHARGE_TAP.chargeInput[ID]}`
   ).value;
 
   if (checkChargeChangeInput(chargeInput)) {
