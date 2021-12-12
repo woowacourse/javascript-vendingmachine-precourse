03a9d3d6cbf1b7c9a2a11fd2629db43932e2eb81
Seungjae Lee <sjlee4108@gmail.com>
2021-12-12 18:54:36 -0500

    feat: create coinManageMenu container class

---
+++ src/components/coinManageMenuContainer.js
@@ -0,0 +1,46 @@
+import Component from "../common/component.js";
+import EntrySection from "./entrySection.js";
+import Table from "./table.js";
+
+export default class CoinManageMenuContainer extends Component {
+  template() {
+    return `
+      <div id="product-add-menu-entry"></div>
+      <div id="product-add-menu-table"></div>
+    `;
+  }
+
+  componentDidMount() {
+    const $entrySelector = this.$target.querySelector(
+      "#product-add-menu-entry"
+    );
+    const $tableSelector = this.$target.querySelector(
+      "#product-add-menu-table"
+    );
+
+    new EntrySection($entrySelector, {
+      title: "금액 투입",
+      inputInfo: [
+        {
+          type: "number",
+          id: "vending-machine-charge-input",
+          placeholder: "투입할 금액",
+        },
+      ],
+      buttonInfo: {
+        id: "vending-machine-charge-button",
+        value: "충전하기",
+        callBack: () => alert("it worked"),
+      },
+      moneyInfo: {
+        id: "vending-machine-charge-amount",
+        type: "보유",
+      },
+    });
+
+    new Table($tableSelector, {
+      title: "자판기가 보유한 동전",
+      tableHeaders: ["동전", "개수"],
+    });
+  }
+}
