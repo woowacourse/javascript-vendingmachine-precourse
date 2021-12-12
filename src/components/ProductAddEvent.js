7c9d982b02dfde565e0d0d0ad5503348bdd46656
kevin7498 <kevin7498@naver.com>
2021-12-12 10:12:28 +0900

    refactor: 함수 순서 변경

--- src/components/ProductAddEvent.js
+++ src/components/ProductAddEvent.js
@@ -5,7 +5,41 @@ import ProductAddView from "./ProductAddView.js";
 export default class ProductAddEvent {
     static addEvent() {
         this.addProductAddEvent();
+    }
 
+    static checkAlreadyHave(name, price, quantity, product) {
+        if(product[name] !== undefined && product[name][VALUES][0] === price) {
+            return parseInt(product[name][VALUES][1]) + parseInt(quantity);
+        }
+
+        return quantity;
+    }
+
+    static storeProduct(name, price, quantity) {
+        const product = JSON.parse(localStorage.getItem(PRODUCT));
+
+        if(localStorage.getItem(PRODUCT) === null) {
+            localStorage.setItem(PRODUCT, JSON.stringify({[name]:{values: [price, quantity]}}));
+        } else {
+            product[name] = {values: [price, this.checkAlreadyHave(name, price, quantity, product)]};
+            localStorage.setItem(PRODUCT, JSON.stringify(product));
+        }
+    }
+
+    static makeBlank() {
+        document.getElementById('product-name-input').value = "";
+        document.getElementById('product-price-input').value = "";
+        document.getElementById('product-quantity-input').value = "";
+    }
+
+    static checkResult(name, price, quantity, productAddCheck) {
+        if(productAddCheck.checkAll()) {
+            this.storeProduct(name, price, quantity);
+            ProductAddView.showTable();
+            this.makeBlank();
+        } else {
+            alert(ERROR_MESSAGE);
+        }
     }
 
     static addProductAddEvent() {
@@ -18,40 +52,5 @@ export default class ProductAddEvent {
     
             this.checkResult(name, price, quantity, productAddCheck);
         });
-      }
-    
-      static checkResult(name, price, quantity, productAddCheck) {
-        if(productAddCheck.checkAll()) {
-            this.storeProduct(name, price, quantity);
-            ProductAddView.showTable();
-            this.makeBlank();
-        } else {
-            alert(ERROR_MESSAGE);
-        }
-      }
-    
-        static storeProduct(name, price, quantity) {
-            const product = JSON.parse(localStorage.getItem(PRODUCT));
-    
-            if(localStorage.getItem(PRODUCT) === null) {
-                localStorage.setItem(PRODUCT, JSON.stringify({[name]:{values: [price, quantity]}}));
-            } else {
-                product[name] = {values: [price, this.checkAlreadyHave(name, price, quantity, product)]};
-                localStorage.setItem(PRODUCT, JSON.stringify(product));
-            }
-        }
-    
-        static checkAlreadyHave(name, price, quantity, product) {
-            if(product[name] !== undefined && product[name][VALUES][0] === price) {
-                return parseInt(product[name][VALUES][1]) + parseInt(quantity);
-            }
-    
-            return quantity;
-        }
-    
-        static makeBlank() {
-            document.getElementById('product-name-input').value = "";
-            document.getElementById('product-price-input').value = "";
-            document.getElementById('product-quantity-input').value = "";
-        }
+    }
 }
