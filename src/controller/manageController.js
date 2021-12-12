11767e00c493c424f77a7ca5a56fdc634119b6a2
jswith <9geul@naver.com>
2021-12-12 11:59:30 +0900

    feat: 상품을 추가하면 상품 객체를 생성한다.

--- src/controller/manageController.js
+++ src/controller/manageController.js
@@ -1,6 +1,20 @@
+import Item from '../model/item.js';
+import { $ } from '../utils/dom.js';
+
 class ManageController {
   constructor(view) {
     this.view = view;
+    this.bindEvent();
+  }
+
+  addItem() {
+    const { name, price, quantity } = this.view.getInput();
+    const newItem = new Item(name, price, quantity);
+    console.log(newItem);
+  }
+
+  bindEvent() {
+    $('#product-add-button').addEventListener('click', this.addItem.bind(this));
   }
 }
 
