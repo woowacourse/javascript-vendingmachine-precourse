43dd759ff31bd5a118cef97c31e8d70bbdc08e40
정현수 <jung660317@naver.com>
2021-12-12 21:51:32 +0900

    refactor: ProductAddMenuController 함수 분리
    - onClickProductAddButton 메소드의 로직을 여러 개의 메소드로 분리

--- src/controllers/ProductAddMenuController.js
+++ src/controllers/ProductAddMenuController.js
@@ -1,9 +1,8 @@
-import ProductAddMenuModel from '../models/ProductAddMenuModel.js';
-import ProductAddMenuView from '../views/ProductAddMenuView.js';
-import ProductAddMenuValidator from '../validators/productAddMenu.js';
-import { $ } from '../utils/dom.js';
-
 import SELECTOR from '../constants/selector.js';
+import ProductAddMenuModel from '../models/ProductAddMenuModel.js';
+import { $ } from '../utils/dom.js';
+import ProductAddMenuValidator from '../validators/productAddMenu.js';
+import ProductAddMenuView from '../views/ProductAddMenuView.js';
 
 class ProductAddMenuController {
   constructor(currentMenu) {
@@ -39,29 +38,52 @@ class ProductAddMenuController {
     const productPrice = $(`#${SELECTOR.productPriceInputId}`).value;
     const productQuantity = $(`#${SELECTOR.productQuantityInputId}`).value;
 
-    if (!ProductAddMenuValidator.validateProductNameExist(productName)) return;
-    if (
-      !ProductAddMenuValidator.validateProductPriceIsOver100(productPrice) ||
-      !ProductAddMenuValidator.validateProductPriceCanDivide10(productPrice)
-    )
-      return;
-    if (
-      !ProductAddMenuValidator.validateProductQuantityExist(productQuantity) ||
-      !ProductAddMenuValidator.validateProductQuantityPlusInteger(productQuantity)
-    )
-      return;
+    if (!this.validateProductItem(productName, productPrice, productQuantity)) return;
+    this.addProductItemInPresentList(productName, productPrice, productQuantity);
+    this.renderWithProductItems();
+  }
 
-    const item = {
-      productName,
-      productPrice,
-      productQuantity,
-    };
-    this.$productAddMenuModel.setProductItems([
-      ...this.$productAddMenuModel.getProductItems(),
-      item,
-    ]);
+  renderWithProductItems() {
     this.$productAddMenuView.renderTableWithProductItems(
       this.$productAddMenuModel.getProductItems(),
+    );
+  }
+
+  addProductItemInPresentList(name, price, quantity) {
+    const addItem = {
+      productName: name,
+      productPrice: price,
+      productQuantity: quantity,
+    };
+
+    this.$productAddMenuModel.setProductItems([
+      ...this.$productAddMenuModel.getProductItems(),
+      addItem,
+    ]);
+  }
+
+  validateProductItem(name, price, quantity) {
+    if (!this.validateProductName(name)) return false;
+    if (!this.validateProductPrice(price)) return false;
+    if (!this.validateProductQuantity(quantity)) return false;
+    return true;
+  }
+
+  validateProductName(productName) {
+    return ProductAddMenuValidator.validateProductNameExist(productName);
+  }
+
+  validateProductPrice(productPrice) {
+    return (
+      ProductAddMenuValidator.validateProductPriceIsOver100(productPrice) &&
+      ProductAddMenuValidator.validateProductPriceCanDivide10(productPrice)
+    );
+  }
+
+  validateProductQuantity(productQuantity) {
+    return (
+      ProductAddMenuValidator.validateProductQuantityExist(productQuantity) &&
+      ProductAddMenuValidator.validateProductQuantityPlusInteger(productQuantity)
     );
   }
 }
