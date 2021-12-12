14959186e06713f12e51e6ecaa74f90784adea49
leezer94 <2kunhee94@gmail.com>
2021-12-13 03:23:46 +0900

    feat: 잔돈 반환 함수 구현 잔돈 반환 함수 구현

--- src/common/utils.js
+++ src/common/utils.js
@@ -17,3 +17,18 @@ export const getRandomCoinsAmongList = (money) => {
 
   return coinList;
 };
+
+export const returnChangesinCoins = (money, coinList) => {
+  const exchangeCoin = { 500: 0, 100: 0, 50: 0, 10: 0 };
+  const changesList = [500, 100, 50, 10];
+
+  for (const coin of changesList) {
+    for (let idx = 0; idx < coinList[coin]; idx++) {
+      if (money - coin < 0) break;
+      money -= coin;
+      exchangeCoin[coin]++;
+    }
+  }
+
+  return exchangeCoin;
+};
