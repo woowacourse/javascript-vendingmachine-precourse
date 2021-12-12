5ec95c589a3b2294a75cb7046a16cef7a9d4e601
jswith <9geul@naver.com>
2021-12-12 14:40:51 +0900

    style: 잔돈 충전 탭 마크업 수정

--- src/view/changeView.js
+++ src/view/changeView.js
@@ -5,19 +5,22 @@ class ChangeView {
 
   template() {
     return `
-      <section id="change-tab">
-        <form>
-          <h3>자판기 동전 충전하기</h3>
-          <input id="vending-machine-charge-input" type="number" placeholder="자판기가 보유할 금액" />
-          <button id="vending-machine-charge-button">충전하기</button>
-          <div>보유 금액: <span id="vending-machine-charge-amount">0원</span></div>
-        </form>
-        <h3>자판기가 보유한 동전</h3>
-        <table>
+    <section id="change-tab">
+      <form>
+        <h3>자판기 동전 충전하기</h3>
+        <input id="vending-machine-charge-input" type="number" placeholder="자판기가 보유할 금액" />
+        <button id="vending-machine-charge-button">충전하기</button>
+        <div>보유 금액: <span id="vending-machine-charge-amount">0원</span></div>
+      </form>
+      <h3>자판기가 보유한 동전</h3>
+      <table>
+        <thead>
           <tr>
             <th>동전</th>
             <th>개수</th>
           </tr>
+        </thead>
+        <tbody>
           <tr>
             <td>500원</td>
             <td id="vending-machine-coin-500-quantity">0개</td>
@@ -34,8 +37,9 @@ class ChangeView {
             <td>10원</td>
             <td id="vending-machine-coin-10-quantity">0개</td>
           </tr>
-        </table>
-      </section>
+        </tbody>
+      </table>
+    </section>
       `;
   }
 }
