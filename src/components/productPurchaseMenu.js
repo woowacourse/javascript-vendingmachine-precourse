a3dc48f85c81575507951ee9b57f942e802bfa0b
Leejin-Yang <ejin1996@gmail.com>
2021-12-12 21:56:04 +0900

    feat: 동전 테이블 구현

--- src/components/productPurchaseMenu.js
+++ src/components/productPurchaseMenu.js
@@ -9,6 +9,9 @@ import {
   Span,
   Table,
   TableHead,
+  TableRow,
+  TableData,
+  TableDataWithId,
 } from './elements.js';
 
 const onClickAddMoneyButton = (e) => {
@@ -40,9 +43,25 @@ const createProductPurchaseTable = () => {
   return productPurchaseTable;
 };
 
+const createCoinRow = (coin, id) => {
+  const tableRow = TableRow();
+  const coinNameData = TableData(coin);
+  const coinQuantityData = TableDataWithId('', id);
+
+  tableRow.append(coinNameData, coinQuantityData);
+
+  return tableRow;
+};
+
 const createCoinTable = () => {
   const coinTable = Table();
+  const coin500Row = createCoinRow('500원', ID.COIN_500);
+  const coin100Row = createCoinRow('100원', ID.COIN_100);
+  const coin50Row = createCoinRow('50원', ID.COIN_50);
+  const coin10Row = createCoinRow('10원', ID.COIN_10);
+
   TableHead(coinTable, COIN_TABLE.HEADS);
+  coinTable.append(coin500Row, coin100Row, coin50Row, coin10Row);
 
   return coinTable;
 };
--- src/components/vendingMachineManageMenu.js
+++ src/components/vendingMachineManageMenu.js
@@ -9,6 +9,9 @@ import {
   Span,
   Table,
   TableHead,
+  TableRow,
+  TableData,
+  TableDataWithId,
 } from './elements.js';
 
 const onClickAddCoinButton = (e) => {
@@ -33,9 +36,25 @@ const createAddCoinForm = () => {
   return addCoinForm;
 };
 
+const createCoinRow = (coin, id) => {
+  const tableRow = TableRow();
+  const coinNameData = TableData(coin);
+  const coinQuantityData = TableDataWithId('', id);
+
+  tableRow.append(coinNameData, coinQuantityData);
+
+  return tableRow;
+};
+
 const createCoinTable = () => {
   const coinTable = Table();
+  const coin500Row = createCoinRow('500원', ID.VENDING_MACHINE_COIN_500);
+  const coin100Row = createCoinRow('100원', ID.VENDING_MACHINE_COIN_100);
+  const coin50Row = createCoinRow('50원', ID.VENDING_MACHINE_COIN_50);
+  const coin10Row = createCoinRow('10원', ID.VENDING_MACHINE_COIN_10);
+
   TableHead(coinTable, COIN_TABLE.HEADS);
+  coinTable.append(coin500Row, coin100Row, coin50Row, coin10Row);
 
   return coinTable;
 };
