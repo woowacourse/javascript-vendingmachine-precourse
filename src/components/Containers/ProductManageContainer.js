25a29a9f447cf2bfb9c5c72679ff0c0d2f527e68
ziho <ziho@kakao.com>
2021-12-12 20:16:53 +0900

    feat: 상품 추가하기 템플릿 작성

--- src/components/Containers/ProductManageContainer.js
+++ src/components/Containers/ProductManageContainer.js
@@ -1,7 +1,39 @@
+import {ID, STORAGE_KEY} from '../../utils/constants.js';
+import {createInputElement} from '../../utils/domUtil.js';
+import {getLocalStorage} from '../../utils/localStorage.js';
 import Component from '../core/Component.js';
 
 export default class ProductManageContainer extends Component {
+  init() {
+    this.$state = {products: getLocalStorage(STORAGE_KEY.PRODUCT_MANAGE)};
+  }
+
+  mounted() {
+    this.$state = {products: getLocalStorage(STORAGE_KEY.PRODUCT_MANAGE)};
+    this.$target.querySelector(`.product-manage-table-container`).innerHTML = this.printProductTable();
+  }
+
   template() {
-    return 'productManage';
+    return `
+    <div>
+      <h2>상품 추가하기</h2>
+      ${this.printProductForm()}
+    </div>
+    <div>
+      <h2>상품 현황</h2>
+      <div class="product-manage-table-container"></div>
+    </div>
+    `;
+  }
+
+  printProductForm() {
+    return `
+        <form id="product-manage-form">
+          ${createInputElement('text', ID.PRODUCT_NAME_INPUT, '상품명')}
+          ${createInputElement('number', ID.PRODUCT_PRICE_INPUT, '가격')}
+          ${createInputElement('number', ID.PRODUCT_QUANTITY_INPUT, '수량')}
+          <button id=${ID.PRODUCT_ADD_BUTTON}>추가하기</button>
+        </form>
+    `;
   }
 }
