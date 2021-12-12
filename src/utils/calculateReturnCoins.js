11d47d8ec16c7f332410f2a3d47aba380a15f10a
minbr0ther <minbr0ther@naver.com>
2021-12-12 14:49:15 +0900

    feat: 그리디 알고리즘을 사용해서 현재 보유한 최소 개수의 동전으로 잔돈을 돌려준다

--- src/utils/calculateReturnCoins.js
+++ src/utils/calculateReturnCoins.js
@@ -6,15 +6,32 @@ const calculateReturnCoins = () => {
   const charge = DB.load('chargeToPurchaseProduct');
   const wallet = DB.load('vendingMachineCoins');
   const sumCoins = UT.calculateToCharge('vendingMachineCoins');
+  const newWallet = { coin500: 0, coin100: 0, coin50: 0, coin10: 0 };
 
-  if (charge >= sumCoins) {
-    DB.overwrite('chargeToPurchaseProduct', charge - sumCoins);
+  //   if (charge >= sumCoins) {
+  //     DB.overwrite('chargeToPurchaseProduct', charge - sumCoins);
 
-    DOM.showChargeToPurchaseProduct();
-    DOM.showReturnCoins();
+  //     DOM.showChargeToPurchaseProduct();
+  //     DOM.showReturnCoins();
 
-    return DB.overwrite('vendingMachineCoins', { coin500: 0, coin100: 0, coin50: 0, coin10: 0 });
-  }
+  //     return DB.overwrite('vendingMachineCoins', newWallet);
+  //   }
+  useGreedyArgorithm(charge, wallet, newWallet);
+};
+
+const useGreedyArgorithm = (charge, wallet, newWallet) => {
+  const tryCaseByCoinType = coinType => {
+    while (charge >= coinType && wallet['coin' + coinType] > 0) {
+      wallet['coin' + coinType] -= 1;
+      newWallet['coin' + coinType] += 1;
+      charge -= coinType;
+    }
+  };
+
+  [500, 100, 50, 10].forEach(coinType => tryCaseByCoinType(coinType));
+
+  console.log(wallet);
+  console.log(newWallet);
 };
 
 export default calculateReturnCoins;
