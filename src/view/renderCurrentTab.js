762e8ec028afeaec615a1f3ebd8459b53a7002ba
DomMorello <zuzudnf@gmail.com>
2021-12-12 21:19:45 +0900

    fix: renderCurrentTab 이벤트핸들러 추가
    현재탭을 보여줄 때 이벤트핸들러를 등록하지 않아서
    버튼을 눌러도 이벤트가 발생하지 않는 버그 수정

--- src/view/renderCurrentTab.js
+++ src/view/renderCurrentTab.js
@@ -4,6 +4,9 @@ import {
   TAB_CHARGE_ID,
   TAB_PURCHASE_ID,
 } from '../constant/constant.js';
+import addProductHandler from '../eventHandler/addProductHandler.js';
+import chargeHandler from '../eventHandler/chargeHandler.js';
+import purchaseHandler from '../eventHandler/purchaseHandler.js';
 import renderCharge from './renderCharge.js';
 import renderProductAdd from './renderProductAdd.js';
 import renderPurchase from './renderPurchase.js';
@@ -13,11 +16,14 @@ export default function renderCurrentTab(vendingMachine) {
 
   if (id === null || id === TAB_PRODUCT_ADD_ID) {
     renderProductAdd(vendingMachine);
+    addProductHandler(vendingMachine);
   }
   if (id === TAB_CHARGE_ID) {
     renderCharge(vendingMachine);
+    chargeHandler(vendingMachine);
   }
   if (id === TAB_PURCHASE_ID) {
     renderPurchase(vendingMachine);
+    purchaseHandler(vendingMachine);
   }
 }
