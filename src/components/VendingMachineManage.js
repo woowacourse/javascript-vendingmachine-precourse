0fc85654703c8cdba0268ad187513336d9d17e00
heejaykong <heejay.dev@gmail.com>
2021-12-12 16:54:00 +0900

    feat: [잔돈충전탭] 자판기 보유금액 그리기 구현

--- src/components/VendingMachineManage.js
+++ src/components/VendingMachineManage.js
@@ -17,7 +17,10 @@ export default class VendingMachineManage {
   init = () => {
     this.paintTemplate();
     this.loadCharges();
+
+    this.paintHoldingAmount();
     this.paintLoadedCharges();
+
     $('form').addEventListener('submit', this.handleVendingMachineChargeSubmit);
   };
 
@@ -35,13 +38,31 @@ export default class VendingMachineManage {
     return;
   };
 
+  paintHoldingAmount = () => {
+    let holdingAmount = 0;
+    this.charges.map(({ coinType, quantity }) => {
+      holdingAmount += coinType * quantity;
+    });
+    $(`#${ID.VENDING_MACHINE_CHARGE_AMOUNT}`).innerHTML = `${holdingAmount}원`;
+  };
+
   paintLoadedCharges = () => {
-    this.charges.map((coinType) => console.log(coinType));
+    this.charges.map(({ coinType, quantity }) => {
+      if (coinType === 500)
+        $(`#${ID.VENDING_MACHINE_COIN_500_QUANTITY}`).innerHTML = `${quantity}개`;
+      if (coinType === 100)
+        $(`#${ID.VENDING_MACHINE_COIN_100_QUANTITY}`).innerHTML = `${quantity}개`;
+      if (coinType === 50)
+        $(`#${ID.VENDING_MACHINE_COIN_50_QUANTITY}`).innerHTML = `${quantity}개`;
+      if (coinType === 10)
+        $(`#${ID.VENDING_MACHINE_COIN_10_QUANTITY}`).innerHTML = `${quantity}개`;
+    });
   };
 
   handleVendingMachineChargeSubmit = (e) => {
     e.preventDefault();
     const charge = $(`#${ID.VENDING_MACHINE_CHARGE_INPUT}`).value;
+
     console.log(charge);
     this.clearInputs();
   };
