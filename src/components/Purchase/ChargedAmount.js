0f86f998db9dad0fe681b1ba57b46d85bc654f37
moonheekim0118 <hellomooneekim@gmail.com>
2021-12-12 11:15:48 +0900

    feat: (상품구매탭) 사용자가 투입한 금액을 화면에 출력한다.

---
+++ src/components/Purchase/ChargedAmount.js
@@ -0,0 +1,13 @@
+import Component from '../../core/Component.js';
+import UserStore from '../../stores/UserStore.js';
+
+export default class ChargedAmount extends Component {
+  getGlobalState() {
+    return UserStore.getState();
+  }
+
+  render() {
+    const { chargedMoney } = this.getGlobalState();
+    this.$container.innerHTML = `투입한 금액: ${chargedMoney}원`;
+  }
+}
--- src/components/Purchase/index.js
+++ src/components/Purchase/index.js
@@ -1,9 +1,13 @@
 import Component from '../../core/Component.js';
 import { newElement } from '../../utils/dom.js';
 import Form from './Form.js';
+import ChargedAmount from './ChargedAmount.js';
 
 export default class Purchase extends Component {
   initChildren() {
-    this.children = [new Form(newElement('<form id="charge-money"/>'))];
+    this.children = [
+      new Form(newElement('<form id="charge-money"/>')),
+      new ChargedAmount(newElement('<span id="charge-amount"/>')),
+    ];
   }
 }
