f5482ab932971462e182a8b7907b1c8aef30edd9
KIM YEJI <rladpwl0512@gmail.com>
2021-12-12 23:13:30 +0900

    refactor: 상품 구매 후, 남은 보유 금액과 상품 개수 업데이트하는 로직을 함수로 분리
    updateAmount, updateProductQuantity를 통해 남은 보유 금액과 상품 개수를 업데이트할 수 있다.

--- src/js/controllers/handleProductPurchase.js
+++ src/js/controllers/handleProductPurchase.js
@@ -11,6 +11,24 @@ function HandleProductPurchase() {
     renderProducts();
   }
 
+  const updateAmount = price => {
+    this.amount -= price; // 아래코드까지, amount를 업데이트해주는 함수로 변환하기
+    printInputCharge(this.amount);
+  };
+
+  const updateProductQuantity = e => {
+    const purchaseIndex = e.target.closest('.product-purchase-item').querySelector('.product-purchase-quantity').dataset.productQuantity;
+    const products = store.getLocalStorage('products'); // 따로 파일로 빼주기 (현재 상태 가져옴)
+
+    if (products[purchaseIndex].quantity) {
+      products[purchaseIndex].quantity -= 1;
+      store.setLocalStorage('products', products);
+      e.target.closest('.product-purchase-item').querySelector('.product-purchase-quantity').innerText -= 1;
+      return;
+    }
+    alert('상품이 없습니다.');
+  };
+
   // (1) 금액 투입 기능
   $('#charge-button').addEventListener('click', e => {
     e.preventDefault();
@@ -28,21 +46,10 @@ function HandleProductPurchase() {
   $('#product-purchase-list').addEventListener('click', e => {
     if (e.target.className === 'purchase-button') {
       const price = Number(e.target.closest('.product-purchase-item').querySelector('.product-purchase-price').innerText);
-      if (isValidPurchase(this.amount, price)) {
-        // 보유 금액 업데이트
-        this.amount -= price; // 아래코드까지, amount를 업데이트해주는 함수로 변환하기
-        printInputCharge(this.amount);
 
-        // 수량 업데이트
-        const purchaseIndex = e.target.closest('.product-purchase-item').querySelector('.product-purchase-quantity').dataset.productQuantity;
-        const products = store.getLocalStorage('products'); // 따로 파일로 빼주기 (현재 상태 가져옴)
-        if (products[purchaseIndex].quantity) {
-          products[purchaseIndex].quantity -= 1;
-          store.setLocalStorage('products', products);
-          e.target.closest('.product-purchase-item').querySelector('.product-purchase-quantity').innerText -= 1;
-          return;
-        }
-        alert('상품이 없습니다.');
+      if (isValidPurchase(this.amount, price)) {
+        updateAmount(price);
+        updateProductQuantity(e);
       }
     }
   });
