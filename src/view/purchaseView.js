4ac8c9f7798b3e6f1846207495440cf49e82ebe9
jswith <9geul@naver.com>
2021-12-12 17:01:38 +0900

    style: 상품 구매 탭 마크업 수정

--- src/view/purchaseView.js
+++ src/view/purchaseView.js
@@ -5,35 +5,43 @@ class PurchaseView {
 
   template() {
     return `
-      <section id="purchase-tab">
-        <form>
-          <h3>금액 투입</h3>
-          <input id="charge-input" type="number" placeholder="투입할 금액" />
-          <button id="charge-button">투입하기</button>
-          <div>투입한 금액: <span id="charge-amount">0원</span></div>
-        </form>
-        <h3>구매할 수 있는 상품 현황</h3>
-        <table>
+    <section id="purchase-tab">
+      <form>
+        <h3>금액 투입</h3>
+        <input id="charge-input" type="number" placeholder="투입할 금액" />
+        <button id="charge-button">투입하기</button>
+        <div>투입한 금액: <span id="charge-amount">0원</span></div>
+      </form>
+      <h3>구매할 수 있는 상품 현황</h3>
+      <table>
+        <thead>
           <tr>
             <th>상품명</th>
             <th>가격</th>
             <th>수량</th>
             <th>구매</th>
           </tr>
+        </thead>
+        <tbody id="purchase-list">
+          <!-- 렌더링 될 부분 -->
           <tr class="product-purchase-item">
             <td class="product-purchase-name" data-product-name="임시상품">임시상품</td>
             <td class="product-purchase-price" data-product-price="1000">1000</td>
             <td class="product-purchase-quantity" data-product-quantity="20">20</td>
             <td><button class="purchase-button">구매하기</button></td>
           </tr>
-        </table>
-        <h3>잔돈</h3>
-        <button id="coin-return-button">반환하기</button>
-        <table>
+        </tbody>
+      </table>
+      <h3>잔돈</h3>
+      <button id="coin-return-button">반환하기</button>
+      <table>
+        <thead>
           <tr>
             <th>동전</th>
             <th>개수</th>
           </tr>
+        </thead>
+        <tbody>
           <tr>
             <td>500원</td>
             <td id="coin-500-quantity">0개</td>
@@ -50,8 +58,9 @@ class PurchaseView {
             <td>10원</td>
             <td id="coin-10-quantity">0개</td>
           </tr>
-        </table>
-      </section>
+        </tbody>
+      </table>
+    </section>  
     `;
   }
 }
