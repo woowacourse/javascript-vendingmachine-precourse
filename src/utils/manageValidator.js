0c24953dbd60ba8eb0347687a3fee55041c3e5d5
jswith <9geul@naver.com>
2021-12-12 14:02:29 +0900

    feat: [예외] 상품명이 공백인 경우

--- src/utils/manageValidator.js
+++ src/utils/manageValidator.js
@@ -8,8 +8,16 @@ class ManageValidator {
     }
   }
 
+  static isProductNameBlank(name) {
+    if (name === '') {
+      alert('상품명을 입력해주세요.');
+      return true;
+    }
+  }
+
   static isInvalidAddItemInput({ name, price, quantity }) {
     if (this.isProudctNameDuplicated(name)) return true;
+    if (this.isProductNameBlank(name)) return true;
     return false;
   }
 }
