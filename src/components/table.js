ada46381151baebd8c9d3acb46f74bb7123fa5b6
Seungjae Lee <sjlee4108@gmail.com>
2021-12-12 03:43:03 -0500

    feat: create table component

---
+++ src/components/table.js
@@ -0,0 +1,27 @@
+import Component from "../common/component.js";
+
+export default class Table extends Component {
+  template() {
+    return `
+      <h2>${this.$props.title}</h2>
+      <table>
+        <tr>${this.getTableHeader()}</tr>
+        ${this.getTableContents()}
+      </table>
+    `;
+  }
+
+  getTableHeader() {
+    return this.$props.tableHeaders
+      .map((header) => `<th>${header}</th>`)
+      .join("");
+  }
+
+  getTableContents() {
+    return this.$props.tableContents.map((c) => this.getTableRow(c)).join("");
+  }
+
+  getTableRow(tableRow) {
+    return `<tr>${tableRow.map((entry) => `<td>${entry}</td>`).join("")}</tr>`;
+  }
+}
