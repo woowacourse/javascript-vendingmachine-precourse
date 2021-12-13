81fa76c5363deaeb514671308c91790947eb7fa9
2019015414 <2019015414@hanyang.ac.kr>
2021-12-14 08:17:27 +0900

    fix: fix while exception

--- src/Coin/Coin.js
+++ src/Coin/Coin.js
@@ -22,6 +22,7 @@ export default class Coin {
   addCoin(inputCoin) {
     let changeToCoinCost = inputCoin;
     while (true) {
+      if (changeToCoinCost == 0) break;
       let coin = MissionUtils.Random.pickNumberInList([500, 100, 50, 10]);
       if (changeToCoinCost < coin) continue;
       changeToCoinCost -= coin;
@@ -29,7 +30,6 @@ export default class Coin {
       if (coin == 100) this.currentCoin[100] += 1;
       if (coin == 50) this.currentCoin[50] += 1;
       if (coin == 10) this.currentCoin[10] += 1;
-      if (changeToCoinCost == 0) break;
     }
   }
 
