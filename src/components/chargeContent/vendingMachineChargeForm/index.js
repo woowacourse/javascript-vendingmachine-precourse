7e84b94240e2dde8fd9f74da67fb071f7b4106b5
leegwae <leegwae@gmail.com>
2021-12-14 02:18:55 +0900

    feat: add feature to alert when charged coin is invalid

--- src/components/chargeContent/vendingMachineChargeForm/index.js
+++ src/components/chargeContent/vendingMachineChargeForm/index.js
@@ -1,6 +1,8 @@
 import ChargeForm from '../../chargeForm/index.js';
 import { addChargedCoinsOfVendingMachine } from '../../../library/storage/vendingMachineCoins.js';
 import { ID_FORM, DICT_PROPS_BUTTON, DICT_PROPS_INPUT } from './const.js';
+import { ERROR_INVALID_CHARGE_COIN } from '../../chargeForm/const.js';
+import isValidCoinToCharge from '../../../machine/utils/charge/isValidCoinToCharge.js';
 
 class VendingMachineChargeForm extends ChargeForm {
   constructor() {
@@ -11,9 +13,12 @@ class VendingMachineChargeForm extends ChargeForm {
   }
 
   onButtonClick() {
-    const input = this.input.value;
-
-    addChargedCoinsOfVendingMachine(Number(input));
+    const coin = Number(this.input.value);
+    if (!isValidCoinToCharge(coin)) {
+      alert(ERROR_INVALID_CHARGE_COIN);
+      return;
+    }
+    addChargedCoinsOfVendingMachine(coin);
   }
 }
 
--- src/components/purchaseContent/consumerChargeForm/index.js
+++ src/components/purchaseContent/consumerChargeForm/index.js
@@ -1,4 +1,6 @@
 import { addChargedCoinOfConsumer } from '../../../library/storage/consumerCoin.js';
+import isValidCoinToCharge from '../../../machine/utils/charge/isValidCoinToCharge.js';
+import { ERROR_INVALID_CHARGE_COIN } from '../../chargeForm/const.js';
 import ChargeForm from '../../chargeForm/index.js';
 import { DICT_PROPS_INPUT, DICT_PROPS_BUTTON, ID_FORM } from './const.js';
 
@@ -11,9 +13,13 @@ class ConsumerChargeForm extends ChargeForm {
   }
 
   onButtonClick() {
-    const input = this.input.value;
-    console.log(input);
-    addChargedCoinOfConsumer(Number(input));
+    const coin = Number(this.input.value);
+
+    if (!isValidCoinToCharge(coin)) {
+      alert(ERROR_INVALID_CHARGE_COIN);
+      return;
+    }
+    addChargedCoinOfConsumer(coin);
   }
 }
 
