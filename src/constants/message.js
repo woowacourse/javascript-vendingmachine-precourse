6216af2a74bd001a6f1980f57778384f746d50de
Leejin-Yang <ejin1996@gmail.com>
2021-12-13 10:42:46 +0900

    feat: 보유할 금액 입력 에러메시지 상수화

--- src/constants/message.js
+++ src/constants/message.js
@@ -2,4 +2,6 @@ export const ERROR = {
   PRODUCT_NAME: '상품명을 입력해주세요',
   PRODUCT_PRICE: '100원 이상, 10의 배수의 가격을 입력해주세요',
   PRODUCT_QUANTITY: '1개 이상의 수량을 입력해주세요',
+
+  CHARGE: '10원 이상, 10의 배수의 금액을 입력해주세요',
 };
