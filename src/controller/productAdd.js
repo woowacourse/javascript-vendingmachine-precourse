81c6356a1750186ae15174cdc33a64fa813ca67a
hanyoonseok <hys6078@naver.com>
2021-12-13 21:09:25 +0900

    refactor: 상품 추가 탭 컨트롤러 class화

--- src/controller/productAdd.js
+++ src/controller/productAdd.js
@@ -2,60 +2,73 @@ import { $, handleStorage, validation, onKeyUpNumericEvent } from './utils.js';
 import { SELECTOR, KEY } from '../model/constants.js';
 import { productAddTableRow } from '../model/template.js';
 import Product from '../model/product.js';
-import { addTableRow, clearInput } from '../view/index.js';
 
-const initInput = (productName, productPrice, productQuantity) => {
-  clearInput(productName);
-  clearInput(productPrice);
-  clearInput(productQuantity);
-};
-
-const isProductNameValid = productName =>
-  !validation.isBlankExist(productName) && !validation.isAlreadyExistProduct(productName);
-
-const isProductPriceValid = productPrice =>
-  validation.isInputNumberValid(productPrice) &&
-  validation.isMultipleOf10(productPrice) &&
-  validation.isOver100(productPrice);
-
-const isProductInputsValid = (productName, productPrice, productQuantity) =>
-  isProductNameValid(productName) &&
-  isProductPriceValid(productPrice) &&
-  validation.isInputNumberValid(productQuantity);
-
-const updateProductLocalStorage = product => {
-  const allProducts = handleStorage.getItemOrArray(KEY.product);
-  allProducts.push(product);
-  handleStorage.setItem(KEY.product, allProducts);
-};
-
-const addProduct = () => {
-  const productName = $(SELECTOR.productNameInput);
-  const productPrice = $(SELECTOR.productPriceInput);
-  const productQuantity = $(SELECTOR.productQuantityInput);
-  if (isProductInputsValid(productName, productPrice, productQuantity)) {
-    const product = new Product(productName.value, productPrice.value, productQuantity.value);
-    const table = document.querySelector('tbody');
-
-    updateProductLocalStorage(product);
-    addTableRow(table, productAddTableRow(product));
-    initInput(productName, productPrice, productQuantity);
+export default class ProductAdd {
+  constructor(view) {
+    this.view = view;
+    this.initTable(this.view);
+    this.addEventListeners();
   }
-};
 
-const initTable = () => {
-  const allProducts = handleStorage.getItemOrArray(KEY.product);
-  const table = document.querySelector('tbody');
-  allProducts.forEach(product => addTableRow(table, productAddTableRow(product)));
-};
+  addEventListeners() {
+    $(SELECTOR.productAddButton).addEventListener('click', () => this.addProduct());
+    $(SELECTOR.productPriceInput).addEventListener('keyup', () =>
+      onKeyUpNumericEvent($(SELECTOR.productPriceInput)),
+    );
+    $(SELECTOR.productQuantityInput).addEventListener('keyup', () =>
+      onKeyUpNumericEvent($(SELECTOR.productQuantityInput)),
+    );
+  }
 
-export const initAllProductAdd = () => {
-  initTable();
-  $(SELECTOR.productAddButton).addEventListener('click', () => addProduct());
-  $(SELECTOR.productPriceInput).addEventListener('keyup', () =>
-    onKeyUpNumericEvent($(SELECTOR.productPriceInput)),
-  );
-  $(SELECTOR.productQuantityInput).addEventListener('keyup', () =>
-    onKeyUpNumericEvent($(SELECTOR.productQuantityInput)),
-  );
-};
+  initTable(view) {
+    const allProducts = handleStorage.getItemOrArray(KEY.product);
+    const table = document.querySelector('tbody');
+    allProducts.forEach(product => view.addTableRow(table, productAddTableRow(product)));
+  }
+
+  initInput(productName, productPrice, productQuantity) {
+    this.view.clearInput(productName);
+    this.view.clearInput(productPrice);
+    this.view.clearInput(productQuantity);
+  }
+
+  isProductNameValid(productName) {
+    return !validation.isBlankExist(productName) && !validation.isAlreadyExistProduct(productName);
+  }
+
+  isProductPriceValid(productPrice) {
+    return (
+      validation.isInputNumberValid(productPrice) &&
+      validation.isMultipleOf10(productPrice) &&
+      validation.isOver100(productPrice)
+    );
+  }
+
+  isProductInputsValid(productName, productPrice, productQuantity) {
+    return (
+      this.isProductNameValid(productName) &&
+      this.isProductPriceValid(productPrice) &&
+      validation.isInputNumberValid(productQuantity)
+    );
+  }
+
+  updateProductLocalStorage(product) {
+    const allProducts = handleStorage.getItemOrArray(KEY.product);
+    allProducts.push(product);
+    handleStorage.setItem(KEY.product, allProducts);
+  }
+
+  addProduct() {
+    const productName = $(SELECTOR.productNameInput);
+    const productPrice = $(SELECTOR.productPriceInput);
+    const productQuantity = $(SELECTOR.productQuantityInput);
+    if (this.isProductInputsValid(productName, productPrice, productQuantity)) {
+      const product = new Product(productName.value, productPrice.value, productQuantity.value);
+      const table = document.querySelector('tbody');
+
+      this.updateProductLocalStorage(product);
+      this.view.addTableRow(table, productAddTableRow(product));
+      this.initInput(productName, productPrice, productQuantity);
+    }
+  }
+}
