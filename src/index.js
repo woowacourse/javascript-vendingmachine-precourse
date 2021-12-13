55730c4f83702e374c605a6e770fbdc795c6c790
compy-ryu <94832610+compy-ryu@users.noreply.github.com>
2021-12-14 00:08:52 +0900

    style: 스타일 시트 수정

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { $ } from './utils/element-tools.js';
 
 import App from './App.js';
 
-new App($(SELECTOR.APP));
+document.addEventListener('DOMContentLoaded', () => new App($(SELECTOR.APP)));
