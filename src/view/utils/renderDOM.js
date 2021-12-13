dfa733de321a3f19cdf508e96402bb563e10ca2a
jhy979 <jhy979@gmail.com>
2021-12-13 20:27:26 +0900

    feat(MANAGE_VIEW): 상품관리탭 상품 관리탭 렌더링 구현

--- src/view/utils/renderDOM.js
+++ src/view/utils/renderDOM.js
@@ -1,11 +1,23 @@
 const COMMON_VIEW = `
-    <h1>🍹자판기🍹</h1>
-    <button id = "product-add-menu">상품 관리</button>
-    <button id = "vending-machine-manage-menu">잔돈 충전</button>
-    <button id = "product-purchase-menu">상품 구매</button>
-    `;
+  <h1>🍹자판기🍹</h1>
+  <button id = "product-add-menu">상품 관리</button>
+  <button id = "vending-machine-manage-menu">잔돈 충전</button>
+  <button id = "product-purchase-menu">상품 구매</button>
+  `;
 
-const MANAGE_VIEW = document.createElement("div");
+const MANAGE_VIEW = `
+  <h2>상품 추가하기</h2>
+  <input placeholder="상품명" id="product-name-input"/>
+  <input placeholder="가격" id="product-price-input"/>
+  <input placeholder="수량" id="product-quantity-input"/>
+  <button id=" product-add-button">추가하기</button>
+  <h2>상품 현황</h2>
+  <table>
+    <th>상품명</th><th>가격</th><th>수량</th>
+    <tr className = "product-manage-item">
+    </tr>
+  </table>
+  `;
 
 const CHARGE_VIEW = document.createElement("div");
 
--- src/view/view.js
+++ src/view/view.js
@@ -1,12 +1,20 @@
-import { COMMON_VIEW } from "./utils/renderDOM.js";
+import { COMMON_VIEW, MANAGE_VIEW } from "./utils/renderDOM.js";
 
 export default class View {
   constructor() {
     this.commonRender();
+    this.manageTabRener();
   }
 
   commonRender() {
     const $app = document.getElementById("app");
     $app.innerHTML = COMMON_VIEW;
+  }
+
+  manageTabRener() {
+    const $manageTab = document.createElement("div");
+    const $app = document.getElementById("app");
+    $manageTab.innerHTML = MANAGE_VIEW;
+    $app.after($manageTab);
   }
 }
