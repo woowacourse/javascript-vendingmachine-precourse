776310a7ceed3dd5fe336d0627e7ff1733ab0ae4
nan-noo <uni613@naver.com>
2021-12-13 23:49:07 +0900

    feat: screen change on tab click
    Add addEventListener in index.js
    - onProductManageTabClick
    - onMachineChargeTabClick
    - onProductPurchaseTabClick

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import { fetchHtmlView } from './fetch.js';
 
+function onProductManageTabClick() {
+    document.querySelector("#tab-content").innerHTML = 'hihi';
+}
+function onMachineChargeTabClick() {
+    document.querySelector("#tab-content").innerHTML = 'hihaai';
+}
+function onProductPurchaseTabClick() {
+    document.querySelector("#tab-content").innerHTML = 'hisshi';
+}
+
 // DOM
 const app = document.querySelector("#app");
 // init
 fetchHtmlView('tab.html').then(view => app.innerHTML = view);
+
+app.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const handlers = {
+        "product-add-menu"() { onProductManageTabClick(); },
+        "vending-machine-manage-menu"() { onMachineChargeTabClick(); },
+        "product-purchase-menu"() { onProductPurchaseTabClick(); },
+    };
+    if(Object.keys(handlers).includes(e.target.id)) handlers[e.target.id]();
+});
