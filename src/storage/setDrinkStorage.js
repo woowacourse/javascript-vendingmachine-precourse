84ef1021d5c53ef3be4a1e00064fe4264df08c08
liswkjts <liswktjs@gmail.com>
2021-12-13 14:26:28 +0900

    fix: (setDrinkStorage)🐛 drink localStorage가 비었을 때 발생하는 error fix drink localStorage 값이 비어있을 때에 유저가 새롭게 입력한 값만으로 drink localStorage를 setItem을 진행한다 setDrinkStorage.js

--- src/storage/setDrinkStorage.js
+++ src/storage/setDrinkStorage.js
@@ -3,6 +3,10 @@ import store from './store.js';
 
 export default function setDrinkStorage(userProductInput) {
   let drinkStorage = store.getLocalStorage(DRINK_STORAGE_NAME);
-  drinkStorage.push(userProductInput);
-  store.setLocalStoreage(DRINK_STORAGE_NAME, drinkStorage);
+  if (drinkStorage) {
+    drinkStorage.push(userProductInput);
+    store.setLocalStoreage(DRINK_STORAGE_NAME, drinkStorage);
+  } else {
+    store.setLocalStoreage(DRINK_STORAGE_NAME, [userProductInput]);
+  }
 }
