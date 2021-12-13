6bc072eec2ee5324b1bf35abcd1d632d5c4d3178
hanyoonseok <hys6078@naver.com>
2021-12-13 18:50:27 +0900

    feat: 상품 구매 탭 구매하기 구현 구매하기 클릭시 localstorage 정보의 상품 개수 -1 충전 금액 -= 가격

--- src/controller/purchase.js
+++ src/controller/purchase.js
@@ -1,6 +1,61 @@
-import { $, getItemOrNull, setItem } from './utils.js';
-import { setInnerHTML, clearInput } from '../view/index.js';
+import { $, getItemOrArray, getItemOrNull, setItem, isEnoughCoin } from './utils.js';
+import {
+  addTableRow,
+  addTableHeader,
+  clearTable,
+  setInnerHTML,
+  clearInput,
+} from '../view/index.js';
 import { KEY, SELECTOR } from '../model/constants.js';
+import { productPurchaseTableRow, productPurchaseTableHeader } from '../model/dom.js';
+
+const initAllPurchaseButtonEvent = () => {
+  const allButtons = document.querySelectorAll(`.${SELECTOR.purchaseButton}`);
+  allButtons.forEach(button =>
+    button.addEventListener('click', () => purchaseProduct(button.parentElement.parentElement)),
+  );
+};
+
+const initProductStatusTable = () => {
+  const table = document.querySelector('tbody');
+  const allProducts = getItemOrArray(KEY.product);
+  clearTable(table);
+  addTableHeader(table, productPurchaseTableHeader());
+  allProducts.forEach(product => addTableRow(table, productPurchaseTableRow(product)));
+  initAllPurchaseButtonEvent();
+};
+
+const initPurchaseDom = () => {
+  const charge = getItemOrNull(KEY.charge);
+  if (charge || charge === 0) {
+    setInnerHTML($(SELECTOR.chargeAmount), charge);
+  }
+  initProductStatusTable();
+};
+
+const calculateProducts = (selectProduct, products) => {
+  selectProduct.quantity -= 1;
+  if (selectProduct.quantity === 0) {
+    products = products.filter(product => product.name !== selectProduct.name);
+  }
+  setItem(KEY.product, products);
+};
+
+const calculateCharge = (selectProduct, charge) => {
+  charge -= selectProduct.price;
+  setItem(KEY.charge, charge);
+};
+
+const purchaseProduct = item => {
+  const charge = getItemOrNull(KEY.charge);
+  const products = getItemOrArray(KEY.product);
+  const selectProduct = products.find(e => e.name === item.childNodes[1].dataset.productName);
+  if (isEnoughCoin(charge, selectProduct.price)) {
+    calculateProducts(selectProduct, products);
+    calculateCharge(selectProduct, charge);
+    initPurchaseDom();
+  }
+};
 
 const initChargeDom = () => {
   const charge = getItemOrNull(KEY.charge);
@@ -23,6 +78,7 @@ const chargeMoney = () => {
 };
 
 export const initAllPurchase = () => {
+  initProductStatusTable();
   initChargeDom();
   $(SELECTOR.chargeButton).addEventListener('click', () => chargeMoney());
   // $(SELECTOR.returnButton).addEventListener('click', () => returnMoney());
