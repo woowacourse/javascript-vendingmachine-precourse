ff831ca793de2c798470c71ed22f17eb169cafe4
airman5573 <rpf5573@kakao.com>
2021-12-14 01:34:47 +0900

    fix: eslint parser dosen't allow property in class

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
+  parser: '@babel/eslint-parser',
   env: {
     browser: true,
-    es2020: true,
+    es2021: true,
   },
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
-  parser: '@babel/eslint-parser',
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2015,
     sourceType: 'module',
+    requireConfigFile: false,
   },
   rules: {
     'no-alert': 'off',
-    'import/extensions': ['error', 'always'],
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
     'max-depth': ['error', 2],
     'prettier/prettier': 'error',
     'class-methods-use-this': 'off',
-    'import/prefer-default-export': 'off',
+    'max-lines-per-function': ['error', 15],
+    'no-constructor-return': 'off',
   },
   plugins: ['prettier'],
 };
