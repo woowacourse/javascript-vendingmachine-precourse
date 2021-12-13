2c4cb7c15e8d689535fc89d092add7397e5c2b2b
liswkjts <liswktjs@gmail.com>
2021-12-13 22:54:02 +0900

    refactor: (joinVendingCoinList) ♻️  유저가 입력한 값에 로컬스토리지에 있는 값 바로 더하기 기존에 새로 유저가 입력한 값과 원래 localStorage에 저장되어 있던 값을 초기화된 객체에 합쳐서 계산하였는데 유저가 입력한 값에 원래 localStorage에 저장된 값을 합치는 방식으로 단순화함

--- src/modules/joinVendingCoinList.js
+++ src/modules/joinVendingCoinList.js
@@ -2,24 +2,13 @@ import { COIN_KEY_LIST } from '../constants/coinConstants.js';
 import { VENDINGCOIN_STORGAE_NAME } from '../constants/constants.js';
 import store from '../storage/store.js';
 
-function initTotalCoinList() {
-  let totalCoinList = {};
-  COIN_KEY_LIST.forEach((item) => {
-    totalCoinList[item] = 0;
-  });
-  return totalCoinList;
-}
-
 export default function joinVendingCoinList(vendingCoin) {
-  let totalCoinList = initTotalCoinList();
-  for (const key in vendingCoin) {
-    totalCoinList[key] = vendingCoin[key];
-  }
-  const storageCoinList = store.getLocalStorage(VENDINGCOIN_STORGAE_NAME);
-  if (storageCoinList) {
-    for (const key in storageCoinList) {
-      totalCoinList[key] += storageCoinList[key];
+  let totalCoinObject = vendingCoin;
+  const stroageCoinObject = store.getLocalStorage(VENDINGCOIN_STORGAE_NAME);
+  if (stroageCoinObject) {
+    for (const key in stroageCoinObject) {
+      totalCoinObject[key] += stroageCoinObject[key];
     }
   }
-  return totalCoinList;
+  return totalCoinObject;
 }
