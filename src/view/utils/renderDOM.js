b238ad9270e649ef16986d3f851888cdb22b5142
jhy979 <jhy979@gmail.com>
2021-12-13 20:18:52 +0900

    fix(COMMON_VIEW): 3가지 탭 메뉴 버튼 순서 변경

--- src/view/utils/renderDOM.js
+++ src/view/utils/renderDOM.js
@@ -1,9 +1,10 @@
 const COMMON_VIEW = `
     <h1>🍹자판기🍹</h1>
-    <button id = "product-purchase-menu">상품 구매</button>
-    <button id = "vending-machine-manage-menu">잔돈 충전</button>
     <button id = "product-add-menu">상품 관리</button>
-`;
+    <button id = "vending-machine-manage-menu">잔돈 충전</button>
+    <button id = "product-purchase-menu">상품 구매</button>
+    `;
+
 const MANAGE_VIEW = document.createElement("div");
 
 const CHARGE_VIEW = document.createElement("div");
