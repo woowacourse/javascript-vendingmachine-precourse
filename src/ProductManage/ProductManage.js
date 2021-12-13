08af43e5ee51876b43ddd6d6055b04e2fa1a5efc
Daye Lee <leeda1801@gmail.com>
2021-12-13 19:50:05 +0900

    refactor: separate append elements from create elements functions

--- src/ProductManage/ProductManage.js
+++ src/ProductManage/ProductManage.js
@@ -1,33 +1,43 @@
 import { HEADER } from '../common/constants.js';
-import { createDiv, createHeader } from '../common/CreateElement.js';
+import {
+  createDiv,
+  createHeader,
+  createMenuDiv,
+} from '../common/CreateElement.js';
 import { $app } from '../common/elements.js';
 
 import createProductAddForm from './CreateForm.js';
 import createProductListTable from './CreateTable.js';
 
 function createProductManageDiv() {
-  const productManageDiv = createDiv();
-  productManageDiv.setAttribute('id', 'product-manage-div');
-  productManageDiv.style.display = 'none';
+  const id = 'product-manage-div';
+  const productManageDiv = createMenuDiv(id);
 
   return productManageDiv;
 }
 
-function createProductAdd(productManageDiv) {
+function createProductAdd() {
+  const productAddDiv = createDiv();
   const productAddHeader = createHeader(HEADER.PRODUCT_ADD);
   const productAddForm = createProductAddForm();
-  productManageDiv.append(productAddHeader, productAddForm);
+  productAddDiv.append(productAddHeader, productAddForm);
+
+  return productAddDiv;
 }
 
-function createProductList(productManageDiv) {
+function createProductList() {
+  const productListDiv = createDiv();
   const productListHeader = createHeader(HEADER.PRODUCT_LIST);
   const productListTable = createProductListTable();
-  productManageDiv.append(productListHeader, productListTable);
+  productListDiv.append(productListHeader, productListTable);
+
+  return productListDiv;
 }
 
 export default function createProductManage() {
   const productManageDiv = createProductManageDiv();
-  createProductAdd(productManageDiv);
-  createProductList(productManageDiv);
+  const productAdd = createProductAdd();
+  const productList = createProductList();
+  productManageDiv.append(productAdd, productList);
   $app.append(productManageDiv);
 }
