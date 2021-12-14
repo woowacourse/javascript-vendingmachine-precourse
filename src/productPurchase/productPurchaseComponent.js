0d2815f0ede137f59b2f60ba467621666d3c684d
kamwoo <rkadndud06@naver.com>
2021-12-14 22:53:22 +0900

    feat: product purchase 금액 투입 버튼 메소드 작성
    - 버튼 클릭시 유효성 검사
    - 투입 금액 프로퍼티에 값 더하기
    - 투입 금액 element 갱신

--- src/productPurchase/productPurchaseComponent.js
+++ src/productPurchase/productPurchaseComponent.js
@@ -1,6 +1,7 @@
 import ProductPurchaseView from './productPurchaseView.js';
+import ValidateUtils from '../utils/validateUtils.js';
 import { $ } from '../utils/common.js';
-import { BUTTON } from './productPurchaseViewInfo.js';
+import { BUTTON, INPUT } from './productPurchaseViewInfo.js';
 
 export default class productPurchaseComponent {
   constructor() {
@@ -16,7 +17,13 @@ export default class productPurchaseComponent {
     });
   }
 
-  onClickChargeButton = () => {};
+  onClickChargeButton = () => {
+    const inputMoney = Number($(`#${INPUT.ID}`).value);
+    if (ValidateUtils.checkInputAmount(inputMoney)) {
+      this.chargeMoney += inputMoney;
+      this.productPurchaseView.showAmountText(this.chargeMoney);
+    }
+  };
 
   onClickPurchaseButton = (event) => {};
 
