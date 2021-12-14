99dc5ba734084aad50ef1621ab29556664eb12bb
kamwoo <rkadndud06@naver.com>
2021-12-14 21:31:26 +0900

    feat: product add 상품 추가 버튼 액션 작성
    - 상품 추가 버튼 클릭 시 입력 폼의 상품 이름, 가격, 수량 객체 생성

--- src/productAdd/productAddComponent.js
+++ src/productAdd/productAddComponent.js
@@ -1,4 +1,5 @@
 import ProductAddView from './productAddView.js';
+import { BUTTON } from './productAddViewInfo.js';
 
 export default class ProductAddComponent {
   constructor() {
@@ -7,5 +8,21 @@ export default class ProductAddComponent {
 
   render() {
     this.productAddView.render();
+  }
+
+  configureButton() {
+    $(`#${BUTTON.ID}`).addEventListener('click', this.onClickSubmit);
+  }
+
+  onClickSubmit = () => {
+    const inputData = this.createInputDataObject();
+  };
+
+  createInputDataObject() {
+    return {
+      name: $(`#${inputInfo.productNameInput.id}`).value,
+      price: Number($(`#${inputInfo.productPriceInput.id}`).value),
+      quantity: Number($(`#${inputInfo.productQuantityInput.id}`).value),
+    };
   }
 }
