a1db6ecafae007ea5f41166f760446ab80ddc187
2013250433 <shiplight@korea.ac.kr>
2021-12-14 12:32:58 +0900

    fix: 잔돈 반환 함수, 구현 실수 수정

--- README.md
+++ README.md
@@ -45,6 +45,7 @@
 - machine.js 에서 validate (에러 검증) 함수들 분리하기 ✔️
 - machine.js 에서 display(화면 보여주는 기능) 함수들 분리하기 ✔️
 - 처음 앱이 시작할 때, localStorage를 조회하여 탭 마다 자판기 이전 상태 보여주기 ✔️
+- 잔돈충전 탭, 상품금액 탭 잔돈 반환 시, 보유금액 변경하기
 - displayProductPurchaseTab 이벤트 리스너 분리하기
 
 
--- src/Logic/controller.js
+++ src/Logic/controller.js
@@ -3,6 +3,7 @@ import * as validator from './validator.js';
 import { getStorage, updateStorage } from '../utils/storage.js';
 import * as coinUtil from "../utils/coin.js";
 import * as displayer from '../View/display.js';
+import { COIN_VALUE } from '../constants.js';
 
 export default class Controller {
     addProduct(name, price, quantity){
@@ -67,26 +68,37 @@ export default class Controller {
 
     returnMoney(){
         const item = getStorage();
-        const coins = this.getRandomReturn(item);
+        const coins = this.getCoinReturn(item);
         updateStorage(item);
 
         displayer.displayReturnedCoins(coins);
         displayer.displayRemainCoins(item);
     }
 
-    getRandomReturn(item){
-        const returnedCoins = coinUtil.generateTemplateCoins();
-        const inverted = coinUtil.generateInverted();
-
-        while(item.input > 0){
-            const pick = MissionUtils.Random.pickNumberInList(coinUtil.generateOnlyValues());
-            if(item.input >= pick){
-                item.input -= pick;
-                item.coins[inverted[pick]]--;
-                returnedCoins[inverted[pick]]++;
+    getCoinReturn(item){
+        let returnCoin = coinUtil.generateTemplateCoins();
+        for(let key in COIN_VALUE){
+            if(item.input <= 0){
+                break;
             }
+            returnCoin[key] = this.getCoinFromPossessed(key, item)
         }
-        
-        return returnedCoins;
+        return returnCoin;
+    }
+
+    getCoinFromPossessed(key, item){
+        let required = parseInt(item.input / COIN_VALUE[key]);
+        let returningCoin = 0;
+        if(item.coins[key] > required){
+            item.coins[key] -= required;
+            item.input -= required * COIN_VALUE[key];
+            returningCoin = required;
+        } else {
+            returningCoin = item.coins[key];
+            item.input -= item.coins[key] * COIN_VALUE[key];
+            item.coins[key] = 0;
+        }
+
+        return returningCoin;
     }
 }
