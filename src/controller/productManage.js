cadb23fbaeea3500cbc33fca089a66f0ce4a463e
ire4564 <ire4564@gmail.com>
2021-12-14 22:09:03 +0900

    feat: Add loadLocalStorage function in productManage.js

--- src/controller/productManage.js
+++ src/controller/productManage.js
@@ -3,19 +3,20 @@ import { createTd } from "../view/createDOM.js";
 
 export default class ProductManage {
     constructor() {
-        this.addProductList();
+      const itemClass = [
+        PRODUCT_MANAGE_TABLE.NAME,
+        PRODUCT_MANAGE_TABLE.PRICE,
+        PRODUCT_MANAGE_TABLE.QUANTITY,
+      ];
+        this.addProductList(itemClass);
+        this.loadLocalStorage(itemClass);
     }
 
-    addProductList() {
+    addProductList(itemClass) {
         const $addButton = document.querySelector(`#${PRODUCT_MANAGE.ADD_BUTTON_ID}`);
         const $name = document.querySelector(`#${PRODUCT_MANAGE.NAME_ID}`);
         const $price = document.querySelector(`#${PRODUCT_MANAGE.PRICE_ID}`);
         const $quantity = document.querySelector(`#${PRODUCT_MANAGE.QUANTITY_ID}`);
-        const itemClass = [
-          PRODUCT_MANAGE_TABLE.NAME,
-          PRODUCT_MANAGE_TABLE.PRICE,
-          PRODUCT_MANAGE_TABLE.QUANTITY,
-        ]
 
         $addButton.addEventListener('click', e => {
           e.preventDefault();
@@ -28,10 +29,19 @@ export default class ProductManage {
 
     setLocalStorage(nameValue, priceValue, quantityValue) {
       let storeInfo = JSON.parse(localStorage.getItem("productList"));
-      const product = [{name: nameValue, price: priceValue, quantity: quantityValue}];
+      const product = [nameValue, priceValue, quantityValue];
 
       console.log(storeInfo);
-      storeInfo !== null ? storeInfo.push(product) : storeInfo = product;
+      storeInfo = storeInfo !== null ? storeInfo : [];
+      storeInfo.push(product);
       localStorage.setItem("productList", JSON.stringify(storeInfo));
+    }
+
+    loadLocalStorage(itemClass) {
+      let storeInfo = JSON.parse(localStorage.getItem("productList"));
+
+      storeInfo.forEach((list) => {
+        createTd(PRODUCT_MANAGE_TABLE.CLASS, itemClass, list);
+      });
     }
 };
