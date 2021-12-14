ce7eabc6c508123dc7ea0694556412f06264a564
Hoji <b4bynew@gmail.com>
2021-12-14 20:00:28 +0900

    feat: 상품 추가시 입력 유효성 검사

--- src/components/product-add/header.js
+++ src/components/product-add/header.js
@@ -1,4 +1,6 @@
 import Component from '../../essential/component.js';
+import * as CONSTANTS from '../../utils/constants.js';
+import { isNotEmpty, isValidPrice, isValidQuantity } from '../../utils/validator.js';
 import Product from '../../model/product.js';
 
 const CONTENT = `
@@ -14,6 +16,14 @@ export default class Header extends Component {
     return CONTENT;
   }
 
+  checkValidation() {
+    let name = this.$('#product-name-input').value;
+    let price = this.$('#product-price-input').value;
+    let quantity = this.$('#product-quantity-input').value;
+
+    return isNotEmpty(name) && isValidPrice(price) && isValidQuantity(quantity);
+  }
+
   addProduct() {
     this.$props.sendProduct(
       new Product(
@@ -26,6 +36,12 @@ export default class Header extends Component {
 
   setEvent() {
     this.addEvent('click', '#product-add-button', () => {
+      // 유효성 검사
+      if (!this.checkValidation()) {
+        alert(CONSTANTS.INVALID_PRODUCT_INPUT);
+        return false;
+      }
+
       this.addProduct();
     });
   }
