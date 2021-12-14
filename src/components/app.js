3a0c65e857cdba133480502c8ae9cc5e7e582a7c
Seungjae Lee <sjlee4108@gmail.com>
2021-12-14 07:16:16 -0500

    feat: connect business logic to ui logic

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,4 @@
+import VendingMachine from "../class/vendingMachine.js";
 import Component from "../common/component.js";
 import AddMenuContainer from "./addMenuContainer.js";
 import ButtonTabs from "./buttonTabs.js";
@@ -7,7 +8,19 @@ import PurchaseMenuContainer from "./purchaseMenuContainer.js";
 
 export default class App extends Component {
   initialize() {
-    this.$state = { activeTab: "product-add-menu" };
+    this.$vendingMachine = new VendingMachine();
+    this.$state = {
+      activeTab: "product-add-menu",
+      products: this.$vendingMachine.getProducts(),
+      userMoney: this.$vendingMachine.getRemainingUserMoney(),
+      coins: this.$vendingMachine.getVendingMachineCoins(),
+      change: [
+        [500, 0],
+        [100, 0],
+        [50, 0],
+        [10, 0],
+      ],
+    };
   }
 
   template() {
@@ -42,7 +55,6 @@ export default class App extends Component {
         { name: "잔돈 충전", id: "vending-machine-manage-menu" },
         { name: "상품 구매", id: "product-purchase-menu" },
       ],
-      onClickCallback: this.onClickTab.bind(this),
     });
   }
 
@@ -50,19 +62,83 @@ export default class App extends Component {
     const $contentSelector = this.$target.querySelector("#content");
 
     if (this.$state.activeTab === "product-purchase-menu") {
-      new PurchaseMenuContainer($contentSelector, { menuItems: [] });
+      new PurchaseMenuContainer($contentSelector, {
+        menuItems: this.$state.products,
+        userMoney: this.$state.userMoney,
+        change: this.$state.change,
+      });
     } else if (this.$state.activeTab === "vending-machine-manage-menu") {
-      new CoinManageMenuContainer($contentSelector);
+      new CoinManageMenuContainer($contentSelector, {
+        coins: this.$state.coins,
+      });
     } else {
-      new AddMenuContainer($contentSelector);
+      new AddMenuContainer($contentSelector, {
+        menuItems: this.$state.products,
+      });
     }
   }
 
-  onClickTab(event) {
+  setEvent() {
+    this.$target.addEventListener("click", this.onClickButton.bind(this));
+  }
+
+  onClickButton(event) {
     const { target } = event;
 
     if (target.tagName !== "BUTTON") return;
 
-    this.setState({ activeTab: target.id });
+    if (target.className === "tab") this.setState({ activeTab: target.id });
+    else if (target.id === "product-add-button") this.onClickAddProduct();
+    else if (target.id === "vending-machine-charge-button") {
+      this.onClickAddVendingMachineCoins();
+    } else if (target.id === "charge-button") this.onClickAddUserMoney();
+    else if (target.className === "purchase-button") {
+      this.onClickPurchase(target.getAttribute("data-product-name"));
+    } else if (target.id === "coin-return-button") this.onClickGetChange();
+  }
+
+  onClickAddProduct() {
+    const name = document.getElementById("product-name-input").value;
+    const price = document.getElementById("product-price-input").value;
+    const quantity = document.getElementById("product-quantity-input").value;
+    this.$vendingMachine.addProduct(name, price, quantity);
+    this.setState({
+      products: this.$vendingMachine.getProducts(),
+    });
+  }
+
+  onClickAddVendingMachineCoins() {
+    const amount = document.getElementById(
+      "vending-machine-charge-input"
+    ).value;
+    this.$vendingMachine.addVendingMachineCoins(amount);
+    this.setState({
+      coins: this.$vendingMachine.getVendingMachineCoins(),
+    });
+  }
+
+  onClickAddUserMoney() {
+    const amount = document.getElementById("charge-input").value;
+    this.$vendingMachine.chargeUserMoney(parseInt(amount, 10));
+    this.setState({
+      userMoney: this.$vendingMachine.getRemainingUserMoney(),
+    });
+  }
+
+  onClickPurchase(productName) {
+    this.$vendingMachine.purchaseProduct(productName);
+    this.setState({
+      products: this.$vendingMachine.getProducts(),
+      userMoney: this.$vendingMachine.getRemainingUserMoney(),
+      coins: this.$vendingMachine.getVendingMachineCoins(),
+    });
+  }
+
+  onClickGetChange() {
+    this.setState({
+      change: this.$vendingMachine.getChange(),
+      coins: this.$vendingMachine.getVendingMachineCoins(),
+      userMoney: this.$vendingMachine.getRemainingUserMoney(),
+    });
   }
 }
