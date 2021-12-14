fa2c38f1e8f44e5f20cfcde5728e37d11718a082
Hoji <b4bynew@gmail.com>
2021-12-14 20:06:35 +0900

    feat: 반복 사용되는 단어 상수로 처리

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 export const COINS = [500, 100, 50, 10];
 
 export const STORAGE_PRODUCTS_KEY = 'PRODUCTS';
+export const STORAGE_CHANGES_KEY = 'CHANGES';
+export const STORAGE_REMAINS_KEY = 'REMAINS';
 
 export const INVALID_PRODUCT_INPUT = '입력 값이 올바른지 확인해주세요.';
