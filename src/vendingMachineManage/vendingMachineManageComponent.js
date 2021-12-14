b720eb76cf91e67983c05bb6d754c90d1f61d4af
kamwoo <rkadndud06@naver.com>
2021-12-14 22:17:41 +0900

    feat: 무작위 동전 생성 메소드 추가
    - 충전된 금액이 0이 될때까지 500, 100, 50, 10에서 무작위로 선택하여 동전을 충전한다.

--- src/vendingMachineManage/vendingMachineManageComponent.js
+++ src/vendingMachineManage/vendingMachineManageComponent.js
@@ -1,4 +1,7 @@
 import VendingMachineManageView from './vendingMachineManageView.js';
+import LocalStorageUtils from '../utils/localStorageUtils.js';
+import ValidateUtils from '../utils/validateUtils.js';
+import { BUTTON, INPUT } from './vendingMachineManageViewInfo.js';
 import { $ } from '../utils/common.js';
 
 export default class VendingMachineManageComponent {
@@ -12,10 +15,35 @@ export default class VendingMachineManageComponent {
   }
 
   configureButton() {
-    $(`#${buttonInfo.id}`).addEventListener('click', this.onClickButton);
+    $(`#${BUTTON.ID}`).addEventListener('click', this.onClickButton);
   }
 
   onClickButton = () => {
-    const inputCoin = Number($(`#${inputInfo.id}`).value);
+    const inputCoin = Number($(`#${INPUT.ID}`).value);
+
+    if (ValidateUtils.checkInputAmount(inputCoin)) {
+      this.saveRamdomCoins(inputCoin);
+    }
   };
+
+  generateRandomNumber(remain) {
+    const numberList = [500, 100, 50, 10];
+    let randomNumber = MissionUtils.Random.pickNumberInList(numberList);
+    if (randomNumber > remain) {
+      randomNumber = this.generateRandomNumber(remain);
+    }
+    return randomNumber;
+  }
+
+  saveRamdomCoins(coin) {
+    let coinStatus = LocalStorageUtils.getMachineManageTableItem();
+
+    while (coin) {
+      const randomCoin = this.generateRandomNumber(coin);
+      coinStatus[randomCoin] += 1;
+      coin -= randomCoin;
+    }
+
+    LocalStorageUtils.setMachineManageTableItem(coinStatus);
+  }
 }
