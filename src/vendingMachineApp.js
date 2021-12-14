a9627f0357a6bfa7680c9b61da22d4c929cb0dc4
kamwoo <rkadndud06@naver.com>
2021-12-14 21:55:33 +0900

    feat: 잔돈 추가 component, view 빈 클래스 작성
    - vending machine app 버튼 액션에 추가

--- src/vendingMachineApp.js
+++ src/vendingMachineApp.js
@@ -2,11 +2,13 @@ import { BUTTONS_INFO } from './header/headerViewInfo.js';
 import { $ } from './utils/common.js';
 import HeaderView from './header/headerView.js';
 import ProductAddComponent from './productAdd/productAddComponent.js';
+import VendingMachineManageComponent from './vendingMachineManage/vendingMachineManageComponent.js';
 
 export default class VendingMachineApp {
   constructor() {
     this.headerView = new HeaderView();
     this.productAddComponent = new ProductAddComponent();
+    this.vendingMachineComponent = new VendingMachineManageComponent();
     this.configureButtons();
   }
 
@@ -20,6 +22,9 @@ export default class VendingMachineApp {
     switch (event.path[0].id) {
       case BUTTONS_INFO.PRODUCT_ADD.ID:
         this.productAddComponent.render();
+        return;
+      case BUTTONS_INFO.VENDING_MACHINE_MANAGE.id:
+        this.vendingMachineComponent.render();
         return;
     }
   };
