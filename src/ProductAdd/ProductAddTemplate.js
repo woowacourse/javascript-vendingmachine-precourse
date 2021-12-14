15bf5524457685b8015295b93ae0735afb869ff4
2019015414 <2019015414@hanyang.ac.kr>
2021-12-14 15:52:02 +0900

    refactor: refactor ProductAddTemplate.js

--- src/ProductAdd/ProductAddTemplate.js
+++ src/ProductAdd/ProductAddTemplate.js
@@ -16,35 +16,32 @@ export default class ProductAddTemplate {
   }
 
   makeProductAdd() {
-    this.productAddTitle = document.createElement('h2');
-    this.productAddTitle.innerText = '상품 추가하기';
-    this.inputProductAdd = document.createElement('form');
-    this.inputProductAdd.id = 'input-product-add';
-    this.inputProductAdd.innerHTML =
+    const productAddTitle = document.createElement('h2');
+    productAddTitle.innerText = '상품 추가하기';
+    const inputProductAdd = document.createElement('form');
+    inputProductAdd.id = 'input-product-add';
+    inputProductAdd.innerHTML =
       '<input type="text" placeholder="상품명" id="product-name-input" /><input type="text" placeholder="가격" id="product-price-input" /><input type="text" placeholder="수량" id="product-quantity-input" /><button type="submit" id="product-add-button">추가하기</button>';
-    this.productAddDiv.append(this.productAddTitle, this.inputProductAdd);
+    this.productAddDiv.append(productAddTitle, inputProductAdd);
   }
 
   makeProductStatus() {
-    this.productStatusTitle = document.createElement('h2');
-    this.productStatusTitle.innerText = '상품 현황';
+    const productStatusTitle = document.createElement('h2');
+    productStatusTitle.innerText = '상품 현황';
     this.productStatusTable = document.createElement('table');
     this.makeTableElement();
-    this.productStatusDiv.append(
-      this.productStatusTitle,
-      this.productStatusTable
-    );
+    this.productStatusDiv.append(productStatusTitle, this.productStatusTable);
   }
 
   makeTableElement() {
-    this.productStatusTableHead = document.createElement('thead');
-    this.productStatusTableHead.innerHTML =
+    const productStatusTableHead = document.createElement('thead');
+    productStatusTableHead.innerHTML =
       '<tr><th>상품명</th><th>가격</th><th>수량</th></tr>';
-    this.productStatusTableBody = document.createElement('tbody');
-    this.productStatusTableBody.id = 'product-add-status-table-body';
+    const productStatusTableBody = document.createElement('tbody');
+    productStatusTableBody.id = 'product-add-status-table-body';
     this.productStatusTable.append(
-      this.productStatusTableHead,
-      this.productStatusTableBody
+      productStatusTableHead,
+      productStatusTableBody
     );
   }
 }
