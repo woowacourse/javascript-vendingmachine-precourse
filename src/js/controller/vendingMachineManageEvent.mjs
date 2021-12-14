046a6a31d3130f30559d43502f3e79d1dbfa6326
joker77z <joker77z@naver.com>
2021-12-14 09:18:07 +0900

    fix: 자판기 금액 랜더
    새로고침했을 때 NaN나오는 현상 수정

--- src/js/controller/vendingMachineManageEvent.mjs
+++ src/js/controller/vendingMachineManageEvent.mjs
@@ -10,23 +10,19 @@ function addCoins() {
   window.addEventListener('click', e => {
     if (e.target !== document.querySelector('#vending-machine-charge-button')) return;
     let chargeMoney = document.querySelector('#vending-machine-charge-input').value;
-    // const vendingMachineChargeAmount = document.querySelector('#vending-machine-charge-amount');
 
-    // 재접속
     if (localStorage.getItem('vending-machine-charge-amount')) {
       chargeMoney = Number(localStorage.getItem('vending-machine-charge-amount')) + Number(chargeMoney);
       localStorage.setItem('vending-machine-charge-amount', chargeMoney);
     } else {
       localStorage.setItem('vending-machine-charge-amount', chargeMoney);
-
-      // let machine = new VendingMachine(chargeMoney);
     }
-
     vendingMachineChargeAmount();
   });
 }
 
 export function vendingMachineManageEvent() {
+  // const machine = new VendingMachine();
   const $vendingMachineManageMenu = document.querySelector('#vending-machine-manage-menu');
   $vendingMachineManageMenu.addEventListener('click', renderVendingMachineManageTab);
 
