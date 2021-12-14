4875aae99dc93e8657c1902a47b1fb089c31f5e9
YUNHO KANG <ansghkdbsgh@gmail.com>
2021-12-14 14:27:46 +0900

    feat: add purchase product
    - init purchase buttons
    - check money enough
    - check product quantity enough

--- src/Menu/purchaseMenu.js
+++ src/Menu/purchaseMenu.js
@@ -46,6 +46,7 @@ const initProductTable = () => {
   for (let product of products) {
     table.insertAdjacentHTML('beforeend', productTableRow(product));
   }
+  initPurchaseButtons();
 };
 
 const productTableRow = (product) => `
@@ -56,3 +57,57 @@ const productTableRow = (product) => `
       <td class = 'purchase-button'><button id = 'purchase-button'> 구매하기</button></td>
   </tr>
   `;
+
+const initPurchaseButtons = () => {
+  const purchaseButtons = document.getElementsByClassName('purchase-button');
+  for (let button of purchaseButtons) {
+    button.addEventListener('click', () => purchaseProduct(button.parentElement));
+  }
+};
+
+const purchaseProduct = (element) => {
+  const products = getProductsFromLocalStorage('product');
+  const name = element.childNodes[1].innerText;
+  const index = findProductIndex(products, name);
+
+  const product = products[index];
+  const price = product.price;
+  const quantity = product.quantity;
+
+  const money = getMoneyFromLocalStorage('money');
+  const moneyAfterBuy = Number(money) - Number(price);
+
+  if (isMoneyEnough(moneyAfterBuy)) {
+    if (isQuantityEnough(quantity)) {
+      document.getElementById('charge-amount').innerText = moneyAfterBuy;
+      saveMoneyToLocalStorage('money', Number(money) - Number(price));
+      product.quantity = quantity - 1;
+      element.childNodes[5].innerText -= 1;
+      saveProductsToLocalStorage('product', products);
+    }
+  }
+};
+
+const findProductIndex = (products, name) => {
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].name === name) {
+      return i;
+    }
+  }
+};
+
+const isMoneyEnough = (money) => {
+  if (money < 0) {
+    alert('not enough moeny');
+    return false;
+  }
+  return true;
+};
+
+const isQuantityEnough = (quantity) => {
+  if (quantity - 1 < 0) {
+    alert('not enough quantity');
+    return false;
+  }
+  return true;
+};
