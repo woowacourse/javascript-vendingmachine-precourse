1c23c0c1c1b1f5774a54292c2e038042ed9d374a
compy-ryu <94832610+compy-ryu@users.noreply.github.com>
2021-12-14 17:39:07 +0900

    refactor: 사용자 입력 오류 시 출력 메시지 수정

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,13 +10,13 @@ export const DONE = Object.freeze({
 export const ERROR = Object.freeze({
   PRODUCT_INPUT: Symbol('상품의 입력란 중 빈 입력란이 있습니다.'),
   PRODUCT_PRICE: Symbol(`상품의 가격을 정확히 입력해주세요.\n최소 ${MINIMUM_PRODUCT_AMOUNT}원 이상, ${MINIMUM_COIN_TYPE}원 단위로 숫자만 입력하여야 합니다.`),
-  PRODUCT_ONLY_NUMBER: Symbol('상품 가격 및 수량은 숫자만 입력할 수 있습니다.'),
+  PRODUCT_ONLY_NUMBER: Symbol('상품 가격 및 수량은 1 이상의 숫자만 입력할 수 있습니다.'),
 
   CHARGE_POSITIVE_NUMBER: Symbol(`충전할 금액은 숫자만 입력 가능하며, ${MINIMUM_COIN_TYPE}원 단위로 입력하여야 합니다.`),
   CHARGE_MINIMUM_NUMBER: Symbol(`충전할 금액을 ${MINIMUM_COIN_TYPE}원 단위로 입력해주세요.`),
 
   PURCHASE_NEED_BALANCE: Symbol('잔액이 부족합니다.\n금액을 투입하여주세요.'),
-  RETURN_AMOUNT_NO_BALANCE: Symbol('충전 금액이 0원입니다.'),
+  RETURN_AMOUNT_NO_BALANCE: Symbol('충전 금액이 0원입니다.\n금액을 투입하여주세요.'),
 });
 
 export const CONSTANTS = Object.freeze({
