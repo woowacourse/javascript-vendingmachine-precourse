f9e81dc56c487d8e14f3199d4dd247d49e402a7f
kamwoo <rkadndud06@naver.com>
2021-12-14 22:49:26 +0900

    feat: product purchase 버튼 액션 메소드 추가
    - 금액 투입 액션 메소드
    - 상품 구매 액션 메소드
    - 잔돈 반환 액션 메소드

--- src/productPurchase/productPurchaseComponent.js
+++ src/productPurchase/productPurchaseComponent.js
@@ -1,1 +1,24 @@
-export default class productPurchaseComponent {}
+import ProductPurchaseView from './productPurchaseView.js';
+import { $ } from '../utils/common.js';
+import { BUTTON } from './productPurchaseViewInfo.js';
+
+export default class productPurchaseComponent {
+  constructor() {
+    this.productPurchaseView = new ProductPurchaseView();
+    this.chargeMoney = 0;
+  }
+
+  configureButtons() {
+    $(`#${BUTTON.CHARGE.ID}`).addEventListener('click', this.onClickChargeButton);
+    $(`#${BUTTON.CHANGES.ID}`).addEventListener('click', this.onClickChagesButton);
+    document.querySelectorAll(`.${BUTTON.PURCHASE.CLASS}`).forEach((button) => {
+      button.addEventListener('click', this.onClickPurchaseButton);
+    });
+  }
+
+  onClickChargeButton = () => {};
+
+  onClickPurchaseButton = (event) => {};
+
+  onClickChagesButton = () => {};
+}
