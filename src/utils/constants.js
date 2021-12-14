69d71332571939fd05881eb5eaa5829713e2e8b6
kamwoo <rkadndud06@naver.com>
2021-12-14 21:33:41 +0900

    feat: constants에 Validation에 사용될 상수 추가
    - Validation에서 사용될 기준 숫자 상수와 에러 메세지 문자열 작성

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,18 @@
 export const APP_ID = '#app';
 export const MAIN_ID = '#main';
+
+export const MIN_PRICE = 100;
+export const MIN_QUANTITY = 1;
+export const ZERO = 0;
+export const TEN = 10;
+
+export const ERROR = {
+  NO_NAME: '상품명을 입력해주세요.',
+  CANT_PLACE: '가격은 1원 단위를 사용할 수 없습니다.',
+  MIN_QUANTITY: '수량을 1이상 입력해주세요',
+  NO_AMOUNT: '금액을 입력해주세요.',
+  MIN_PRICE: '가격은 100원 이상 입력해주세요.',
+  CANT_PURCHASE: '금액이 부족합니다.',
+  NO_QUANTITY: '구매 가능한 수량이 없습니다.',
+  MIN_AMOUNT: '10원 이상의 금액을 입력해주세요.',
+};
