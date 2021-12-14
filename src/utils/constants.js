5c6570df389a4b4037bd702f8125d964cd45ff52
Hoji <b4bynew@gmail.com>
2021-12-14 20:46:59 +0900

    feat: 에러메시지 및 반복단어 상수에 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,12 @@
 export const COINS = [500, 100, 50, 10];
 
+export const INVALID_PRODUCT_INPUT = '입력 값이 올바른지 확인해주세요.';
+export const INVALID_CHARGE_INPUT = '올바른 금액을 입력해주세요.';
+export const INVALID_INSERT_INPUT = '올바른 금액을 투입하세요.';
+export const INVALID_PURCHASE_PRICE = '구매하려는 상품이 투입된 금액보다 비쌉니다.';
+export const INVALID_RETURN_INSERTED = `반환할 금액이 없거나 잔돈이 없습니다.`;
+
 export const STORAGE_PRODUCTS_KEY = 'PRODUCTS';
 export const STORAGE_CHANGES_KEY = 'CHANGES';
 export const STORAGE_REMAINS_KEY = 'REMAINS';
-
-export const INVALID_PRODUCT_INPUT = '입력 값이 올바른지 확인해주세요.';
-export const INVALID_CHARGE_INPUT = '올바른 금액을 입력해주세요.';
+export const STORAGE_INSERTED_KEY = 'INSERTED';
