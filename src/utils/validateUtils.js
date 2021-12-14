0dea88f22b8939381cb0e6e73a10cc4c2c8a7b4a
kamwoo <rkadndud06@naver.com>
2021-12-14 21:36:42 +0900

    feat: validation utils 작성
    - 상품의 이름을 입력했을 때 텍스트가 있는지 검사
    - 상품의 가격을 입력했을 때 1의 자리가 있는지, 100보다 작은지 검사
    - 상품의 수량을 입력했을 때 1보다 작은지 검사

---
+++ src/utils/validateUtils.js
@@ -0,0 +1,31 @@
+import { ERROR, MIN_PRICE, MIN_QUANTITY, TEN } from '../utils/constants.js';
+
+export default class ValidateUtils {
+  static checkInputName(name) {
+    if (!name) {
+      alert(ERROR.NO_NAME);
+      return false;
+    }
+    return true;
+  }
+
+  static checkInputPrice(price) {
+    if (price % TEN) {
+      alert(ERROR.CANT_PLACE);
+      return false;
+    }
+    if (MIN_PRICE > price) {
+      alert(ERROR.MIN_PRICE);
+      return false;
+    }
+    return true;
+  }
+
+  static checkInputQuantity(quantity) {
+    if (MIN_QUANTITY > quantity) {
+      alert(ERROR.MIN_QUANTITY);
+      return false;
+    }
+    return true;
+  }
+}
