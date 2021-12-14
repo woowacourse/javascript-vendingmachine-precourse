f6936197fa40c58ac8ce96c2a51728e07c9c6947
연훈 배 <sbaru1997@naver.com>
2021-12-14 23:51:31 +0900

    style: table 스타일을 global로 추가

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import Header from './components/header.js';
 import $ from './util/domSelector.js';
+import globalStyle from './components/globalStyle.js';
 
 export default class App {
   constructor() {
@@ -8,6 +9,7 @@ export default class App {
   }
 
   init() {
+    $('head').innerHTML += globalStyle();
     new Header(this.$target);
   }
 }
---
+++ src/components/globalStyle.js
@@ -0,0 +1,15 @@
+export default function globalStyle() {
+  return `
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+        
+        th, td {
+            border: 1px solid black;
+            padding: 10px 20px 10px 20px;
+            text-align: center;
+        }
+    </style>
+    `;
+}
