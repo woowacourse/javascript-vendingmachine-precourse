5f0195c3e98d99a46b773f8f8b87988cd0aeed84
kamwoo <rkadndud06@naver.com>
2021-12-14 22:10:39 +0900

    feat: 충전하기 버튼 액션 작성
    - 버튼을 클릭했을 때 입력 폼의 텍스트 받아옴

--- src/vendingMachineManage/vendingMachineManageComponent.js
+++ src/vendingMachineManage/vendingMachineManageComponent.js
@@ -1,1 +1,21 @@
-export default class VendingMachineManageComponent {}
+import VendingMachineManageView from './vendingMachineManageView.js';
+import { $ } from '../utils/common.js';
+
+export default class VendingMachineManageComponent {
+  constructor() {
+    this.vendingMachineManageView = new VendingMachineManageView();
+  }
+
+  render() {
+    this.vendingMachineManageView.render();
+    this.configureButton();
+  }
+
+  configureButton() {
+    $(`#${buttonInfo.id}`).addEventListener('click', this.onClickButton);
+  }
+
+  onClickButton = () => {
+    const inputCoin = Number($(`#${inputInfo.id}`).value);
+  };
+}
