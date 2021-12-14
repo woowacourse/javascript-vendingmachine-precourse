80e803603114bff7dab6b3688f66dd7f5a244259
kamwoo <rkadndud06@naver.com>
2021-12-14 20:45:43 +0900

    feat: product add 컴포넌트, 뷰 빈 클래스 작성
    - vending machine app에 객체 생성하는 구문 추가

--- src/vendingMachineApp.js
+++ src/vendingMachineApp.js
@@ -1,9 +1,11 @@
 import { BUTTONS_INFO } from './header/headerViewInfo.js';
 import { $ } from './utils/common.js';
 import HeaderView from './header/headerView.js';
+import ProductAddComponent from './productAdd/productAddComponent.js';
 
 export default class VendingMachineApp {
   constructor() {
     this.headerView = new HeaderView();
+    this.productAddComponent = new ProductAddComponent();
   }
 }
