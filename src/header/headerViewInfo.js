7685cf8e7e67a6042ad14b436b731965fae5ce74
kamwoo <rkadndud06@naver.com>
2021-12-14 20:17:17 +0900

    feat: header view info 파일 작성
    - header view의 정보를 저장한다.
    - 타이틀 문자열 상수
    - 버튼 id, text 문자열 상수

---
+++ src/header/headerViewInfo.js
@@ -0,0 +1,16 @@
+export const HEADER_TITLE = '자판기';
+
+export const BUTTONS_INFO = {
+  PRODUCT_ADD: {
+    TEXT: '상품 관리',
+    ID: 'product-add-menu',
+  },
+  VENDING_MACHINE_MANAGE: {
+    TEXT: '잔돈 충천',
+    ID: 'vending-machine-manage-menu',
+  },
+  PRODUCT_PURCHASE: {
+    TEXT: '상품 구매',
+    ID: 'product-purchase-menu',
+  },
+};
