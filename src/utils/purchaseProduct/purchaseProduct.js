65bc97ea2947f46c653d69b765299afb11dbff6d
JuYoung <ken44929@gmail.com>
2021-12-14 22:26:23 +0900

    feat: 투입 금액보다 더 비싼 상품을 구매했을 때 경고창 띄우기

--- src/utils/purchaseProduct/purchaseProduct.js
+++ src/utils/purchaseProduct/purchaseProduct.js
@@ -5,9 +5,13 @@ import { renderPurchaseProduct } from './renderPurchaseProduct.js';
 export const purchaseProduct = (e, state) => {
   const products = e.target.closest('tr').childNodes;
   const id = e.target.closest('tr').dataset.productId;
-  const name = products[1].dataset.productName;
   const price = products[3].dataset.productPrice;
   let quantity = products[5].dataset.productQuantity;
+
+  if (price > state.purchase.input) {
+    alert(ERROR_MSG.INSUFFICIENT_CASH);
+    return;
+  }
 
   if (quantity <= 0) {
     alert(ERROR_MSG.SOLD_OUT);
