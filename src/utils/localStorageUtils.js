99896b0058be83bdcbba915bd5bb4f6d05067260
kamwoo <rkadndud06@naver.com>
2021-12-14 22:02:13 +0900

    feat: local storage utils에 동전 데이터 메소드 추가
    - 로컬 저장소에 동전 객체를 저장하는 메소드 추가
    - 로컬 저장소에 동전 객체를 가져오는 메소드 추가

--- src/utils/localStorageUtils.js
+++ src/utils/localStorageUtils.js
@@ -1,6 +1,7 @@
 export default class LocalStorageUtils {
   static keys = {
     productAdd: 'productAdd',
+    machineMange: 'machineMange',
   };
 
   static getItem(key) {
@@ -18,5 +19,20 @@ export default class LocalStorageUtils {
 
   static setProductAddItem(value) {
     this.setItem(this.keys.productAdd, value);
+  }
+
+  static getMachineManageTableItem() {
+    const emptyItem = {
+      500: 0,
+      100: 0,
+      50: 0,
+      10: 0,
+    };
+    const data = this.getItem(this.keys.machineMange);
+    return data ? data : emptyItem;
+  }
+
+  static setMachineManageTableItem(value) {
+    this.setItem(this.keys.machineMange, value);
   }
 }
