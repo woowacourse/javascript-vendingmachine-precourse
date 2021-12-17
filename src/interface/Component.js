39837651326a74b0dfeeb2f699431fea3cb2b2ee
NE5054 <w_lan123@naver.com>
2021-12-18 01:26:37 +0900

    refactor: Component interface 구조 변경

--- src/interface/Component.js
+++ src/interface/Component.js
@@ -1,31 +1,35 @@
-import { getState, setState, setStates } from '../store/index.js';
+import { $ } from '../common/utils.js';
+import config from '../constants/configuration.js';
+import resources from '../constants/resources.js';
+import selectors from '../constants/selectors.js';
 
 export default class Component {
-    $target;
+    constructor(target, props) {
+        this.target = target;
+        this.props = props;
 
-    $props;
+        if (props?.key) {
+            this.key = props.key;
+            this.resources = resources[this.key];
+            this.selectors = selectors[this.key];
+            this.config = config[this.key];
+        }
 
-    $state;
-
-    constructor($target, $props) {
-        this.$target = $target;
-        this.$props = $props;
-        this.$state = null;
-        this.setState = setState;
-        this.setStates = setStates;
-        this.getState = getState;
+        this.eventAttr = selectors.COMMON.EVENT_DATA_KEY;
         this.setup();
-        this.render();
     }
-
-    template() {}
 
     setup() {}
 
     mount() {}
 
+    willmount() {}
+
+    template() {}
+
     render() {
-        this.$target.innerHTML = this.template();
+        this.willmount();
+        $(this.target).innerHTML = this.template();
         this.mount();
     }
 }
