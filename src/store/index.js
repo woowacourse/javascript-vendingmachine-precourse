9bbc1bb67df836fd226ded3c50f08ab08a1f16e4
NE5054 <w_lan123@naver.com>
2021-12-18 01:24:53 +0900

    refactor: store 구조 변경 및 기능 개선

--- src/store/index.js
+++ src/store/index.js
@@ -1,59 +1,60 @@
-import defaultStatus from './defaultStatus.js';
-
 class Store {
-    constructor(initialState) {
-        if (localStorage.getItem('state') === null) {
-            localStorage.setItem('state', JSON.stringify({ ...initialState }));
-        }
-        this.state = JSON.parse(localStorage.getItem('state'));
+    constructor() {
+        this.storage = localStorage.getItem('state');
+        this.callback = () => {};
+
+        if (this.storage !== null) {
+            this.state = JSON.parse(this.storage);
+        } else this.state = {};
+
+        // window.onunload = () => localStorage.removeItem('state');
     }
 
-    setTriggerStateChange(func) {
-        this.afterStateChange = func;
+    createStore(initialization) {
+        if (this.storage !== null) return;
+
+        this.state = { ...initialization };
+        this.apply();
     }
 
     setState(key, value) {
-        if (typeof key !== 'string') throw 'setState: parameter type error';
-
         this.state = {
             ...this.state,
             [key]: value,
         };
 
-        this.applyLocalStorage();
+        this.apply();
     }
 
-    setStates(state) {
-        if (typeof state !== 'object') throw 'setState: parameter type error';
-
+    setStates(states) {
         this.state = {
             ...this.state,
-            ...state,
+            ...states,
         };
 
-        this.applyLocalStorage();
-    }
-
-    applyLocalStorage() {
-        localStorage.setItem('state', JSON.stringify(this.state));
-        this.afterStateChange(this.state);
+        this.apply();
     }
 
     getState(selector) {
         return selector(this.state);
     }
+
+    apply() {
+        localStorage.setItem('state', JSON.stringify(this.state));
+        this.callback();
+    }
+
+    subscribe(callback) {
+        this.callback = callback;
+    }
 }
 
-const store = new Store(defaultStatus);
+const store = new Store();
 
-export function setState(key, value) {
-    store.setState(key, value);
-}
-export function setStates(states) {
-    store.setStates(states);
-}
-export function getState(selector) {
-    return store.getState(selector);
-}
+export const createStore = store.createStore.bind(store);
+export const subscribe = store.subscribe.bind(store);
+export const setStates = store.setStates.bind(store);
+export const setState = store.setState.bind(store);
+export const getState = store.getState.bind(store);
 
 export default store;
